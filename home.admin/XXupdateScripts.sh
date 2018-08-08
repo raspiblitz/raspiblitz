bf3f1ca02821f9121ebe6cc66b3f4bbbd39ffb69
rootzoll <christian@geektank.de>
2018-08-08 19:58:15 +0200

    minimal update script

---
+++ home.admin/XXupdateScripts.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+echo ""
+echo "*** UPDATING SHELL SCRIPTS FROM GITHUB ***"
+echo "justincase, not the final upadte mechanism"
+echo "******************************************"
+cd /home/admin/raspiblitz
+git pull
+cd ..
+rm *.sh
+rm -r assets
+sudo -u admin cp /home/admin/raspiblitz/home.admin/*.sh /home/admin
+sudo -u admin chmod +x *.sh
+sudo -u admin cp -r /home/admin/raspiblitz/home.admin/assets /home/admin/
+echo "OK - shell scripts and assests are up to date"
