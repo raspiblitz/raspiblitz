de8b659c421191af84c81064be440f7d030b2097
-k <slowdive@me.com>
2018-08-09 10:09:56 -0400

    update destinations

--- home.admin/XXupdateScripts.sh
+++ home.admin/XXupdateScripts.sh
@@ -6,17 +6,15 @@ cd /home/admin/raspiblitz
 get_latest_release_tag() {
   curl --silent "https://api.github.com/repos/charlesrocket/raspiblitz/releases/latest" |
     grep '"tag_name":' |
-    sed -E 's/.*"([^"]+)".*/\1/'                                  
+    sed -E 's/.*"([^"]+)".*/\1/'
 }
-
 vTAG=`get_latest_release_tag`
-
 wget https://github.com/charlesrocket/raspiblitz/archive/$vTAG.zip
-
+unzip $vTAG.zip
 cd ..
 rm *.sh
 rm -r assets
-sudo -u admin cp /home/admin/raspiblitz/home.admin/*.sh /home/admin
+sudo -u admin cp /home/admin/raspiblitz/raspiblitz-$vTAG/home.admin/*.sh /home/admin
 sudo -u admin chmod +x *.sh
 sudo -u admin cp -r /home/admin/raspiblitz/home.admin/assets /home/admin/
 echo "******************************************"
