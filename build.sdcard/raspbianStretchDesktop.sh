ee67947a631926779783d4cc22127108b4d8e284
rootzoll <christian@geektank.de>
2018-08-15 13:48:35 +0200

    updated version

--- build.sdcard/raspbianStretchDesktop.sh
+++ build.sdcard/raspbianStretchDesktop.sh
@@ -10,7 +10,7 @@
 
 echo ""
 echo "***************************************"
-echo "* RASPIBLITZ SD CARD IMAGE SETUP v0.5 *"
+echo "* RASPIBLITZ SD CARD IMAGE SETUP v0.6 *"
 echo "***************************************"
 echo ""
 
