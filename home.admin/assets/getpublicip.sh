5d706308b5fb853b44d5a0c38d24db369adf8746
rootzoll <christian@geektank.de>
2018-08-24 14:07:31 +0200

    fix public for tor

--- home.admin/assets/getpublicip.sh
+++ home.admin/assets/getpublicip.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-# RaspiBolt LND Mainnet: script to get public ip address
-# /usr/local/bin/getpublicip.sh
 
 echo 'getpublicip.sh started, writing public IP address every 10 minutes into /run/publicip'
 while [ 0 ];
     do
-    # when TOR is installed the fixed onion address is already in /run/publicip
-    torExists=$(sudo ls /mnt/hdd/tor/web80/hostname 2>/dev/null | grep hostname -c)
+    # check if TOR is running
+    torExists=$(sudo ls /mnt/hdd/tor/lnd9735/hostname 2>/dev/null | grep hostname -c)
     if [ ${torExists} -eq 0 ]; then
-      # get public IP
+      # get and set public IP
       printf "PUBLICIP=$(curl -vv ipinfo.io/ip 2> /run/publicip.log)\n" > /run/publicip;
+    else
+      # set onion address
+      printf "PUBLICIP=$(sudo cat /mnt/hdd/tor/lnd9735/hostname)\n" > /run/publicip;
     fi
     sleep 600
 done;
