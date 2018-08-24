bda47cefa5a1a46704472f27ab1cdd0dcd384d9d
rootzoll <christian@geektank.de>
2018-08-24 13:40:15 +0200

    better scanning info

--- home.admin/80scanLND.sh
+++ home.admin/80scanLND.sh
@@ -11,11 +11,22 @@ localip=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1 
 item=0
 blockchaininfo=$(sudo -u bitcoin ${network}-cli -datadir=/home/bitcoin/.${network} getblockchaininfo)
 chain="$(echo "${blockchaininfo}" | jq -r '.chain')"
-item=$(sudo -u bitcoin tail -n 100 /mnt/hdd/lnd/logs/${network}/${chain}net/lnd.log | grep "Caught up to height" | tail -n1 | cut -d ']' -f2 | tr -dc '0-9')
+
+## TRY to get the actual progress height of scanning
+
+# 1) First try the "Rescanned through block" - it seems to happen if it restarts
+item=$(sudo -u bitcoin tail -n 100 /mnt/hdd/lnd/logs/${network}/${chain}net/lnd.log | grep "Rescanned through block" | tail -n1 | cut -d ']' -f2 | cut -d '(' -f2 | tr -dc '0-9')
+
+# 2) Second try the "Caught up to height" - thats the usual on first scan start
 if [ ${#item} -eq 0 ]; then
-  # TODO add fallback later here if necessary
+  item=$(sudo -u bitcoin tail -n 100 /mnt/hdd/lnd/logs/${network}/${chain}net/lnd.log | grep "Caught up to height" | tail -n1 | cut -d ']' -f2 | tr -dc '0-9')
+fi
+
+# TODO next fallback try later here if necessary
+if [ ${#item} -eq 0 ]; then
   item="?" 
 fi
+
 # get total number of blocks
 total=$(echo "${blockchaininfo}" | jq -r '.blocks')
 # put scanstate
