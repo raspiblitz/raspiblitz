4c676a1d0a2e6cc34ff3fdfae6f53f5a5b687a43
rootzoll <christian@geektank.de>
2018-08-31 20:54:42 +0200

    menu

--- home.admin/97addMobileWallet.sh
+++ home.admin/97addMobileWallet.sh
@@ -6,8 +6,7 @@ OPTIONS=(ZAP "Zap Wallet (iOS)" \
 
 CHOICE=$(dialog --clear \
                 --title "Choose Mobile Wallet" \
-                --menu "MENU" \
-                8 40 2 \
+                8 40 3 \
                 "${OPTIONS[@]}" \
                 2>&1 >/dev/tty)
 
