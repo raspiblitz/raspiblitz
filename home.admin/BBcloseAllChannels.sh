be2f161db2376dfc1235d0a288a814c9331a8285
rootzoll <christian@geektank.de>
2018-11-30 13:14:27 +0100

    added autopilot precheck

--- home.admin/BBcloseAllChannels.sh
+++ home.admin/BBcloseAllChannels.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+source /mnt/hdd/raspiblitz.conf
+if [ "${autoPilot}" = "on" ]; then
+  echo "PRECHECK: You need to turn OFF the AutoPilot first,"
+  echo "so that closed channels are not opening up again."
+  echo "You find the AutoPilot under the SERVICES section."
+  exit 1
+fi
+
 # load network and chain info
 network=`cat .network`
 chain=$(${network}-cli -datadir=/home/bitcoin/.${network} getblockchaininfo | jq -r '.chain')
