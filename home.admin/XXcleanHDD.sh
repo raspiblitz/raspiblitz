87d72e7abfdd2b80c72e479fcb91ab534b0c1f4f
rootzoll <christian@geektank.de>
2018-11-30 17:04:04 +0100

    delete the new folders

--- home.admin/XXcleanHDD.sh
+++ home.admin/XXcleanHDD.sh
@@ -21,7 +21,10 @@ sudo rm -f /mnt/hdd/${network}/testnet3/*.dat
 sudo rm -f /mnt/hdd/${network}/testnet3/*.log
 sudo rm -f /mnt/hdd/${network}/testnet3/.lock
 sudo rm -f -r /mnt/hdd/${network}/database
+sudo rm -f -r /mnt/hdd/lost+found
+sudo rm -f -r /mnt/hdd/download
 sudo rm -f -r /mnt/hdd/tor
+sudo rm -f /mnt/hdd/raspiblitz.conf
 sudo chown admin:admin -R /mnt/hdd/${network}
 echo "1" > /home/admin/.setup
 echo "OK - the HDD is now clean"
