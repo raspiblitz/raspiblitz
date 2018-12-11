a1fdafde5a3486445ed729632b9ed8fbc450d546
rootzoll <christian@geektank.de>
2018-12-11 04:53:22 +0100

    scan action detail

--- home.admin/80scanLND.sh
+++ home.admin/80scanLND.sh
@@ -12,15 +12,18 @@ chain="$(echo "${blockchaininfo}" | jq -r '.chain')"
 
 # 1) First try the "Rescanned through block" - it seems to happen if it restarts
 item=$(sudo -u bitcoin tail -n 100 /mnt/hdd/lnd/logs/${network}/${chain}net/lnd.log | grep "Rescanned through block" | tail -n1 | cut -d ']' -f2 | cut -d '(' -f2 | tr -dc '0-9')
+action="Rescanning"
 
 # 2) Second try the "Caught up to height" - thats the usual on first scan start
 if [ ${#item} -eq 0 ]; then
   item=$(sudo -u bitcoin tail -n 100 /mnt/hdd/lnd/logs/${network}/${chain}net/lnd.log | grep "Caught up to height" | tail -n1 | cut -d ']' -f2 | tr -dc '0-9')
+  action="Catching-Up"
 fi
 
 # 3) Third try the "LNWL: Filtering block" - thats the usual on later starts
 if [ ${#item} -eq 0 ]; then
   item=$(sudo -u bitcoin tail -n 100 /mnt/hdd/lnd/logs/${network}/${chain}net/lnd.log | grep "LNWL: Filtering block" | tail -n1 | cut -d ' ' -f7 | tr -dc '0-9')
+  action="Filtering"
 fi
 
 # if no progress info
@@ -55,11 +58,11 @@ if [ ${isInitialChainSync} -gt 0 ]; then
   fi
 else
   heigh=7
-  infoStr=$(echo " Lightning Rescanning Blockchain\n Progress: ${scanstate}\n Please wait - this can take some time\n ssh admin@${localip}\n Password A")
+  infoStr=$(echo " Lightning ${action} Blockchain\n Progress: ${scanstate}\n Please wait - this can take some time\n ssh admin@${localip}\n Password A")
   if [ "$USER" = "admin" ]; then
     heigh=6
     width=53
-    infoStr=$(echo " Lightning Rescanning Blockchain\n Progress: ${scanstate}\n Please wait - this can take some long time.\n Its OK to close terminal and ssh back in later.")
+    infoStr=$(echo " Lightning ${action} Blockchain\n Progress: ${scanstate}\n Please wait - this can take some long time.\n Its OK to close terminal and ssh back in later.")
   fi
 fi
 
