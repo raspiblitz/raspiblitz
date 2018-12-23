913f4eabbd9959efacb9ef1784698eb8565022cc
/geco <geco@MacBookAir512.local>
2018-12-24 00:56:22 +0100

    adding shortcut commands

---
+++ home.admin/_commands.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# SHORTCUT COMMANDS you can call as user 'admin' from terminal
+
+# command: raspiblitz
+# calls the the raspiblitz mainmenu
+function raspiblitz() {
+  /home/admin/00mainMenu.sh
+}
