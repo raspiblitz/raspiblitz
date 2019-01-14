2c6f3f3279791d107fae92ce056f0e3b15433b09
rootzoll <christian@geektank.de>
2019-01-14 20:22:32 +0100

    #273 prevent double publicIP entry

--- home.admin/_bootstrap.sh
+++ home.admin/_bootstrap.sh
@@ -257,7 +257,8 @@ if [ ${configExists} -eq 1 ]; then
 
   # update public IP on boot
   freshPublicIP=$(curl -s http://v4.ipv6-test.com/api/myip.php)
-  if [ ${#publicIP} -eq 0 ]; then
+  publicIPValueExists=$( sudo cat ${configFile} | grep -c 'publicIP=' )
+  if [ ${publicIPValueExists} -eq 0 ]; then
     echo "create value (${freshPublicIP})" >> $logFile
     echo "publicIP=${freshPublicIP}" >> $configFile
   else
