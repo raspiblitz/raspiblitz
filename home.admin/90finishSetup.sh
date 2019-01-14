ea86e9c3c6c9b965255057feed23b9edba19347c
rootzoll <christian@geektank.de>
2019-01-14 20:56:14 +0100

    #274 remove os upgrade during setup

--- home.admin/90finishSetup.sh
+++ home.admin/90finishSetup.sh
@@ -84,7 +84,6 @@ echo ""
 echo "*** Update System ***"
 sudo apt-mark hold raspberrypi-bootloader
 sudo apt-get update
-sudo apt-get upgrade -f -y --allow-change-held-packages
 echo "OK - System is now up to date"
 
 # mark setup is done
