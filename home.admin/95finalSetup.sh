4ed14930f341963938fe55698bb3e76d5da4160e
rootzoll <christian@geektank.de>
2019-02-04 11:33:35 +0100

    make sure setup and upadte set setupstep=100

--- home.admin/95finalSetup.sh
+++ home.admin/95finalSetup.sh
@@ -29,7 +29,7 @@ cp $logFile /home/admin/raspiblitz.setup.log
 echo "Setting the Name/Alias/Hostname .."
 sudo /home/admin/config.scripts/lnd.setname.sh ${hostname}
 
-# mark setup is done (100%)
+# mark setup is done
 sudo sed -i "s/^setupStep=.*/setupStep=100/g" /home/admin/raspiblitz.info
 
 clear
