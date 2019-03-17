ca708c83cdf20632e3c98f761d35703206c10623
Christian Rotzoll <christian@geektank.de>
2019-03-17 18:04:56 +0100

    color undervoltage warning

--- home.admin/00infoBlitz.sh
+++ home.admin/00infoBlitz.sh
@@ -295,5 +295,5 @@ ${color_yellow}${ln_publicColor}${ln_external}
 "${hdd}" "${sync_percentage}"
 
 if [ ${#undervoltageReports} -gt 0 ] && [ "${undervoltageReports}" != "0" ]; then
-  echo "${undervoltageReports} undervoltage reports found in syslog"
+  echo "!! ${color_red}${undervoltageReports} undervoltage reports found in syslog !!"
 fi
