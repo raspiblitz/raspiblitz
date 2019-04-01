2a0dac32bfad544c8f93052a55fd57b8dcd6f871
Christian Rotzoll <christian@geektank.de>
2019-04-01 23:43:21 +0100

    help text

--- home.admin/config.scripts/internet.sshtunnel.py
+++ home.admin/config.scripts/internet.sshtunnel.py
@@ -2,5 +2,10 @@
 
 import sys
 
-print ('Number of arguments:', len(sys.argv), 'arguments.')
-print ('Argument List:', str(sys.argv))
+if len(sys.argv) <= 1 or sys.argv[1] == "-h" or sys.argv[1] == "-help":
+    print("forward ports from another server to raspiblitz with reverse SSH tunnel")
+    print("internet.sshtunnel.py [on|off] [USER]@[SERVER] [INTERNAL-PORT]:[EXTERNAL-PORT]")
+    print("note that [INTERNAL-PORT]:[EXTERNAL-PORT] can one or multiple forwardings")
+    sys.exit(1)
+
+print ("TODO: implement")
