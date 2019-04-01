67b25673d9e70dd6545bc6eb094f47578138dc60
Christian Rotzoll <christian@geektank.de>
2019-04-01 23:31:42 +0100

    test strings

--- home.admin/config.scripts/internet.sshtunnel.py
+++ home.admin/config.scripts/internet.sshtunnel.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
 import sys
-print(f"Number of Arguments: {len(sys.argv)}")
-print(f"Arguments: {sys.argv}")
+
+print ('Number of arguments:', len(sys.argv), 'arguments.')
+print ('Argument List:', str(sys.argv))
