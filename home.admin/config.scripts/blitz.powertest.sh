d11b2c91940fb85f8837c6fe9d8b4de7c97ee9af
Christian Rotzoll <christian@geektank.de>
2019-04-10 00:48:53 +0100

    openoms powertest

--- home.admin/config.scripts/blitz.powertest.sh
+++ home.admin/config.scripts/blitz.powertest.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
+
+# Based on https://github.com/bamarni/pi64/issues/4#issuecomment-292707581
+# sysbench manual: http://imysql.com/wp-content/uploads/2014/10/sysbench-manual.pdf
+
+sysbenchInstalled=$(sysbench --version 2>/dev/null | grep -c 'sysbench 0.')
+if [ ${sysbenchInstalled} -eq 0 ];then
+  sudo apt install -y sysbench
+fi
+clear
+
+echo ""
+echo "***"
+echo "RaspiBlitz powertest v0.1"
+echo "Starting sysbench to run for 60 seconds (--max-time=60 --cpu-max-prime=10000)"
+echo "***"
+echo ""
+
+sysbench --max-time=60 --test=cpu --cpu-max-prime=10000 --num-threads=4 run & # > /dev/null 2>&1 & 
+
 Maxfreq=$(( $(awk '{printf ("%0.0f",$1/1000); }'  </sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq) -15 ))
-while true ; do
-	# Health=$(perl -e "printf \"%19b\n\", $(vcgencmd get_throttled | cut -f2 -d=)")
-	Temp=$(vcgencmd measure_temp | cut -f2 -d=)
-	RealClockspeed=$(vcgencmd measure_clock arm | awk -F"=" '{printf ("%0.0f",$2/1000000); }' )
+for (( n=0; n<15; ++n )); do
+	Temp=$(sudo vcgencmd measure_temp | cut -f2 -d=)
+	RealClockspeed=$(sudo vcgencmd measure_clock arm | awk -F"=" '{printf ("%0.0f",$2/1000000); }' )
 	SysFSClockspeed=$(awk '{printf ("%0.0f",$1/1000); }' </sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq)
-	CoreVoltage=$(vcgencmd measure_volts | cut -f2 -d= | sed 's/000//')
+	CoreVoltage=$(sudo vcgencmd measure_volts | cut -f2 -d= | sed 's/000//')
 	if [ ${RealClockspeed} -ge ${Maxfreq} ]; then
-	  echo -e "${Temp}$(printf "%5s" ${SysFSClockspeed}) MHz $(printf "%019d" ${Health}) ${CoreVoltage}"
+		echo -e "${Temp}$(printf "%5s" ${SysFSClockspeed}) MHz  ${CoreVoltage}"
 	else
-	  echo -e "${Temp}$(printf "%5s" ${SysFSClockspeed})/$(printf "%4s" ${RealClockspeed}) MHz ${CoreVoltage}"
-  fi
+	  	echo -e "${Temp}$(printf "%5s" ${RealClockspeed})/$(printf "%4s" ${SysFSClockspeed}) MHz ${CoreVoltage}"
+  	fi
 	sleep 5
 done
