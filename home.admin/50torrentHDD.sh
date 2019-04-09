f8953c8f77c68851a1317de46134bed21dedf794
Christian Rotzoll <christian@geektank.de>
2019-04-09 21:49:58 +0100

    #504 new update torrent

--- home.admin/50torrentHDD.sh
+++ home.admin/50torrentHDD.sh
@@ -27,7 +27,7 @@ echo "*** Torrent Files ***"
 # WITHOUT THE '.torrent' ENDING
 
 bitcoinBase="raspiblitz-bitcoin1-2018-10-13-base"
-bitcoinUpdate="raspiblitz-bitcoin1-2019-01-16-update"
+bitcoinUpdate="raspiblitz-bitcoin1-2019-04-09-update"
 
 litecoinBase="raspiblitz-litecoin1-2018-11-18-base"
 litecoinUpdate="raspiblitz-litecoin1-2018-11-18-update"
