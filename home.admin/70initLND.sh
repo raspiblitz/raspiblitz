fbec65fac1f117220cf142bcf507bb8f1dcd92ca
Christian Rotzoll <christian@geektank.de>
2019-04-15 02:05:58 +0100

    UI fixes

--- home.admin/70initLND.sh
+++ home.admin/70initLND.sh
@@ -189,7 +189,7 @@ if [ ${walletExists} -eq 0 ]; then
     ack=0
     while [ ${ack} -eq 0 ]
     do
-      whiptail --title "IMPORTANT - PLEASE WRITE DOWN" --msgbox "LND Wallet got created. Store these numbered words in a safe location:\n${seedwords6x4}" 12 76
+      whiptail --title "IMPORTANT SEED WORDS - PLEASE WRITE DOWN" --msgbox "LND Wallet got created. Store these numbered words in a safe location:\n\n${seedwords6x4}" 12 76
       whiptail --title "Please Confirm" --yes-button "Show Again" --no-button "CONTINUE" --yesno "  Are you sure that you wrote down the word list?" 8 55
       if [ $? -eq 1 ]; then
         ack=1
