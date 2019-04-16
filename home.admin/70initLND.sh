656f37f8b5042c4cdef9989309b04365ad732a78
Christian Rotzoll <christian@geektank.de>
2019-04-16 14:11:21 +0100

    activate seed word list again

--- home.admin/70initLND.sh
+++ home.admin/70initLND.sh
@@ -228,7 +228,7 @@ or having a complete LND rescue-backup from your old node.
     fi
 
     # get seed word list
-    if [ "${CHOICE}" == "!SEED+SCB" ] || [ "${CHOICE}" == "!ONLYSEED" ]; then
+    if [ "${CHOICE}" == "SEED+SCB" ] || [ "${CHOICE}" == "ONLYSEED" ]; then
 
       # dialog to enter
       dialog --backtitle "RaspiBlitz - LND Recover" --inputbox "Please enter/paste the SEED WORD LIST:\n(just the words, seperated by commas, in correct order as numbered)" 9 78 2>/home/admin/.seed.tmp
