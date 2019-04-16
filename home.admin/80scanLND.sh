2d0a6f5db677265d24773876932ac5a3d171bfba
Christian Rotzoll <christian@geektank.de>
2019-04-16 21:53:22 +0100

    Progress screen

--- home.admin/80scanLND.sh
+++ home.admin/80scanLND.sh
@@ -6,12 +6,11 @@ source /mnt/hdd/raspiblitz.conf
 source <(sudo /home/admin/config.scripts/blitz.statusscan.sh)
 
 height=6
-width=44
+width=52
 
-#infoStr=" Waiting for Blockchain Sync\n Progress: ${syncProgress}% \n Please wait - this can take some time.\n ssh admin@${localIP}\n Password A"
-infoStr=" Lightning Scanning Blockchain\n Progress: ${scanProgress}%\n Please wait - this can take some time\n ssh admin@${localIP}\n Password A"
+#infoStr=" Waiting for Blockchain Sync\n Progress: ${syncProgress}% \n Please wait - this can take some time.\n ssh admin@${localIP} -> Password A"
+infoStr=" Node is Syncing\n Blockchain Progress : ${scanProgress}%\n Lightning Progress  : ${scanProgress}%\n Please wait - this can take some time\n ssh admin@${localIP} ->Password A"
 
 # display progress to user
-sleep 3
 temp=$(echo "scale=1; $(cat /sys/class/thermal/thermal_zone0/temp)/1000" | bc)
 dialog --title " ${network} / ${chain} " --backtitle "RaspiBlitz (${hostname}) CPU: ${temp}°C" --infobox "${infoStr}" ${height} ${width}
