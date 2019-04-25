b996738c141f642fa862d58ca9981d969a1b3714
Christian Rotzoll <christian@geektank.de>
2019-04-25 13:35:41 +0200

    test lnd check script

---
+++ home.admin/config.scripts/lnd.check.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]; then
+ echo "# script to check LND states"
+ echo "# lnd.check.sh basic-setup"
+ exit 1
+fi
+
+if [ "$1" -eq "basic-setup" ]; then
+
+   # check TLS exits
+  tlsExists=$(sudo ls /mnt/hdd/lnd/tls.cert | grep -c 'tls.cert')
+  if [ ${tlsExists} -gt 0 ]; then
+    echo "tls=1"
+  else
+    echo "tls=0"
+  fi
+
+  # check lnd.conf exits
+  lndConfExists=$(sudo ls /mnt/hdd/lnd/lnd.conf | grep -c 'lnd.conf')
+  if [ ${lndConfExists} -gt 0 ]; then
+    echo "config=1"
+  else
+    echo "config=0"
+  fi
+
+  # load config values
+  source <(sudo sed -e 's/\[/#/g' /mnt/hdd/lnd/lnd.conf)
+  echo "debuglevel=${debuglevel}"
+  echo "bitcoin.mainnet=${bitcoin.mainnet}"
+  echo "bitcoin.testnet=${bitcoin.testnet}"
+
+else
+  echo "# FAIL: parameter not known"
+fi
