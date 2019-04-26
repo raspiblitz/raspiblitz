e7d1d8806c5028d79b8d8729eedc45f57ca50578
Christian Rotzoll <christian@geektank.de>
2019-04-26 18:04:32 +0200

    result info scb import

--- home.admin/70initLND.sh
+++ home.admin/70initLND.sh
@@ -466,25 +466,36 @@ if [ ${gotSCB} -eq 1 ]; then
   rm /home/admin/.error.tmp 2>/dev/null
 
   if [ ${#error} -gt 0 ]; then
+
+    # output error message
     echo ""
     echo "!!! FAIL !!! SOMETHING WENT WRONG:"
+    echo "${error}"
 
+    # check if its possible to give background info on the error
     notMachtingSeed=$(echo $error | grep -c 'unable to unpack chan backup')
     if [ ${notMachtingSeed} -gt 0 ]; then
-      echo "--> unable to unpack chan backup"
+      echo "--> ERROR BACKGROUND:"
       echo "The WORD SEED is not matching the channel.backup file."
       echo "Either there was an error in the word seed list or"
       echo "or the channel.backup file is from another RaspiBlitz."
-    else
-      echo "${error}"
+      echo 
     fi
 
+    # basic info on error
     echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-    echo ""
+    echo 
     echo "You can try after full setup to restore channel.backup file again with:"
     echo "lncli restorechanbackup --multi_file=/home/admin/channel.backup"
+    echo
     echo "Press ENTER to continue for now ..."
     read key
+  else
+     dialog --title " OK channel.backup IMPORT " --msgbox "
+LND accepted the channel.backup file you uploaded. 
+After LND is synced to the blockchain it will try 
+to close your old channels to recover the funds.
+     " 9 54
   fi
   
 else
