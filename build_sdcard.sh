b7f6b006a88c6fd9996aa37374a868958c13d44e
openoms <openoms@riseup.net>
2019-05-09 11:29:08 +0100

    update go to 1.12.5

--- build_sdcard.sh
+++ build_sdcard.sh
@@ -569,6 +569,7 @@ echo ""
 
 # "*** Installing Go ***"
 # Go is needed for ZAP connect later
+# see https://golang.org/dl/
 goVersion="1.12.5"
 if [ ${isARM} -eq 1 ] || [ ${isAARCH64} -eq 1 ] ; then
   goOSversion="armv6l"
