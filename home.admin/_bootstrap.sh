1a9d1c0d99c649a7336949f4cd2936c0692aa2fc
Christian Rotzoll <christian@geektank.de>
2019-06-13 14:44:02 +0200

    #576 delete syslog in no warnings

--- home.admin/_bootstrap.sh
+++ home.admin/_bootstrap.sh
@@ -508,6 +508,10 @@ if [ "${baseImage}" = "raspbian" ] ; then
   sed -i "s/^state=.*/state=stresstest/g" ${infoFile}
   sed -i "s/^message=.*/message='Testing Hardware 60s'/g" ${infoFile}
   sudo /home/admin/config.scripts/blitz.stresstest.sh /home/admin/stresstest.report
+  source /home/admin/stresstest.report
+  if [ "${powerWARN}" = "0" ]; then
+    sudo rm /var/log/syslog
+  fi
 fi
 
 echo "DONE BOOTSTRAP" >> $logFile
