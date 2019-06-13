ed2af7d9c08c654d5f615ff2dbeea34762d335be
Christian Rotzoll <christian@geektank.de>
2019-06-13 14:17:26 +0200

    make sync all file sin dir

--- home.admin/XXsyncScripts.sh
+++ home.admin/XXsyncScripts.sh
@@ -61,8 +61,8 @@ else
   echo "******************************************"
 fi
 echo "COPYING from GIT-Directory to /home/admin/ .."
-sudo -u admin cp -r -f /home/admin/raspiblitz/home.admin/*.* /home/admin
-sudo -u admin cp -r -f /home/admin/raspiblitz/home.admin/assets/*.* /home/admin/assets
+sudo -u admin cp -r -f /home/admin/raspiblitz/home.admin/* /home/admin
+sudo -u admin cp -r -f /home/admin/raspiblitz/home.admin/assets/* /home/admin/assets
 sudo -u admin chmod +x /home/admin/*.sh
 sudo -u admin chmod +x /home/admin/*.py
 sudo -u admin chmod +x /home/admin/config.scripts/*.sh
