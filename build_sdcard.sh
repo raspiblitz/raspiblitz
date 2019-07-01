d7dcf34244ca46a0039e9e7dda7dde05b48614ff
Christian Rotzoll <christian@geektank.de>
2019-07-02 01:24:06 +0200

    change raspbian base image back to desktop

--- build_sdcard.sh
+++ build_sdcard.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #########################################################################
 # Build your SD card image based on:
-# Raspbian Buster Lite (2019-06-20)
+# Raspbian Buster Desktop (2019-06-20)
 # https://www.raspberrypi.org/downloads/raspbian/
-# SHA256: 9009409a9f969b117602d85d992d90563f181a904bc3812bdd880fc493185234
+# SHA256: 49a6b840ec2cb3e220f9a02bbceed91d21d20a7eeaac32f103923fdbdc9490a9
 ##########################################################################
 # setup fresh SD card with image above - login per SSH and run this script:
 ##########################################################################
