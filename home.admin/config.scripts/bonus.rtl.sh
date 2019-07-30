b4f457e43ca477e88bff20b9d8958cc5ae7255dc
Christian Rotzoll <christian@geektank.de>
2019-07-30 23:57:24 +0200

    fix directory independence on RTL install

--- home.admin/config.scripts/bonus.rtl.sh
+++ home.admin/config.scripts/bonus.rtl.sh
@@ -98,7 +98,7 @@ if [ "$1" = "1" ] || [ "$1" = "on" ]; then
     # download source code and set to tag release
     echo "*** Get the RTL Source Code ***"
     rm -r ./RTL 2>/dev/null
-    git clone https://github.com/ShahanaFarooqui/RTL.git
+    git clone https://github.com/ShahanaFarooqui/RTL.git /home/admin/RTL
     cd RTL
     git reset --hard v0.4.2
     # check if node_modles exists now
