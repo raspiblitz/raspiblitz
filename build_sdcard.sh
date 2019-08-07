47cc2402ef343faa331aed421542ea7662c4f50e
Christian Rotzoll <christian@geektank.de>
2019-08-07 23:11:45 +0200

    #684 ssh comment out LANG LC

--- build_sdcard.sh
+++ build_sdcard.sh
@@ -101,12 +101,17 @@ if [ "${baseImage}" = "raspbian" ] || [ "${baseImage}" = "dietpi" ] ; then
   # https://stackoverflow.com/questions/38188762/generate-all-locales-in-a-docker-image
   echo ""
   echo "*** FIXING LOCALES FOR BUILD ***"
+
   sudo sed -i "s/^# en_US.UTF-8 UTF-8.*/en_US.UTF-8 UTF-8/g" /etc/locale.gen
   sudo sed -i "s/^# en_US ISO-8859-1.*/en_US ISO-8859-1/g" /etc/locale.gen
   sudo locale-gen
   export LANGUAGE=en_GB.UTF-8
   export LANG=en_GB.UTF-8
   export LC_ALL=en_GB.UTF-8
+
+  # https://github.com/rootzoll/raspiblitz/issues/684
+  sudo sed -i "s/^    SendEnv LANG LC.*/#   SendEnv LANG LC_*/g" /etc/ssh/ssh_config
+
 fi
 
 # update debian
