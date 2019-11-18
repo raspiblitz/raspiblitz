1760446534ce3af301b3c9e687a49ead57571204
openoms <oms@tuta.io>
2019-11-18 12:07:42 +0000

    RTL: add Tor support and update

--- home.admin/config.scripts/bonus.rtl.sh
+++ home.admin/config.scripts/bonus.rtl.sh
@@ -32,78 +32,15 @@ if [ "$1" = "1" ] || [ "$1" = "on" ]; then
   isInstalled=$(sudo ls /etc/systemd/system/RTL.service 2>/dev/null | grep -c 'RTL.service')
   if [ ${isInstalled} -eq 0 ]; then
 
-    # determine nodeJS VERSION and DISTRO
-    echo "Detect CPU architecture ..."
-    isARM=$(uname -m | grep -c 'arm')
-    isAARCH64=$(uname -m | grep -c 'aarch64')
-    isX86_64=$(uname -m | grep -c 'x86_64')
-    isX86_32=$(uname -m | grep -c 'i386\|i486\|i586\|i686\|i786')
-    VERSION="v10.16.0"
-    # get checksums from -> https://nodejs.org/dist/vx.y.z/SHASUMS256.txt
-    if [ ${isARM} -eq 1 ] ; then
-      DISTRO="linux-armv7l"
-      CHECKSUM="3a3710722a1ce49b4c72c4af3155041cce3c4f632260ec8533be3fc7fd23f92c"
-    fi
-    if [ ${isAARCH64} -eq 1 ] ; then
-      DISTRO="linux-arm64"
-      CHECKSUM="ae2e74ab2f5dbff96bf0b7d8457004bf3538233916f8834740bbe2d5a35442e5"
-    fi
-    if [ ${isX86_64} -eq 1 ] ; then
-      DISTRO="linux-x64"
-      CHECKSUM="1827f5b99084740234de0c506f4dd2202a696ed60f76059696747c34339b9d48"
-    fi
-    if [ ${isX86_32} -eq 1 ] ; then
-      echo "FAIL: No X86 32bit build available - will abort setup"
-      exit 1
-    fi
-    if [ ${#DISTRO} -eq 0 ]; then
-      echo "FAIL: Was not able to determine architecture"
-      exit 1
-    fi
-    echo "VERSION: ${VERSION}"
-    echo "DISTRO: ${DISTRO}"
-    echo "CHECKSUM: ${CHECKSUM}"
-    echo ""
-
-    # install latest nodejs
-    # https://github.com/nodejs/help/wiki/Installation
-    echo "*** Install NodeJS $VERSION-$DISTRO ***"
-    
-    # download
-    wget https://nodejs.org/dist/$VERSION/node-$VERSION-$DISTRO.tar.xz
-
-    # checksum
-    isChecksumValid=$(sha256sum node-$VERSION-$DISTRO.tar.xz | grep -c "${CHECKSUM}")
-    if [ ${isChecksumValid} -eq 0 ]; then
-      echo "FAIL: The checksum of node-$VERSION-$DISTRO.tar.xz is NOT ${CHECKSUM}"
-      exit 1
-    fi
-    echo "OK CHECKSUM of nodeJS is OK"
-    sleep 3
-
-    # install
-    sudo mkdir -p /usr/local/lib/nodejs
-    sudo tar -xJvf node-$VERSION-$DISTRO.tar.xz -C /usr/local/lib/nodejs 
-    export PATH=/usr/local/lib/nodejs/node-$VERSION-$DISTRO/bin:$PATH
-    sudo ln -s /usr/local/lib/nodejs/node-$VERSION-$DISTRO/bin/node /usr/bin/node
-    sudo ln -s /usr/local/lib/nodejs/node-$VERSION-$DISTRO/bin/npm /usr/bin/npm
-    sudo ln -s /usr/local/lib/nodejs/node-$VERSION-$DISTRO/bin/npx /usr/bin/npx
-    echo ""
-
-    # check if nodeJS was installed
-    nodeJSInstalled=$(node -v | grep -c "v1.")
-    if [ ${nodeJSInstalled} -eq 0 ]; then
-      echo "FAIL - Was not able to install nodeJS"
-      echo "ABORT - RTL install"
-      exit 1
-    fi
+    # check and install NodeJS
+    /home/admin/config.scripts/bonus.nodejs.sh
 
     # download source code and set to tag release
     echo "*** Get the RTL Source Code ***"
     rm -r /home/admin/RTL 2>/dev/null
     git clone https://github.com/ShahanaFarooqui/RTL.git /home/admin/RTL
     cd /home/admin/RTL
-    git reset --hard v0.4.2
+    git reset --hard v0.5.4
     # check if node_modles exists now
     if [ -d "/home/admin/RTL" ]; then
      echo "OK - RTL code copy looks good"
@@ -158,7 +95,40 @@ if [ "$1" = "1" ] || [ "$1" = "on" ]; then
   # setting value in raspi blitz config
   sudo sed -i "s/^rtlWebinterface=.*/rtlWebinterface=on/g" /mnt/hdd/raspiblitz.conf
 
-  echo "needs reboot to activate new setting"
+  # Hidden Service for RTL if Tor is active
+  if [ "${runBehindTor}" = "on" ]; then
+    isRTLTor=$(sudo cat /etc/tor/torrc 2>/dev/null | grep -c 'RTL')
+    if [ ${isRTLTor} -eq 0 ]; then
+      echo "
+# Hidden Service for RTL
+HiddenServiceDir /mnt/hdd/tor/RTL
+HiddenServiceVersion 3
+HiddenServicePort 3000 127.0.0.1:3000
+      " | sudo tee -a /etc/tor/torrc
+  
+      sudo systemctl restart tor
+      sleep 2
+    else
+      echo "The Hidden Service is already installed"
+    fi
+  
+    TOR_ADDRESS=$(sudo cat /mnt/hdd/tor/RTL/hostname)
+    if [ -z "$TOR_ADDRESS" ]; then
+      echo "Waiting for the Hidden Service"
+      sleep 10
+      TOR_ADDRESS=$(sudo cat /mnt/hdd/tor/RTL/hostname)
+        if [ -z "$TOR_ADDRESS" ]; then
+          echo " FAIL - The Hidden Service address could not be found - Tor error?"
+          exit 1
+        fi
+    fi    
+    echo ""
+    echo "***"
+    echo "The Tor Hidden Service address for RTL is:"
+    echo "$TOR_ADDRESS:3000"
+    echo "***"
+    echo "" 
+  fi
   exit 0
 fi
 
@@ -184,6 +154,6 @@ if [ "$1" = "0" ] || [ "$1" = "off" ]; then
   exit 0
 fi
 
-echo "FAIL - Unknown Paramter $1"
-echo "may needs reboot to run normal again"
+echo "FAIL - Unknown Parameter $1"
+echo "may need reboot to run normal again"
 exit 1
