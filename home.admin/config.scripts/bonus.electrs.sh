b1a8e2d4d384df4204112bef1e66f1d3e4555cf7
openoms <oms@tuta.io>
2019-11-25 18:50:15 +0000

    electrs: freeze rust to 1.39.0

--- home.admin/config.scripts/bonus.electrs.sh
+++ home.admin/config.scripts/bonus.electrs.sh
@@ -20,7 +20,6 @@ fi
 echo "making sure services are not running"
 sudo systemctl stop electrs 2>/dev/null
 
-
 # switch on
 if [ "$1" = "1" ] || [ "$1" = "on" ]; then
   echo "*** INSTALL ELECTRS ***"
@@ -47,7 +46,7 @@ if [ "$1" = "1" ] || [ "$1" = "on" ]; then
     echo "Installing Rust"
     echo "***"
     echo ""
-    sudo -u electrs curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sudo -u electrs sh -s -- -y
+    sudo -u electrs curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sudo -u electrs sh -s -- --default-toolchain 1.39.0 -y
     # check Rust version with: $ sudo -u electrs /home/electrs/.cargo/bin/cargo --version
     # workaround to keep Rust at v1.37.0
     # sudo -u electrs /home/electrs/.cargo/bin/rustup install 1.37.0 --force
