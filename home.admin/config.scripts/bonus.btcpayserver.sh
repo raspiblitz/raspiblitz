7a95c311a0fa47b87754a826bfb3fd0044246f9d
openoms <openoms@riseup.net>
2019-12-05 13:37:33 +0000

    add BTCpayServer self-signed certificate option

--- home.admin/config.scripts/bonus.btcpayserver.sh
+++ home.admin/config.scripts/bonus.btcpayserver.sh
@@ -24,29 +24,13 @@ sudo systemctl disable btcpayserver 2>/dev/null
 # switch on
 if [ "$1" = "1" ] || [ "$1" = "on" ]; then
   echo "*** INSTALL BTCPAYSERVER ***"
-  
+
+  # setting up nginx and the SSL certificate    
+  /home/admin/config.scripts/bonus.btcpaysetdomain.sh  
+
   isInstalled=$(sudo ls /etc/systemd/system/btcpayserver.service 2>/dev/null | grep -c 'btcpayserver.service')
   if [ ${isInstalled} -eq 0 ]; then
-    echo ""
-    echo "***"
-    echo "Confirm that the port 80, 443 and 9735 are forwarded to the IP of the RaspiBlitz by pressing [ENTER]" 
-    read key
-    
-    echo ""
-    echo "***"
-    echo "Type the domain/ddns you want to use for BTCPayServer and press [ENTER]"
-    read YOUR_DOMAIN
-    
-    echo ""
-    echo "***"
-    echo "Type an email address that will be used to register the SSL certificate and press [ENTER]"
-    read YOUR_EMAIL
-    
-    echo ""
-    echo "***"
-    echo "Creating the btcpay user"
-    echo "***"
-    echo ""
+
     sudo adduser --disabled-password --gecos "" btcpay 2>/dev/null
     cd /home/btcpay
     
@@ -244,190 +228,8 @@ BTC.lightning=type=lnd-rest;server=https://127.0.0.1:8080/;macaroonfilepath=/hom
 " | sudo -u btcpay tee -a /home/btcpay/.btcpayserver/Main/settings.config
 
     sudo systemctl restart btcpayserver
-    
-    echo ""
-    echo "***"
-    echo "Setting up Nginx and Certbot"
-    echo "***"
-    echo ""
-    
-    # install nginx and certbot
-    sudo apt-get install nginx-full certbot -y
-    
-    sudo ufw allow 80 comment 'btcpayserver TCP'
-    sudo ufw allow 443 comment 'btcpayserver SSL'
-    
-    # get SSL cert
-    sudo systemctl stop certbot 2>/dev/null
-    sudo certbot certonly -a standalone -m $YOUR_EMAIL --agree-tos -d $YOUR_DOMAIN --pre-hook "service nginx stop" --post-hook "service nginx start"
+
   
-    # set nginx
-    sudo rm -f /etc/nginx/sites-enabled/default
-    
-    echo "
-# If we receive X-Forwarded-Proto, pass it through; otherwise, pass along the
-# scheme used to connect to this server
-map \$http_x_forwarded_proto \$proxy_x_forwarded_proto {
-  default \$http_x_forwarded_proto;
-  ''      \$scheme;
-}
-# If we receive X-Forwarded-Port, pass it through; otherwise, pass along the
-# server port the client connected to
-map \$http_x_forwarded_port \$proxy_x_forwarded_port {
-  default \$http_x_forwarded_port;
-  ''      \$server_port;
-}
-# If we receive Upgrade, set Connection to \"upgrade\"; otherwise, delete any
-# Connection header that may have been passed to this server
-map \$http_upgrade \$proxy_connection {
-  default upgrade;
-  '' close;
-}
-# Apply fix for very long server names
-#server_names_hash_bucket_size 128;
-# Prevent Nginx Information Disclosure
-server_tokens off;
-# Default dhparam
-# Set appropriate X-Forwarded-Ssl header
-map \$scheme \$proxy_x_forwarded_ssl {
-  default off;
-  https on;
-}
-
-gzip_types text/plain text/css application/javascript application/json application/x-javascript text/xml application/xml application/xml+rss text/javascript;
-log_format vhost '\$host \$remote_addr - \$remote_user [\$time_local] '
-                 '\"\$request\" \$status \$body_bytes_sent '
-                 '\"\$http_referer\" \"\$http_user_agent\"';
-access_log off;
-# HTTP 1.1 support
-proxy_http_version 1.1;
-proxy_buffering off;
-proxy_set_header Host \$http_host;
-proxy_set_header Upgrade \$http_upgrade;
-proxy_set_header Connection \$proxy_connection;
-proxy_set_header X-Real-IP \$remote_addr;
-proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-proxy_set_header X-Forwarded-Proto \$proxy_x_forwarded_proto;
-proxy_set_header X-Forwarded-Ssl \$proxy_x_forwarded_ssl;
-proxy_set_header X-Forwarded-Port \$proxy_x_forwarded_port;
-# Mitigate httpoxy attack (see README for details)
-proxy_set_header Proxy \"\";
-
-
-server {
-    listen 80 default_server;
-    server_name _;
-    return 301 https://\$host\$request_uri;
-}
-
-server {
-  listen 443 ssl;
-  server_name $YOUR_DOMAIN;
-  ssl on;
-
-  ssl_certificate /etc/letsencrypt/live/$YOUR_DOMAIN/fullchain.pem;
-  ssl_certificate_key /etc/letsencrypt/live/$YOUR_DOMAIN/privkey.pem;
-  ssl_session_timeout 1d;
-  ssl_session_cache shared:SSL:50m;
-  ssl_session_tickets off;
-  ssl_protocols TLSv1.1 TLSv1.2;
-  ssl_ciphers 'ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!3DES:!MD5:!PSK';
-  ssl_prefer_server_ciphers on;
-  ssl_stapling on;
-  ssl_stapling_verify on;
-  ssl_trusted_certificate /etc/letsencrypt/live/$YOUR_DOMAIN/chain.pem;
-
-  location / {
-    proxy_set_header Host \$host;
-    proxy_set_header X-Real-IP \$remote_addr;
-    proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
-    proxy_set_header X-Forwarded-Proto \$scheme;
-    proxy_pass http://localhost:23000;
-  }
-}
-" | sudo tee -a /etc/nginx/sites-available/btcpayserver
-
-    sudo ln -s /etc/nginx/sites-available/btcpayserver /etc/nginx/sites-enabled/ 2>/dev/null
-    
-    sudo systemctl restart nginx
-    
-    echo ""
-    echo "***"
-    echo "Setting up certbot-auto renewal service"
-    echo "***"
-    echo ""
-    
-    sudo rm -f /etc/systemd/system/certbot.timer
-    echo "
-[Unit]
-Description=Certbot-auto renewal service
-
-[Timer]
-OnBootSec=20min
-OnCalendar=*-*-* 4:00:00
-
-[Install]
-WantedBy=timers.target
-" | sudo tee -a /etc/systemd/system/certbot.timer
-
-    sudo rm -f /etc/systemd/system/certbot.service
-    echo "
-[Unit]
-Description=Certbot-auto renewal service
-After=bitcoind.service
-
-[Service]
-WorkingDirectory=/home/admin/
-ExecStart=sudo certbot renew --pre-hook \"service nginx stop\" --post-hook \"service nginx start\"
-
-User=admin
-Group=admin
-Type=simple
-KillMode=process
-TimeoutSec=60
-Restart=always
-RestartSec=60
-" | sudo tee -a /etc/systemd/system/certbot.service
-
-    sudo systemctl enable certbot.timer
-  
-  else 
-    echo "BTCPayServer is already installed."
-    # start service
-    echo "start service"
-    sudo systemctl start btcpayserver 2>/dev/null
-  fi
-
-  # setting value in raspiblitz config
-  sudo sed -i "s/^BTCPayServer=.*/BTCPayServer=on/g" /mnt/hdd/raspiblitz.conf
-
-  # Hidden Service for BTCPay if Tor active
-  source /mnt/hdd/raspiblitz.conf
-  if [ "${runBehindTor}" = "on" ]; then
-    /home/admin/config.scripts/internet.hiddenservice.sh btcpay 80 23000
-
-    TOR_ADDRESS=$(sudo cat /mnt/hdd/tor/btcpay/hostname)
-    if [ -z "$TOR_ADDRESS" ]; then
-      echo "Waiting for the Hidden Service"
-      sleep 10
-      TOR_ADDRESS=$(sudo cat /mnt/hdd/tor/btcpay/hostname)
-      if [ -z "$TOR_ADDRESS" ]; then
-        echo " FAIL - The Hidden Service address could not be found - Tor error?"
-        exit 1
-      fi
-    fi   
-    echo ""
-    echo "***"
-    echo "The Tor Hidden Service address for BTCPayServer is:"
-    echo "$TOR_ADDRESS"
-    echo "***"
-    echo "" 
-  fi
-
-  echo ""
-  echo "Visit your BTCpayServer instance on https://$YOUR_DOMAIN"
-  echo ""
-
   exit 0
 fi
 
--- home.admin/config.scripts/bonus.electrs.sh
+++ home.admin/config.scripts/bonus.electrs.sh
@@ -126,64 +126,8 @@ EOF
             echo "OK"
     fi
 
-    echo ""
-    echo "***"
-    echo "installing Nginx"
-    echo "***"
-    echo ""
-
-    sudo apt-get install -y nginx
-    sudo /etc/init.d/nginx start
-
-    # Only generate if there is none. Or Electrum will not connect if the cert changed.
-    if [ -f /etc/ssl/certs/localhost.crt ] ; then
-        echo "skipping self signed SSL certificate" 
-    else
-        echo ""
-        echo "***"
-        echo "Create a self signed SSL certificate"
-        echo "***"
-        echo ""
-        
-        #https://www.humankode.com/ssl/create-a-selfsigned-certificate-for-nginx-in-5-minutes
-        #https://stackoverflow.com/questions/8075274/is-it-possible-making-openssl-skipping-the-country-common-name-prompts
-
-        echo "
-[req]
-prompt             = no
-default_bits       = 2048
-default_keyfile    = localhost.key
-distinguished_name = req_distinguished_name
-req_extensions     = req_ext
-x509_extensions    = v3_ca
-
-[req_distinguished_name]
-C = US
-ST = California
-L = Los Angeles
-O = Our Company Llc
-#OU = Org Unit Name
-CN = Our Company Llc
-#emailAddress = info@example.com
-
-[req_ext]
-subjectAltName = @alt_names
-
-[v3_ca]
-subjectAltName = @alt_names
-
-[alt_names]
-DNS.1   = localhost
-DNS.2   = 127.0.0.1
-        " | sudo tee /mnt/hdd/electrs/localhost.conf
-
-        cd /mnt/hdd/electrs
-        sudo openssl req -x509 -nodes -days 1825 -newkey rsa:2048 -keyout localhost.key -out localhost.crt -config localhost.conf
-
-        sudo cp localhost.crt /etc/ssl/certs/localhost.crt
-        sudo cp localhost.key /etc/ssl/private/localhost.key
-
-    fi
+    # create a self-signed ssl certificate
+    /home/admin/config.scripts/internet.selfsignedcert.sh
 
     echo ""
     echo "***"
