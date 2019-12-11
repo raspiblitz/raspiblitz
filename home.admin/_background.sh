7f0c8de0a106a725b744ce60d1042344fa49a816
openoms <oms@tuta.io>
2019-12-11 13:44:29 +0000

    electrs: use as address API if active, allow large history

--- home.admin/_background.sh
+++ home.admin/_background.sh
@@ -308,6 +308,16 @@ do
   fi
 
   ###############################
+  # Set the address API use for BTC-RPC-Explorer depending on Electrs status
+  ###############################
+
+  # check every 10 minutes
+  electrsExplorer=$((($counter % 600)+1))
+  if [ ${electrsExplorer} -eq 1 ]; then
+    /home/admin/config.scripts/bonus.electrsexplorer.sh
+  fi
+
+  ###############################
   # Prepare next loop
   ###############################
 
