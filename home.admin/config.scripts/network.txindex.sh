d7d0ad8abc8c3040df7e40ddae5ac1d20dbbdb75
rootzoll <christian@geektank.de>
2019-12-18 22:40:17 +0100

    index status

--- home.admin/config.scripts/network.txindex.sh
+++ home.admin/config.scripts/network.txindex.sh
@@ -13,6 +13,28 @@ source /mnt/hdd/${network}/${network}.conf
 # add default value to bitcoin.conf if needed
 if [ ${#txindex} -eq 0 ]; then
   echo "txindex=0" >> /mnt/hdd/${network}/${network}.conf
+  source /mnt/hdd/${network}/${network}.conf
+fi
+
+if [ "$1" = "status" ]; then
+
+  echo "##### STATUS TXINDEX"
+
+  echo "txindex=${txindex}"
+  if [ ${txindex} -eq 0 ]; then
+    exit 0
+  fi
+
+  # try to gather if still indexing
+  indexedToBlock=$(sudo tail -n 100 /mnt/hdd/litecoin/debug.log | grep "Syncing txindex with block chain from height" | tail -n 1 | cut -d " " -f 9)
+  blockchainHeight=$(sudo -u bitcoin ${network}-cli getblockchaininfo 2>/dev/null | jq -r '.blocks' | sed 's/[^0-9]*//g')
+  if [ ${#indexedToBlock} -eq 0 ] || [ "${indexedToBlock}" = "${blockchainHeight}" ]; then
+    echo "isSynced=1"
+  else
+    echo "isSynced=0"
+  fi
+  exit 0
+
 fi
 
 # switch on
