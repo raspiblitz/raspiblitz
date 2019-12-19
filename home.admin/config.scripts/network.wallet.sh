bf98bd3a4eef4436d2e7caeab95be39f9bde2eb7
rootzoll <christian@geektank.de>
2019-12-19 16:49:00 +0100

    #858 make switch wallet flexible for litecoin

--- home.admin/config.scripts/network.wallet.sh
+++ home.admin/config.scripts/network.wallet.sh
@@ -7,25 +7,26 @@ if [ $# -eq 0 ] || [ "$1" = "-h" ] || [ "$1" = "-help" ]; then
  exit 1
 fi
 
-source /mnt/hdd/bitcoin/bitcoin.conf
+source /mnt/hdd/raspiblitz.conf
+source /mnt/hdd/${network}/${network}.conf
 
 # switch on
 if [ "$1" = "1" ] || [ "$1" = "on" ]; then
   if [ ${disablewallet} == 1 ]; then
-    sudo sed -i "s/^disablewallet=.*/disablewallet=0/g" /mnt/hdd/bitcoin/bitcoin.conf
-    echo "switching the Bitcoin Core wallet on and restarting bitcoind"
-    sudo systemctl restart bitcoind
+    sudo sed -i "s/^disablewallet=.*/disablewallet=0/g" /mnt/hdd/${network}/${network}.conf
+    echo "switching the ${network} core wallet on and restarting ${network}d"
+    sudo systemctl restart ${network}d
     exit 0
   else
-    echo "The Bitcoin Core wallet is already on"    
+    echo "The ${network} core wallet is already on"    
     exit 0
   fi
 fi
 
 # switch off
 if [ "$1" = "0" ] || [ "$1" = "off" ]; then
-  sudo sed -i "s/^disablewallet=.*/disablewallet=1/g" /mnt/hdd/bitcoin/bitcoin.conf
-  sudo systemctl restart bitcoind
+  sudo sed -i "s/^disablewallet=.*/disablewallet=1/g" /mnt/hdd/${network}/${network}.conf
+  sudo systemctl restart ${network}d
   exit 0
 fi
 
