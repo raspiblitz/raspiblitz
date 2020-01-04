32193301cf9351c0925a0dd7eff0000bfbda04af
openoms <openoms@riseup.net>
2020-01-04 00:02:10 +0000

    hiddenservice: extend script

--- home.admin/config.scripts/internet.hiddenservice.sh
+++ home.admin/config.scripts/internet.hiddenservice.sh
@@ -6,7 +6,7 @@
 # command info
 if [ $# -eq 0 ] || [ "$1" = "-h" ] || [ "$1" = "-help" ]; then
  echo "config script to configure a Tor Hidden Service"
- echo "internet.hiddenservice.sh [service] [toPort] [fromPort]"
+ echo "internet.hiddenservice.sh [service] [toPort] [fromPort] [optional-toPort2] [optional-fromPort2]"
  exit 1
 fi
 
@@ -30,21 +30,68 @@ if [ ${#fromPort} -eq 0 ]; then
   exit 1
 fi
 
+# not mandatory
+toPort2="$4"
+
+# needed if $4 is given
+fromPort2="$5"
+if [ ${#toPort2} -gt 0 ]; then
+  if [ ${#fromPort2} -eq 0 ]; then
+    echo "ERROR:the second port to forward from is missing"
+    exit 1
+  fi
+fi
+
 if [ "${runBehindTor}" = "on" ]; then
+  #check if the service is already present
   isHiddenService=$(sudo cat /etc/tor/torrc 2>/dev/null | grep -c $service)
   if [ ${isHiddenService} -eq 0 ]; then
+    #check if the port is already forwarded
+    alreadyThere=$(sudo cat /etc/tor/torrc 2>/dev/null | grep -c 127.0.0.1:$fromPort)
+    if [ ${alreadyThere} -gt 0 ]; then
+      echo "The $fromPort is already forwarded. Check /etc/tor/torrc for the details"
+      exit 1
+    fi
     echo "
 # Hidden Service for $service
 HiddenServiceDir /mnt/hdd/tor/$service
 HiddenServiceVersion 3
 HiddenServicePort $toPort 127.0.0.1:$fromPort" | sudo tee -a /etc/tor/torrc
 
+# check and insert second port pair
+if [ ${#toPort2} -gt 0 ]; then
+  alreadyThere=$(sudo cat /etc/tor/torrc 2>/dev/null | grep -c 127.0.0.1:$fromPort2)
+  if [ ${alreadyThere} -gt 0 ]; then
+    echo "The $fromPort2 is already forwarded. Check /etc/tor/torrc for the details"
+  else
+    echo "HiddenServicePort $toPort2 127.0.0.1:$fromPort2" | sudo tee -a /etc/tor/torrc
+  fi
+fi
+    # restart tor
     echo "Restarting Tor to activate the Hidden Service..."
     sudo systemctl restart tor
     sleep 10
   else
     echo "The Hidden Service is already installed"
   fi
+  # show the Hidden Service address
+  TOR_ADDRESS=$(sudo cat /mnt/hdd/tor/$service/hostname)
+  if [ -z "$TOR_ADDRESS" ]; then
+    echo "Waiting for the Hidden Service"
+    sleep 10
+    TOR_ADDRESS=$(sudo cat /mnt/hdd/tor/$service/hostname)
+    if [ -z "$TOR_ADDRESS" ]; then
+      echo " FAIL - The Hidden Service address could not be found - Tor error?"
+      exit 1
+    fi
+  fi
+  echo "The Tor Hidden Service address for $service is:"
+  echo "$TOR_ADDRESS"
+  echo "use with the port: $toPort"
+  if [ ${#toPort2} -gt 0 ] && [ ${alreadyThere} -eq 0 ]; then
+    echo "or the port: $toPort2"
+  fi
 else
   echo "Tor is not active"
+  exit 1
 fi
