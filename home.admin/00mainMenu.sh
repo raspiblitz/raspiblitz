b166f3c113170fc8597f0b79788de438e322cbb9
rootzoll <christian@geektank.de>
2020-02-18 12:15:05 +0100

    make main menu bigger

--- home.admin/00mainMenu.sh
+++ home.admin/00mainMenu.sh
@@ -15,9 +15,9 @@ source ${configFile}
 localip=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1 -d'/')
 
 # BASIC MENU INFO
-HEIGHT=15
+HEIGHT=17
 WIDTH=64
-CHOICE_HEIGHT=7
+CHOICE_HEIGHT=9
 BACKTITLE="RaspiBlitz"
 TITLE=""
 MENU="Choose one of the following options:"
