90570c0f56981c96295dc893bb442bf90d239cbe
rootzoll <rotzoll@MacBook2020.fritz.box>
2020-03-22 21:41:37 -0700

    #1090 BTCPayServer update

--- home.admin/config.scripts/bonus.btcpayserver.sh
+++ home.admin/config.scripts/bonus.btcpayserver.sh
@@ -236,7 +236,7 @@ EOF
     sudo -u btcpay git clone https://github.com/btcpayserver/btcpayserver.git 2>/dev/null
     cd btcpayserver
     # check https://github.com/btcpayserver/btcpayserver/releases 
-    sudo -u btcpay git reset --hard v1.0.3.156 
+    sudo -u btcpay git reset --hard v1.0.3.164 
     # from the build.sh with path
     sudo -u btcpay /home/btcpay/dotnet/dotnet build -c Release /home/btcpay/btcpayserver/BTCPayServer/BTCPayServer.csproj   
     
