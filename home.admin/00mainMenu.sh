503011dc3b8b5ee548a82a077e11660856ea879f
rootzoll <rotzoll@MacBook2020.fritz.box>
2020-04-18 01:12:12 +0200

    use shutdown script

--- home.admin/00mainMenu.sh
+++ home.admin/00mainMenu.sh
@@ -249,18 +249,7 @@ case $CHOICE in
             echo ""
             echo "LCD turns white when shutdown complete."
             echo "Then wait 5 seconds and disconnect power."
-            echo "-----------------------------------------------"
-            echo "stop lnd - please wait .."
-            sudo systemctl stop lnd
-            echo "stop ${network}d (1) - please wait .."
-            sudo -u bitcoin ${network}-cli stop
-            sleep 10
-            echo "stop ${network}d (2) - please wait .."
-            sudo systemctl stop ${network}d
-            sleep 3
-            sync
-            echo "starting shutdown ..."
-            sudo shutdown now
+            sudo /home/admin/XXshutdown.sh
             exit 0
             ;;
         DELETE)
