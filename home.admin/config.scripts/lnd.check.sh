9c92e1e51fc017b78ae0da158af8764f139509f2
Robert Habermann <mail@rhab.de>
2020-04-30 22:33:16 +0200

    extract update-credentials from lnd.check.sh

--- home.admin/config.scripts/lnd.check.sh
+++ home.admin/config.scripts/lnd.check.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
 if [ $# -eq 0 ]; then
- echo "# script to check LND states"
- echo "# lnd.check.sh basic-setup"
- echo "# lnd.check.sh update-credentials"
- exit 1
+  echo "# script to check LND states"
+  echo "# lnd.check.sh basic-setup"
+  exit 1
 fi
 
 # load raspiblitz conf
-source /home/admin/raspiblitz.info
+source /home/admin/raspiblitz.info # ToDo(frennkie) I don't think this is needed
 source /mnt/hdd/raspiblitz.conf
 
 # check basic LND setup
-if [ "$1" == "basic-setup" ]; then
+if [ "$1" == "basic-setup" ]; then # ToDo(frennkie) If there is only one option.. just run it by default?!
 
-   # check TLS exits
+  # check TLS exits
   tlsExists=$(sudo ls /mnt/hdd/lnd/tls.cert 2>/dev/null | grep -c 'tls.cert')
   if [ ${tlsExists} -gt 0 ]; then
     echo "tls=1"
@@ -22,7 +21,7 @@ if [ "$1" == "basic-setup" ]; then
     echo "tls=0"
     echo "err='tls.cert is missing in /mnt/hdd/lnd'"
   fi
-   # check TLS exits (on SD card for admin)
+  # check TLS exits (on SD card for admin)
   tlsExists=$(sudo ls /home/admin/.lnd/tls.cert 2>/dev/null | grep -c 'tls.cert')
   if [ ${tlsExists} -gt 0 ]; then
     echo "tlsCopy=1"
@@ -67,7 +66,7 @@ if [ "$1" == "basic-setup" ]; then
     echo "configMismatch=0"
     echo "err='lnd.conf is missing for user admin'"
   fi
-  
+
   # get network from config (BLOCKCHAIN)
   lndNetwork=""
   source <(sudo cat /mnt/hdd/lnd/lnd.conf 2>/dev/null | grep 'bitcoin.active' | sed 's/^[a-z]*\./bitcoin_/g')
@@ -175,46 +174,17 @@ if [ "$1" == "basic-setup" ]; then
   fi
   echo "rpcpasscorrect=${rpcpasscorrect}"
 
-# enforce basic LND credentials for users
-elif [ "$1" == "update-credentials" ]; then
-
-  echo "# making sure LND blockchain RPC password is set correct in lnd.conf"
-  source <(sudo cat /mnt/hdd/${network}/${network}.conf 2>/dev/null | grep "rpcpass" | sed 's/^[a-z]*\./lnd/g')
-  if [ ${#rpcpassword} -gt 0 ]; then
-    sudo sed -i "s/^${network}d.rpcpass=.*/${network}d.rpcpass=${rpcpassword}/g" /mnt/hdd/lnd/lnd.conf 2>/dev/null
-  else
-    echo "# WARN: could not get value 'rpcuser' from blockchain conf"
-  fi
-
-  echo "# make sure admin user LND data dirs exist"
-  sudo mkdir -p /home/admin/.lnd
-  sudo mkdir -p /home/admin/.lnd/data
-  sudo mkdir -p /home/admin/.lnd/data/chain
-  sudo mkdir -p /home/admin/.lnd/data/chain/${network}
-  sudo mkdir -p /home/admin/.lnd/data/chain/${network}/${chain}net
-
-  echo "# updating/cleaning admin user LND data"
-  sudo rm -R /home/admin/.lnd 2>/dev/null
-  sudo mkdir -p /home/admin/.lnd/data/chain/${network}/${chain}net 2>/dev/null
-  sudo cp /mnt/hdd/lnd/lnd.conf /home/admin/.lnd/lnd.conf
-  sudo cp /mnt/hdd/lnd/tls.cert /home/admin/.lnd/tls.cert
-  sudo sh -c "cat /mnt/hdd/lnd/data/chain/${network}/${chain}net/admin.macaroon > /home/admin/.lnd/data/chain/${network}/${chain}net/admin.macaroon"
-  sudo chown admin:admin -R /home/admin/.lnd
-
-  echo "# updating/cleaning pi user LND data (just read & invoice)"
-  sudo rm -R /home/pi/.lnd 2>/dev/null
-  sudo mkdir -p /home/pi/.lnd/data/chain/${network}/${chain}net/
-  sudo cp /mnt/hdd/lnd/tls.cert /home/pi/.lnd/tls.cert
-  sudo sh -c "cat /mnt/hdd/lnd/data/chain/${network}/${chain}net/readonly.macaroon > /home/pi/.lnd/data/chain/${network}/${chain}net/readonly.macaroon"
-  sudo sh -c "cat /mnt/hdd/lnd/data/chain/${network}/${chain}net/invoice.macaroon > /home/pi/.lnd/data/chain/${network}/${chain}net/invoice.macaroon"
-  sudo chown pi:pi -R /home/pi/.lnd
-
-  if [ "${LNBits}" = "on" ]; then
-    echo "# updating macaroons for LNBits fresh on start"
-    sudo -u admin /home/admin/config.scripts/bonus.lnbits.sh write-macaroons
-    sudo chown admin:admin -R /mnt/hdd/app-data/LNBits
-  fi
-
 else
   echo "# FAIL: parameter not known"
 fi
+
+# ToDo(frennkie)
+#$: grep lnd.check.sh *
+#00raspiblitz.sh:  source <(sudo /home/admin/config.scripts/lnd.check.sh basic-setup)
+#10setupBlitz.sh:  source <(sudo /home/admin/config.scripts/lnd.check.sh basic-setup)
+#80scanLND.sh:  lndErrorFull=$(sudo /home/admin/config.scripts/lnd.check.sh basic-setup | grep "err=" | tail -1)
+#80scanLND.sh:      sudo /home/admin/config.scripts/lnd.check.sh basic-setup | grep "err="
+#80scanLND.sh:      source <(sudo /home/admin/config.scripts/lnd.check.sh basic-setup)
+#80scanLND.sh:      lndSetupErrorCount=$(sudo /home/admin/config.scripts/lnd.check.sh basic-setup | grep -c "err=")
+
+#70initLND.sh:sudo /home/admin/config.scripts/lnd.check.sh update-credentials
