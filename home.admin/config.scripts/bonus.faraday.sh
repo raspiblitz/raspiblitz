873937d23ec8e1e17941bbe6d7a4f47d46441dce
geco91 <red78@web.de>
2020-05-15 18:34:31 +0200

    fix/test service

--- home.admin/config.scripts/bonus.faraday.sh
+++ home.admin/config.scripts/bonus.faraday.sh
@@ -149,6 +149,8 @@ if [ "${mode}" = "on" ] || [ "${mode}" = "1" ]; then
   # install service
   echo "*** Install systemd ***"
   cat > /mnt/hdd/temp/faraday.service <<EOF
+# test: ${binaryName} or $binaryName
+
 [Unit]
 Description=faraday
 Wants=lnd.service
@@ -156,7 +158,7 @@ After=lnd.service
 
 [Service]
 WorkingDirectory=/home/faraday/
-ExecStart='./faraday --macaroondir=/mnt/hdd/app-data/lnd/data/chain/bitcoin/mainnet --macaroonfile=readonly.macaroon --tlscertpath=/mnt/hdd/app-data/lnd --rpserver=127.0.0.1:10009'
+ExecStart='faraday --macaroondir=/mnt/hdd/app-data/lnd/data/chain/bitcoin/mainnet --macaroonfile=readonly.macaroon --tlscertpath=/mnt/hdd/app-data/lnd/tls.cert --rpcserver=127.0.0.1:10009'
 User=faraday
 Restart=always
 TimeoutSec=120
