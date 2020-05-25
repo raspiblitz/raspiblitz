67cc080c925c8087317b6969ab8264fc2ecea472
rootzoll <rotzoll@MacBook2020.fritz.box>
2020-05-25 03:44:30 +0200

    name ok button

--- home.admin/config.scripts/blitz.ip2tor.py
+++ home.admin/config.scripts/blitz.ip2tor.py
@@ -578,9 +578,10 @@ More information on the service you can find under:
         host['terms_of_service'],
         host['terms_of_service_url'])
 
-        code = d.scrollbox(text, title=host['name'], extra_button=True,  extra_label="AGREE" ,width=60)
-        print(code)
-        sys.exit()
+        code = d.scrollbox(text, title=host['name'], ok_label="Back", extra_button=True,  extra_label="AGREE" ,width=60)
+        
+        # if user AGREED break loop and continue with selected host
+        if code == d.extra_button: break
 
     # if user has canceled
     if host is None:
