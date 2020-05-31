256ac632d4cf5977ccc04708d8edfd63aff864c4
frennkie <mail@rhab.de>
2020-05-31 22:16:51 +0200

    Update XXsyncScripts.sh

--- home.admin/XXsyncScripts.sh
+++ home.admin/XXsyncScripts.sh
@@ -10,7 +10,7 @@
 if [ $# -eq 0 ] || [ "$1" = "-h" ] || [ "$1" = "--help" ] || [ "$1" = "-help" ]; then
   echo "FOR DEVELOPMENT USE ONLY!"
   echo "RaspiBlitz Sync Scripts"
-  echo "XXsyncScripts.sh [-clean] [-install] branch [repo]"
+  echo "XXsyncScripts.sh [-clean] branch [repo]"
   exit 1
 fi
 
