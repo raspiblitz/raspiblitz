6251ca6eea6ba2221a274450746d4eb2259a289a
rootzoll <rotzoll@MacBook2020.fritz.box>
2020-06-01 23:52:26 +0200

    try fix TOR detection

--- home.admin/config.scripts/blitz.subscriptions.py
+++ home.admin/config.scripts/blitz.subscriptions.py
@@ -169,7 +169,7 @@ if tag == "NEW1":
 
     # check if Blitz is running behind TOR
     cfg.reload()
-    if not cfg.run_behind_tor:
+    if not cfg.run_behind_tor.value:
         Dialog(dialog="dialog",autowidgetsize=True).msgbox('''
 The IP2TOR service just makes sense if you run
 your RaspiBlitz behind TOR.
