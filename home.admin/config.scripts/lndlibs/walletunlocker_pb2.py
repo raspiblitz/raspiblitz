f2a26e8eca744c57795a767240af96768fdd3dc6
rootzoll <rotzoll@MacBook2020.fritz.box>
2020-06-23 15:26:24 +0200

    fix dependencies

--- home.admin/config.scripts/lndlibs/walletunlocker_pb2.py
+++ home.admin/config.scripts/lndlibs/walletunlocker_pb2.py
@@ -11,7 +11,7 @@ from google.protobuf import symbol_database as _symbol_database
 _sym_db = _symbol_database.Default()
 
 
-import rpc_pb2 as rpc__pb2
+from . import rpc_pb2 as rpc__pb2
 
 
 DESCRIPTOR = _descriptor.FileDescriptor(
