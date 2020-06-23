73393e694e18d8b8d7a2051e7d6e66fd66d671eb
rootzoll <rotzoll@MacBook2020.fritz.box>
2020-06-23 19:40:34 +0200

    #1253 save networkid to raspiblitz.conf

--- home.admin/config.scripts/bonus.zerotier.sh
+++ home.admin/config.scripts/bonus.zerotier.sh
@@ -3,23 +3,24 @@
 # command info
 if [ $# -eq 0 ] || [ "$1" = "-h" ] || [ "$1" = "-help" ]; then
  echo "config script to install, uninstall ZeroTier"
- echo "bonus.zerotier.sh [on|off|menu]"
+ echo "bonus.zerotier.sh on [?networkid]"
+ echo "bonus.zerotier.sh off"
+ echo "bonus.zerotier.sh menu"
  exit 1
 fi
-
-source /mnt/hdd/raspiblitz.conf
 
 # add default value to raspi config if needed
 if ! grep -Eq "^zerotier=" /mnt/hdd/raspiblitz.conf; then
   echo "zerotier=off" >> /mnt/hdd/raspiblitz.conf
 fi
+source /mnt/hdd/raspiblitz.conf
 
 # show info menu
 if [ "$1" = "menu" ]; then
 
 networkDetails=$(sudo zerotier-cli listnetworks | grep OK)
 
-dialog --title " Info ZeroTier " --msgbox "\n\
+whiptail --title " Info ZeroTier " --msgbox "\n\
 Manage your ZeroTier account at https://my.zerotier.com. Add additional devices
 (desktop/laptop/mobile) to your network so they can communicate.\n\n\
 
@@ -28,7 +29,7 @@ Assigned IP: $(echo $networkDetails | awk '{ print $9}')\n\n\
 
 Find more information on how to get started:\n
 https://zerotier.atlassian.net/wiki/spaces/SD/pages/8454145/Getting+Started+with+ZeroTier
-" 13 100
+" 20 100
   exit 0
 fi
 
@@ -41,36 +42,55 @@ if [ "$1" = "1" ] || [ "$1" = "on" ]; then
     exit 1
   fi
 
-  echo "*** INSTALL ZeroTier ***"
+  networkID=$2
+  if [ ${#networkID} -eq 0 ]; then
+    networkID=$(whiptail --inputbox "\nPlease enter the ZeroTier networkID to connect to:" 10 38 "" --title " Join ZeroTier Network " --backtitle "RaspiBlitz - Settings" 3>&1 1>&2 2>&3)
+    networkID=$(echo "${networkID[0]}")
+    if [ ${#networkID} -eq 0 ]; then
+      echo "# no id entered - cancel"
+      exit 0
+    fi
+  fi
+
+  echo "# *** INSTALL ZeroTier ***"
 
   # Download ZeroTier GPG key and install ZeroTier
   curl -s 'https://raw.githubusercontent.com/zerotier/ZeroTierOne/master/doc/contact%40zerotier.com.gpg' | gpg --import
   if z=$(curl -s 'https://install.zerotier.com/' | gpg); then echo "$z" | sudo bash; fi
 
-  # setting value in raspi blitz config
-  sudo sed -i "s/^zerotier=.*/zerotier=on/g" /mnt/hdd/raspiblitz.conf
-
-  echo ""
   echo "# ZeroTier is now installed on your RaspiBlitz"
-  echo "# Check the ZeroTier menu entry for more details."
-  echo ""
+  echo "# Joining zerotier network: ${networkID}"
 
+  joinOK=$(sudo zerotier-cli join ${networkID} | grep -c '200 join OK')
+  if [ ${joinOK} -eq 1 ]; then
+    echo "# OK - joined"
+
+    # setting value in raspi blitz config
+    sudo sed -i "s/^zerotier=.*/zerotier=${networkID}/g" /mnt/hdd/raspiblitz.conf
+
+  else
+    echo "error='ZweroTier join failed'"
+  fi
   exit 0
 fi
 
 # switch off
 if [ "$1" = "0" ] || [ "$1" = "off" ]; then
 
-  echo "*** REMOVING ZEROTIER ***"
+
+  echo "# *** REMOVING ZEROTIER ***"
+
+
+  # leaving network & deinstall
+  sudo zerotier-cli leave ${zerotier} 1>/dev/null 2>/dev/null
   sudo -u admin sudo apt -y purge zerotier-one
 
   # setting value in raspi blitz config
   sudo sed -i "s/^zerotier=.*/zerotier=off/g" /mnt/hdd/raspiblitz.conf
 
-  echo ""
   echo "# OK, ZeroTier is removed."
   exit 0
 fi
 
-echo "FAIL - Unknown Parameter $1"
+echo "error='unknown parameter'"
 exit 1
