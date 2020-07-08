a13f83681193c8dbfd0559d762447bd5ebcfa824
Dennis Reimann <mail@dennisreimann.de>
2020-07-08 13:55:59 +0200

    Kindle Display: Add jq dependency

--- home.admin/config.scripts/bonus.kindle-display.sh
+++ home.admin/config.scripts/bonus.kindle-display.sh
@@ -29,7 +29,7 @@ if [ "$1" = "1" ] || [ "$1" = "on" ]; then
   isInstalled=$(sudo ls $HOME_DIR 2>/dev/null | grep -c 'kindle-display')
   if [ ${isInstalled} -eq 0 ]; then
     # install dependencies
-    sudo apt install -y firefox-esr pngcrush jo
+    sudo apt install -y firefox-esr pngcrush jo jq
 
     # install nodeJS
     /home/admin/config.scripts/bonus.nodejs.sh on
