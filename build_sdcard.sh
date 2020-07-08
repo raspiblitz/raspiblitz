60336c16824f46d4f96d79f341796b71cdfab414
arno <arnobugs@hotmail.com>
2020-07-08 21:27:55 +0200

    Fix: Insert source file in /home/admin/.bashrc

--- build_sdcard.sh
+++ build_sdcard.sh
@@ -573,7 +573,7 @@ fi
 # "*** LND ***"
 ## based on https://github.com/Stadicus/guides/blob/master/raspibolt/raspibolt_40_lnd.md#lightning-lnd
 ## see LND releases: https://github.com/lightningnetwork/lnd/releases
-lndVersion="0.10.2-beta.rc4"
+lndVersion="0.10.2-beta"
 
 # olaoluwa
 PGPpkeys="https://keybase.io/roasbeef/pgp_keys.asc"
@@ -699,9 +699,12 @@ sudo bash -c "echo 'net.core.wmem_max = 1048576' >> /etc/sysctl.conf"
 # install a command-line fuzzy finder (https://github.com/junegunn/fzf)
 sudo apt -y install fzf
 
+sudo bash -c "echo '' >> /home/admin/.bashrc"
+sudo bash -c "echo '# Raspiblitz' >> /home/admin/.bashrc"
+
 homeFile=/home/admin/.bashrc
 keyBindings="source /usr/share/doc/fzf/examples/key-bindings.bash"
-keyBindingsDone=$(cat ${homeFile}|grep -c ${keyBindings})
+keyBindingsDone=$(cat $homeFile|grep -c "$keyBindings")
 
 if [ ${keyBindingsDone} -eq 0 ]; then
    sudo bash -c "echo 'source /usr/share/doc/fzf/examples/key-bindings.bash' >> /home/admin/.bashrc"
