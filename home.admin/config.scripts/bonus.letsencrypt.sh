da2951023106510f9ba291f09c34c6cca07040be
rootzoll <rotzoll@MacBook2020.fritz.box>
2020-07-14 14:37:20 +0200

    try to fix export

--- home.admin/config.scripts/bonus.letsencrypt.sh
+++ home.admin/config.scripts/bonus.letsencrypt.sh
@@ -174,7 +174,7 @@ elif [ "$1" = "issue-cert" ]; then
   if [ "${dnsservice}" == "duckdns" ]; then
       echo "# preparing DUCKDNS"
       dnsservice="dns_duckdns"
-      envVars="DuckDNS_Token=${token}"
+      export DuckDNS_Token=${token}
   else
     echo "error='not supported dnsservice'"
     exit 1
@@ -182,8 +182,7 @@ elif [ "$1" = "issue-cert" ]; then
 
   # create certicicates
   echo "# creating certs"
-  result=$(export ${envVars} | /home/admin/.acme.sh/acme.sh --home "/home/admin/.acme.sh" --config-home "/mnt/hdd/app-data/letsencrypt" --cert-home "/mnt/hdd/app-data/letsencrypt/certs" --issue --dns ${dnsservice} -d ${FQDN} --keylength ec-256 2>&1)
-  >&2 echo "${result}" 
+  . /home/admin/.acme.sh/acme.sh --home "/home/admin/.acme.sh" --config-home "/mnt/hdd/app-data/letsencrypt" --cert-home "/mnt/hdd/app-data/letsencrypt/certs" --issue --dns ${dnsservice} -d ${FQDN} --keylength ec-256 2>&1
 
   # replace certs for clearnet
   if [ "${options}" == "ip" ] || [ "${options}" == "ip&tor" ]; then
