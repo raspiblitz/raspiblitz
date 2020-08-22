4d8264a4df088ef5be62bc1629ea63f0ffe77808
Chuck Norrison <2964146+ChuckNorrison@users.noreply.github.com>
2020-08-22 13:14:43 +0200

    improve VPN detection
    echos while gathering infos added

--- home.admin/00infoBlitz.sh
+++ home.admin/00infoBlitz.sh
@@ -87,12 +87,17 @@ else
   color_ram=${color_green}
 fi
 
+public_vpn_state="False"
 # get name of active interface with route to internet
 network_active_if=$(ip route get 8.8.8.8 | awk  -- '{print $5}')
-if [ ${network_active_if} != "tun0" ]; then
-  # fallback check if google is unreachable
-  network_active_if=$(ip addr | grep -v "lo:" | grep 'state UP' | tr -d " " | cut -d ":" -f2 | head -n 1)
+if [ "${network_active_if}" != "tun0" ]; then
+  # fallback check if google is unreachable, maybe ip route is the best check
+  network_active_if=$(ip addr | grep -v 'lo:\|tun0' | grep 'state UP' | tr -d " " | cut -d ":" -f2 | head -n 1)
+else
+  # tun0 should be a tunneled VPN setup
+  public_vpn_state="True"
 fi
+echo "INFO: active interface found: ${network_active_if}"
 
 # get network traffic
 # ifconfig does not show eth0 on Armbian or in a VM - get first traffic info
@@ -166,6 +171,7 @@ if [ "${public_port}" = "null" ]; then
     public_port="8333"
   fi
 fi
+echo "INFO: public port: ${public_port}"
 
 # check if RTL web interface is installed
 webinterfaceInfo=""
@@ -184,9 +190,10 @@ networkVersion=$(${network}-cli -datadir=${bitcoin_dir} -version 2>/dev/null | c
 networkInfo=$(${network}-cli -datadir=${bitcoin_dir} getnetworkinfo)
 networkConnections=$(echo ${networkInfo} | jq -r '.connections')
 networkConnectionsInfo="${color_purple}${networkConnections} ${color_gray}connections"
+# remember bad states later
+public_ip_match="True"
 
 if [ "${runBehindTor}" = "on" ]; then
-
   # TOR address
   onionAddress=$(echo ${networkInfo} | jq -r '.localaddresses [0] .address')
   networkConnectionsInfo="${color_purple}${networkConnections} ${color_gray}peers"
@@ -194,16 +201,14 @@ if [ "${runBehindTor}" = "on" ]; then
   public=""
   public_color="${color_green}"
   torInfo="+ Tor"
-
 else
-
   # IP address
   networkConnectionsInfo="${color_purple}${networkConnections} ${color_gray}connections"
   public_addr="${public_ip}:${public_port}"
   # skip netcat public check for tunneled setups (i.e. OpenVPN Gateway)
-  if [ ${network_active_if} != "tun0" ]; then
+  if [ ${public_vpn_state} = "False" ]; then
     public_check=$(nc -z -w6 ${public_ip} ${public_port} 2>/dev/null; echo $?)
-    if [ ${public_check} = "0" ]; then
+    if [ $public_check = "0" ]; then
       public=""
       # only set yellow/normal because netcat can only say that the port is open - not that it points to this device for sure
       public_color="${color_amber}"
@@ -211,10 +216,12 @@ else
       public=""
       public_color="${color_red}"
     fi
+  else
+    public_color="${color_cyan}"
+    echo "INFO: VPN Setup was found"
   fi
 
   # DynDNS
-  public_ip_match="True"
   if [ ${#dynDomain} -gt 0 ]; then
     #check if dyndns resolves to correct IP
     ipOfDynDNS=$(getent hosts ${dynDomain} | awk '{ print $1 }')
@@ -242,8 +249,9 @@ else
 
 fi
 
+###################
 # LIGHTNING NETWORK
-
+###################
 ln_baseInfo="-"
 ln_channelInfo="\n"
 ln_external="\n"
@@ -263,16 +271,19 @@ else
   ln_external=$(echo "${ln_getInfo}" | grep "uris" -A 1 | tr -d '\n' | cut -d '"' -f4)
   ln_tor=$(echo "${ln_external}" | grep -c ".onion")
   if [ ${ln_tor} -eq 1 ]; then
-   ln_publicColor="${color_green}"
+    ln_publicColor="${color_green}"
   else
     # OpenVPN Tunnel without permissions for netcat
-    if [ ${network_active_if} = "tun0" ]; then
+    if [ ${public_vpn_state} = "True" ]; then
+      echo "INFO: check VPN setup for lightning network..." 
       # only colorize if no critical error occured
-      if [ ${public_ip_match} != "False" ]; then
+      if [ ${public_ip_match} = "True" ]; then
         public=""
         public_color="${color_cyan}"
         ln_publicColor="${color_cyan}"
+        echo "INFO: ... succeeded"
       else
+        echo "DEBUG: public ip does not match"
         ln_publicColor="${color_red}"
       fi
     else
@@ -291,12 +302,14 @@ ln_sync=$(echo "${ln_getInfo}" | grep "synced_to_chain" | grep "true" -c)
 ln_version=$(echo "${ln_getInfo}" | jq -r '.version' | cut -d' ' -f1)
 if [ ${ln_sync} -eq 0 ]; then
   if [ ${#ln_getInfo} -eq 0 ]; then
-    ln_baseInfo="${color_red} Not Started | Not Ready Yet"
+     ln_baseInfo="${color_red} Not Started | Not Ready Yet"
   else
+    # parse lnd log
     item=$(sudo -u bitcoin tail -n 100 /mnt/hdd/lnd/logs/${network}/${chain}net/lnd.log 2> /dev/null | grep "Filtering block" | tail -n1 | awk '{print $7}')
     if [ ${#item} -eq 0 ]; then
       item=$(sudo -u bitcoin tail -n 100 /mnt/hdd/lnd/logs/${network}/${chain}net/lnd.log 2> /dev/null | grep "(height" | tail -n1 | awk '{print $10} {print $11} {print $12}' | tr -dc '0-9')
     fi
+    # display blockchain sync state
     total=$(sudo -u bitcoin ${network}-cli -datadir=/home/bitcoin/.${network} getblockchaininfo 2>/dev/null | jq -r '.blocks')
     ln_baseInfo="${color_red} waiting for chain sync"
     if [ ${#item} -gt 0 ]; then
