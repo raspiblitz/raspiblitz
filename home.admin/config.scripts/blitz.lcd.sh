cd8bd78e5d4bb925027a78c2f49b21c14a0f9955
Christian Rotzoll <rotzoll@googlemail.com>
2020-09-14 23:39:06 +0200

    #1418 improve mobile app install & pairing (#1543)

--- home.admin/config.scripts/blitz.lcd.sh
+++ home.admin/config.scripts/blitz.lcd.sh
@@ -207,8 +207,7 @@ if [ "${command}" == "qr-console" ]; then
     exit 1
   fi
 
-  whiptail --title "Get ready" --backtitle "QR-Code in Terminal Window" \
-    --msgbox "Make this terminal window as large as possible - fullscreen would be best. \n\nThe QR-Code might be too large for your display. In that case, shrink the letters by pressing the keys Ctrl and Minus (or Cmd and Minus if you are on a Mac) \n\nPRESS ENTER when you are ready to see the QR-code." 20 60
+  whiptail --title "Get ready" --backtitle "QR-Code in Terminal Window" --msgbox "Make this terminal window as large as possible - fullscreen would be best. \n\nThe QR-Code might be too large for your display. In that case, shrink the letters by pressing the keys Ctrl and Minus (or Cmd and Minus if you are on a Mac) \n\nPRESS ENTER when you are ready to see the QR-code." 15 60
 
   clear
   qrencode -t ANSI256 ${datastring}
