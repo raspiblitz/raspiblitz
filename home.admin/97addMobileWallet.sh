d35d6182afb1a6d6f927f1a9e51c514f5058d558
rootzoll <rotzoll@MacBook2020.local>
2020-09-14 19:53:50 +0200

    remove debug info

--- home.admin/97addMobileWallet.sh
+++ home.admin/97addMobileWallet.sh
@@ -35,8 +35,9 @@ if [ ${isForwarded} -gt 0 ]; then
   aks4IP2TOR=0
 fi
 
-echo "# justLocal(${justLocal})"
-echo "# aks4IP2TOR(${aks4IP2TOR})"
+# echo "# justLocal(${justLocal})"
+# echo "# aks4IP2TOR(${aks4IP2TOR})"
+# read key
 
 # check if dynamic domain is set
 if [ ${justLocal} -eq 1 ]; then
@@ -93,8 +94,9 @@ checkIP2TOR()
     ip2tor="$1"
   fi
 
-  echo "# ip2tor(${ip2tor})"
-  echo "# aks4IP2TOR(${aks4IP2TOR})"
+  #echo "# ip2tor(${ip2tor})"
+  #echo "# aks4IP2TOR(${aks4IP2TOR})"
+  #read key
   
   # if IP2TOR is not already available:
   # and the checks from avove showed there is SSH forwarding / dynDNS
