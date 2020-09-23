fbd3207741f8714d78ba49d1fa9a0605892b2fc9
rootzoll <rotzoll@MacBook2020.fritz.box>
2020-09-23 20:51:30 +0200

    two more spaces

--- home.admin/00mainMenu.sh
+++ home.admin/00mainMenu.sh
@@ -169,7 +169,7 @@ case $CHOICE in
                 echo ""
                 echo -en "Screen is updating in a loop .... press 'x' now to get back to menu."
                 read -n 1 -t 6 keyPressed
-                echo -en "\rGathering information to update info ... please wait.            \n"  
+                echo -en "\rGathering information to update info ... please wait.              \n"  
 
                 # check if user wants to abort session
                 if [ "${keyPressed}" = "x" ]; then
