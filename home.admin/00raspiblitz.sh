1d0276e1d8caaf5e30b4a26e23c01b2ce8946b88
rootzoll <rotzoll@MacBook2020.fritz.box>
2020-10-13 23:21:05 +0200

    edit etxt

--- home.admin/00raspiblitz.sh
+++ home.admin/00raspiblitz.sh
@@ -27,6 +27,7 @@ if [ ${isMounted} -eq 0 ] && [ ${#hddCandidate} -eq 0 ]; then
       echo "- open your VirtualBox GUI and select RaspiBlitzVM"
       echo "- 'change' the 'mass storage' settings"
       echo "- add a second 'Primary Slave' drive to the already existing controller"
+      echo "***********************************************************"
       echo "You can either create a new dynamic VDI with around 900GB or download"
       echo "a VDI with a presynced blockchain to speed up setup. If you dont have 900GB"
       echo "space on your laptop you can store the VDI file on an external drive."
