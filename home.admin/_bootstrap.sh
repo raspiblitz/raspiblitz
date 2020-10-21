d0ee4ff9c36cee5ba5a0109bdfc9c9f27cc72461
Holger Nahrstaedt <holgernahrstaedt@gmx.de>
2020-10-21 12:08:49 +0200

    Fix for issue #1696
    This prevents that the usbdrive is identified as hdd

--- home.admin/_bootstrap.sh
+++ home.admin/_bootstrap.sh
@@ -345,7 +345,11 @@ if [ ${isMounted} -eq 0 ]; then
 
   # temp mount the HDD
   echo "Temp mounting data drive ($hddCandidate)" >> $logFile
-  source <(sudo /home/admin/config.scripts/blitz.datadrive.sh tempmount ${hddPartitionCandidate})
+  if [ "${hddFormat}" != "btrfs" ]; then
+    source <(sudo /home/admin/config.scripts/blitz.datadrive.sh tempmount ${hddPartitionCandidate})
+  else
+    source <(sudo /home/admin/config.scripts/blitz.datadrive.sh tempmount ${hddCandidate})
+  fi
   if [ ${#error} -gt 0 ]; then
     echo "Failed to tempmount the HDD .. awaiting user setup." >> $logFile
     sed -i "s/^state=.*/state=waitsetup/g" ${infoFile}
