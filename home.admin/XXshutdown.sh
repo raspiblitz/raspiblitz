d21f67dbc512e97bc8e2133e9c093427f3dc92a1
rootzoll <rotzoll@MacBook2020.fritz.box>
2020-11-12 15:25:01 +0100

    make sure electrs has more time to shutdown

--- home.admin/XXshutdown.sh
+++ home.admin/XXshutdown.sh
@@ -36,7 +36,8 @@ echo "stop lnd - please wait .."
 sudo systemctl stop lnd 2>/dev/null
 echo "stop ${network}d (1) - please wait .."
 sudo -u bitcoin ${network}-cli stop 2>/dev/null
-sleep 10
+sudo systemctl stop lnd 2>/dev/null
+sudo systemctl stop electrs 2>/dev/null
 echo "stop ${network}d (2) - please wait .."
 sudo systemctl stop ${network}d 2>/dev/null
 sleep 3
