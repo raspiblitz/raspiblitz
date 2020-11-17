8ab51cb804940ca042340b0ed736ff7601760970
rootzoll <rotzoll@MacBook2020.fritz.box>
2020-11-17 21:01:08 +0100

    improve forward command

--- home.admin/_commands.sh
+++ home.admin/_commands.sh
@@ -120,7 +120,7 @@ function status() {
 # command: lnbalance
 # show balance report
 function balance() {
-  echo echo "*** YOUR SATOSHI BALANCES ***"
+  echo "*** YOUR SATOSHI BALANCES ***"
   /home/admin/config.scripts/lnd.balance.sh
 }
 
@@ -134,7 +134,32 @@ function channels() {
 # command: lnfwdreport
 # show forwarding report
 function fwdreport() {
-  /home/admin/config.scripts/lnd.fwdreport.sh
+  _temp="./download/dialog.$$"
+  _error="./.error.out"
+
+  # load raspiblitz config data (with backup from old config)
+  source /home/admin/raspiblitz.info
+  source /mnt/hdd/raspiblitz.conf
+  if [ ${#network} -eq 0 ]; then network=`cat .network`; fi
+  if [ ${#network} -eq 0 ]; then network="bitcoin"; fi
+  if [ ${#chain} -eq 0 ]; then
+    echo "gathering chain info ... please wait"
+    chain=$(${network}-cli getblockchaininfo | jq -r '.chain')
+  fi
+
+  # let user enter a <pubkey>@host
+  l1="Enter the number of days to query:"
+  l2="e.g. '7' will query the last 7 days"
+  dialog --title "Create a forwarding event report" \
+--backtitle "Lightning ( ${network} | ${chain} )" \
+--inputbox "$l1\n$l2" 10 60 7 2>$_temp
+_input=$(cat $_temp | xargs )
+  shred -u $_temp
+  if [ ${#_input} -eq 0 ]; then
+    exit 1
+  fi
+
+  /home/admin/config.scripts/lnd.fwdreport.sh -n ${chain}net -c ${network} -- ${_input}
 }
 
 # command: bos
