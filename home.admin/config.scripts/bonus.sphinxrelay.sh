0c5b549fb042854daa99958df81f65a3e816ca21
Christian Rotzoll <rotzoll@googlemail.com>
2020-11-23 21:27:39 +0100

    #1758 improve warning when Sphinx IP2TOR is not ready yet (#1796)

--- home.admin/config.scripts/bonus.sphinxrelay.sh
+++ home.admin/config.scripts/bonus.sphinxrelay.sh
@@ -21,8 +21,15 @@ if [ "$1" = "menu" ]; then
   echo "# collecting status info ... (please wait)"
   source <(sudo /home/admin/config.scripts/bonus.sphinxrelay.sh status)
 
+  # display possible problems with IP2TOR setup
   if [ ${#ip2torWarn} -gt 0 ]; then
-    whiptail --title " Warning " --msgbox "Your IP2TOR+LetsEncrypt may have problems:\n${ip2torWarn}\n\nCheck if locally responding: http://${localIP}:${httpPort}\n\nCheck if service is reachable over Tor:\n${toraddress}" 14 72
+    whiptail --title " Warning " \
+    --yes-button "Back" \
+    --no-button "Continue Anyway" \
+    --yesno "Your IP2TOR+LetsEncrypt may have problems:\n${ip2torWarn}\n\nCheck if locally responding: http://${localIP}:${httpPort}\n\nCheck if service is reachable over Tor:\n${toraddress}" 14 72
+    if [ "$?" != "1" ]; then
+      exit 0
+	  fi
   fi
 
   extraPairInfo=""
