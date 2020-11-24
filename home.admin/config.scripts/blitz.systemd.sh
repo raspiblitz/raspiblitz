2bd45debfd99e040bc758fc029e72a9f9913744d
rootzoll <rotzoll@MacBook2020.fritz.box>
2020-11-24 15:41:48 +0100

    remove restart

--- home.admin/config.scripts/blitz.systemd.sh
+++ home.admin/config.scripts/blitz.systemd.sh
@@ -11,14 +11,12 @@ fi
 
 # edit of sshd service file
 if [ "${1}" == "update-sshd" ]; then
-  echo "# blitz.systemd.sh -> update sshd service config"
+  echo "# DEACTIVATED: blitz.systemd.sh -> update sshd service config"
+  # echo "# blitz.systemd.sh -> update sshd service config"
   # make sure its updated to wait for HDD mount before start
   # https://github.com/rootzoll/raspiblitz/issues/1785#issuecomment-730476628
-  sudo sed -i "s/^After=.*/After=network.target auditd.service mnt-hdd.mount/g" /etc/systemd/system/sshd.service
-  # make sure sto restart service
-  sudo systemctl daemon-reload
-  sudo systemctl restart sshd
-  echo "# OK sshd config edit done and service restarted"
+  # sudo sed -i "s/^After=.*/After=network.target auditd.service mnt-hdd.mount/g" /etc/systemd/system/sshd.service
+  # echo "# OK sshd config edit done and service restarted"
   exit 0
 fi
 
