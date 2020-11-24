b085e7a392288ee8827b535c7f619fa32e68120a
rootzoll <rotzoll@MacBook2020.fritz.box>
2020-11-25 00:12:12 +0100

    #1798 copy ssh data instead of linking

--- home.admin/40addHDD.sh
+++ home.admin/40addHDD.sh
@@ -116,12 +116,14 @@ fi
 
 # link ssh directory from SD card to HDD
 echo "# --> SSH key settings"
-echo "# moving SSH pub keys to HDD"
+echo "# copying SSH pub keys to HDD"
 sudo cp -r /etc/ssh /mnt/hdd/ssh
-sudo rm -rf /etc/ssh
-sudo ln -s /mnt/hdd/ssh /etc/ssh
+# just copy dont link anymore
+# see: https://github.com/rootzoll/raspiblitz/issues/1798
+#sudo rm -rf /etc/ssh
+#sudo ln -s /mnt/hdd/ssh /etc/ssh
+#sudo /home/admin/config.scripts/blitz.systemd.sh update-sshd
 echo "# OK"
-sudo /home/admin/config.scripts/blitz.systemd.sh update-sshd
 echo ""
 
 # set SetupState
--- home.admin/config.scripts/blitz.systemd.sh
+++ home.admin/config.scripts/blitz.systemd.sh
@@ -3,21 +3,9 @@
 # command info
 if [ "$1" = "-h" ] || [ "$1" = "-help" ]; then
  echo "additional systemd services"
- echo "blitz.systemd.sh update-sshd"
  echo "blitz.systemd.sh log blockchain STARTED"
  echo "blitz.systemd.sh log lightning STARTED"
  exit 1
-fi
-
-# edit of sshd service file
-if [ "${1}" == "update-sshd" ]; then
-  echo "# DEACTIVATED: blitz.systemd.sh -> update sshd service config"
-  # echo "# blitz.systemd.sh -> update sshd service config"
-  # make sure its updated to wait for HDD mount before start
-  # https://github.com/rootzoll/raspiblitz/issues/1785#issuecomment-730476628
-  # sudo sed -i "s/^After=.*/After=network.target auditd.service mnt-hdd.mount/g" /etc/systemd/system/sshd.service
-  # echo "# OK sshd config edit done and service restarted"
-  exit 0
 fi
 
 # check parameter
