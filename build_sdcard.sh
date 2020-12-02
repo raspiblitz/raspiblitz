278070106078fa4736a0dbdf76041d32f4980155
openoms <oms@tuta.io>
2020-12-02 09:39:49 +0000

    don't enable I2C, SPI and UART ports by default

--- build_sdcard.sh
+++ build_sdcard.sh
@@ -966,8 +966,30 @@ if [ "${lcdInstalled}" == "true" ]; then
     sudo ./LCD35-show
   elif [ "${baseImage}" = "raspbian64" ]; then
     cd /home/admin/wavesharelcd-64bit-rpi
-    chmod +x install.sh
-    sudo bash install.sh
+    # from https://github.com/tux1c/wavesharelcd-64bit-rpi/blob/master/install.sh
+    # prepare X11
+    rm -rf /etc/X11/xorg.conf.d/40-libinput.conf
+    mkdir -p /etc/X11/xorg.conf.d
+    cp -rf ./99-calibration.conf  /etc/X11/xorg.conf.d/99-calibration.conf
+    cp -rf ./99-fbturbo.conf  /etc/X11/xorg.conf.d/99-fbturbo.conf
+
+    # load module on boot
+    cp ./waveshare35a.dtbo /boot/overlays/
+    echo "hdmi_force_hotplug=1" >> /boot/config.txt 
+    # don't enable I2C, SPI and UART ports by default
+    # echo "dtparam=i2c_arm=on" >> /boot/config.txt
+    # echo "dtparam=spi=on" >> /boot/config.txt
+    # echo "enable_uart=1" >> /boot/config.txt
+    echo "dtoverlay=waveshare35a:rotate=90" >> /boot/config.txt
+    cp ./cmdline.txt /boot/
+
+    # touch screen calibration
+    apt-get install xserver-xorg-input-evdev
+    cp -rf /usr/share/X11/xorg.conf.d/10-evdev.conf /usr/share/X11/xorg.conf.d/45-evdev.conf
+
+    echo "reboot now"
+    reboot
+
   else
     echo "Use 'sudo reboot' to restart manually."
   fi
