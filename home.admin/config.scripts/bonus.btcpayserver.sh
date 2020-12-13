15175dc431a9dacaff1130b67348c3b293fd2b0b
openoms <oms@tuta.io>
2020-12-13 19:46:56 +0000

    btcpay update to v1.0.6.3, NBXplorer to v2.1.47

--- home.admin/config.scripts/bonus.btcpayserver.sh
+++ home.admin/config.scripts/bonus.btcpayserver.sh
@@ -3,9 +3,9 @@
 # Based on: https://gist.github.com/normandmickey/3f10fc077d15345fb469034e3697d0d0
 
 # https://github.com/dgarage/NBXplorer/releases
-NBXplorerVersion="v2.1.46"
+NBXplorerVersion="v2.1.47"
 # https://github.com/btcpayserver/btcpayserver/releases
-BTCPayVersion="v1.0.5.9"
+BTCPayVersion="v1.0.6.3"
 
 # command info
 if [ $# -eq 0 ] || [ "$1" = "-h" ] || [ "$1" = "-help" ]; then
