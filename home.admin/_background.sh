5043845974ab8a0afbb1889837f3b9ebc8618b74
rootzoll <christian@fulmo.org>
2020-12-19 17:04:13 +0100

    background sny monitoring

--- home.admin/_background.sh
+++ home.admin/_background.sh
@@ -177,6 +177,21 @@ do
   fi
 
   ###############################
+  # Blockchain Sync Monitor
+  ###############################
+
+  # check every 1min
+  recheckSync=$(($counter % 60))
+  if [ ${recheckSync} -eq 1 ]; then
+    source <(/home/admin/config.scripts/network.monitor.sh peer-status)
+    echo "Blockchain Sync Monitoring: peers=${peers}"
+    if [ "${peers}" == "0" ]; then
+      echo "Blockchain Sync Monitoring: ZERO PEERS DETECTED .. doing out-of-band kickstart"
+      /home/admin/config.scripts/network.monitor.sh peer-kickstart
+    fi
+  fi
+
+  ###############################
   # BlitzTUI Monitoring
   ###############################
 
