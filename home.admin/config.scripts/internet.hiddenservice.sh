fadc1d45c99cf83f658deff2cb7f41e5219e629d
rootzoll <christian@fulmo.org>
2020-12-21 21:38:47 +0100

    edit remove double lines

--- home.admin/config.scripts/internet.hiddenservice.sh
+++ home.admin/config.scripts/internet.hiddenservice.sh
@@ -24,14 +24,16 @@ if [ "$1" == "off" ]; then
 
   # remove service paragraph
   sudo sed -i "/# Hidden Service for ${service}/,/^\s*$/{d}" /etc/tor/torrc
-  # remove double empty lines
-  sudo touch /mnt/hdd/temp/tmp
-  sudo chmod 777 /mnt/hdd/temp/tmp
-  sudo awk 'NF > 0 {blank=0} NF == 0 {blank++} blank < 2' /etc/tor/torrc > /mnt/hdd/temp/tmp && sudo mv /mnt/hdd/temp/tmp /etc/tor/torrc
 
-  echo "# OK service is removed - restarting TOR ..."
+  # remove double empty lines
+  sudo cp /etc/tor/torrc /mnt/hdd/temp/tmp
+  sudo chmod 777 /mnt/hdd/temp/tmp
+  sudo awk 'NF > 0 {blank=0} NF == 0 {blank++} blank < 2' /etc/tor/torrc > /mnt/hdd/temp/tmp
+  sudo mv /mnt/hdd/temp/tmp /etc/tor/torrc
   sudo chmod 644 /etc/tor/torrc
   sudo chown bitcoin:bitcoin /etc/tor/torrc
+
+  echo "# OK service is removed - restarting TOR ..."
   sudo systemctl restart tor
   sleep 10
   echo "# Done"
