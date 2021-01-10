4ca1ae2d6aa76125cb8c33d76de46c0a61eb4fe1
NilByte <36365571+NilByte@users.noreply.github.com>
2021-01-10 01:10:54 +0100

    Update 00mainMenu.sh
    Also listing bitcoind command line help is appropriate.

--- home.admin/00mainMenu.sh
+++ home.admin/00mainMenu.sh
@@ -382,6 +382,7 @@ case $CHOICE in
             echo "* RaspiBlitz Commandline"
             echo "* Here be dragons .. have fun :)"
             echo "***********************************"
+	    echo "Bitcoin command line options: bitcoind -h"
             echo "LND command line options: lncli -h"
             echo "Back to main menu use command: raspiblitz"
             echo
