2d0614caab1bfab6e92f6880583f201ca34fd6d0
adaleben <69700936+Ben10Brasileiro@users.noreply.github.com>
2021-01-17 05:58:25 +0100

    Update XXdebugLogs.sh

--- home.admin/XXdebugLogs.sh
+++ home.admin/XXdebugLogs.sh
@@ -35,40 +35,12 @@ echo "***************************************************************"
 echo "* --------------- BEGIN RASPIBLITZ DEBUG LOGS --------------- *"
 echo "***************************************************************"
 echo ""
+
 echo "blitzversion: ${codeVersion}"
 echo "chainnetwork: ${network} / ${chain}"
-uptime
 echo ""
 
-# get HDD/SSD info
-source <(sudo /home/admin/config.scripts/blitz.datadrive.sh status)
-hdd="${hddUsedInfo}"
-
-# get memory
-ram_avail=$(free -m | grep Mem | awk '{ print $7 }')
-ram=$(printf "%sM / %sM" "${ram_avail}" "$(free -m | grep Mem | awk '{ print $2 }')")
-
-# get uptime & load
-## get uptime and current date & time
-uptime=$(uptime --pretty)
-datetime=$(date -R)
-load=$(w | head -n 1 | cut -d 'v' -f2 | cut -d ':' -f2)
-
-# get CPU temp - no measurement in a VM
-cpu=0
-if [ -d "/sys/class/thermal/thermal_zone0/" ]; then
-  cpu=$(cat /sys/class/thermal/thermal_zone0/temp)
-fi
-tempC=$((cpu/1000))
-tempF=$(((tempC * 18 + 325) / 10))
-
-echo "Date=${datetime}"
-echo "CPU load=${load##up*,  }"
-echo "CPU temperature=${tempC}" "${tempF}"
-echo "Mem usage=${ram}"
-echo "HDD usage=${hdd}"
 echo ""
-
 echo "*** BLOCKCHAIN SYSTEMD STATUS ***"
 sudo systemctl status ${network}d -n2 --no-pager
 echo ""
@@ -87,6 +59,7 @@ echo "sudo tail -n 20 /mnt/hdd/${network}${pathAdd}/debug.log"
 sudo tail -n 20 /mnt/hdd/${network}${pathAdd}/debug.log
 echo ""
 
+echo ""
 echo "*** LND SYSTEMD STATUS ***"
 sudo systemctl status lnd -n2 --no-pager
 echo ""
@@ -101,6 +74,7 @@ echo "sudo tail -n 30 /mnt/hdd/lnd/logs/${network}/${chain}net/lnd.log"
 sudo tail -n 30 /mnt/hdd/lnd/logs/${network}/${chain}net/lnd.log
 echo ""
 
+echo ""
 echo "*** LAST NGINX LOGS ***"
 echo "sudo journalctl -u nginx -b --no-pager -n20"
 sudo journalctl -u nginx -b --no-pager -n20
@@ -108,138 +82,160 @@ echo "--> CHECK CONFIG: sudo nginx -t"
 sudo nginx -t
 echo ""
 
+
 if [ "${touchscreen}" = "1" ]; then
-if [ "${touchscreen}" = "0" ]; then
-  echo "- TOUCHSCREEN is OFF by config"
-else
   echo ""
   echo "*** LAST 20 TOUCHSCREEN LOGS ***"
   echo "sudo tail -n 20 /home/pi/.cache/lxsession/LXDE-pi/run.log"
   sudo tail -n 20 /home/pi/.cache/lxsession/LXDE-pi/run.log
   echo ""
 else
+  echo ""
   echo "- TOUCHSCREEN is OFF by config"
+  echo ""
 fi
 
 if [ "${loop}" = "on" ]; then
-if [ "${loop}" = "off" ]; then
-  echo "- Loop is OFF by config"
-else
   echo ""
   echo "*** LAST 20 LOOP LOGS ***"
   echo "sudo journalctl -u loopd -b --no-pager -n20"
   sudo journalctl -u loopd -b --no-pager -n20
   echo ""
 else
+  echo ""
   echo "- Loop is OFF by config"
+  echo ""
 fi
 
 if [ "${rtlWebinterface}" = "on" ]; then
-if [ "${rtlWebinterface}" = "off" ]; then
-  echo "- RTL is OFF by config"
-else
   echo ""
   echo "*** LAST 20 RTL LOGS ***"
   echo "sudo journalctl -u RTL -b --no-pager -n20"
   sudo journalctl -u RTL -b --no-pager -n20
   echo ""
 else
+  echo ""
   echo "- RTL is OFF by config"
+  echo ""
 fi
 
 if [ "${ElectRS}" = "on" ]; then
-if [ "${ElectRS}" = "off" ]; then
-  echo "- Electrum Rust Server is OFF by config"
-else
   echo ""
   echo "*** LAST 20 ElectRS LOGS ***"
   echo "sudo journalctl -u electrs -b --no-pager -n20"
-@@ -117,68 +119,66 @@ if [ "${ElectRS}" = "on" ]; then
+  sudo journalctl -u electrs -b --no-pager -n20
+  echo ""
   echo "*** ElectRS Status ***"
   sudo /home/admin/config.scripts/bonus.electrs.sh status
   echo ""
 else
+  echo ""
   echo "- Electrum Rust Server is OFF by config"
+  echo ""
 fi
 
 if [ "${BTCPayServer}" = "on" ]; then
-if [ "${BTCPayServer}" = "off" ]; then
-  echo "- BTCPayServer is OFF by config"
-else
   echo ""
   echo "*** LAST 20 BTCPayServer LOGS ***"
   echo "sudo journalctl -u btcpayserver -b --no-pager -n20"
   sudo journalctl -u btcpayserver -b --no-pager -n20
   echo ""
 else
+  echo ""
   echo "- BTCPayServer is OFF by config"
+  echo ""
 fi
 
 if [ "${LNBits}" = "on" ]; then
-if [ "${LNBits}" = "off" ]; then
-  echo "- LNbits is OFF by config"
-else
   echo ""
   echo "*** LAST 20 LNbits LOGS ***"
   echo "sudo journalctl -u lnbits -b --no-pager -n20"
   sudo journalctl -u lnbits -b --no-pager -n20
   echo ""
 else
+  echo ""
   echo "- LNbits is OFF by config"
+  echo ""
 fi
 
 if [ "${thunderhub}" = "on" ]; then
-if [ "${thunderhub}" = "off" ]; then
-  echo "- Thunderhub is OFF by config"
-else
   echo ""
   echo "*** LAST 20 Thunderhub LOGS ***"
   echo "sudo journalctl -u thunderhub -b --no-pager -n20"
   sudo journalctl -u thunderhub -b --no-pager -n20
   echo ""
 else
+  echo ""
   echo "- Thunderhub is OFF by config"
+  echo ""
 fi
 
 if [ "${specter}" = "on" ]; then
-if [ "${specter}" = "off" ]; then
-  echo "- SPECTER is OFF by config"
-else
   echo ""
   echo "*** LAST 20 SPECTER LOGS ***"
   echo "sudo journalctl -u cryptoadvance-specter -b --no-pager -n20"
   sudo journalctl -u cryptoadvance-specter -b --no-pager -n20
   echo ""
 else
+  echo ""
   echo "- SPECTER is OFF by config"
+  echo ""
 fi
 
 if [ "${sphinxrelay}" = "on" ]; then
-if [ "${sphinxrelay}" = "off" ]; then
-  echo "- SPHINX is OFF by config"
-else
   echo ""
   echo "*** LAST 20 SPHINX LOGS ***"
   echo "sudo journalctl -u sphinxrelay -b --no-pager -n20"
   sudo journalctl -u sphinxrelay -b --no-pager -n20
   echo ""
 else
+  echo ""
   echo "- SPHINX is OFF by config"
+  echo ""
 fi
 
 echo ""
-echo "*** MOUNTED DRIVES ***"
-df -T
-df -T -h
-echo ""
-
-echo ""
-echo "*** WIFI Info ***"
-sudo /home/admin/config.scripts/internet.wifi.sh status
 echo "*** NETWORK Info ***"
 sudo /home/admin/config.scripts/internet.sh status | grep 'network_device\|localip\|dhcp'
 echo ""
 
+echo ""
+echo "*** MOUNTED DRIVES ***"
+df -T -h
+echo ""
+
+
+# get HDD/SSD info
+source <(sudo /home/admin/config.scripts/blitz.datadrive.sh status)
+hdd="${hddUsedInfo}"
+
+# get memory
+ram_avail=$(free -m | grep Mem | awk '{ print $7 }')
+ram=$(printf "%sM / %sM" "${ram_avail}" "$(free -m | grep Mem | awk '{ print $2 }')")
+
+# get uptime & load
+## get uptime and current date & time
+uptime=$(uptime --pretty)
+datetime=$(date -R)
+load=$(w | head -n 1 | cut -d 'v' -f2 | cut -d ':' -f2)
+# get CPU temp - no measurement in a VM
+cpu=0
+if [ -d "/sys/class/thermal/thermal_zone0/" ]; then
+  cpu=$(cat /sys/class/thermal/thermal_zone0/temp)
+fi
+tempC=$((cpu/1000))
+tempF=$(((tempC * 18 + 325) / 10))
+
+echo ""
+echo "*** HARDWARE STATE ***"
+echo "date=${datetime}"
+echo "cpuLoad=${load##up*,  }"
+echo "cpuTemp= ${tempC}°C" "${tempF}°F"
+echo "freeMem= ${ram}"
+echo "usedHdd= ${hdd}"
+echo ""
+
+echo ""
 echo "*** HARDWARE TEST RESULTS ***"
 showImproveInfo=0
 if [ ${#undervoltageReports} -gt 0 ]; then
@@ -264,10 +260,12 @@ if [ ${showImproveInfo} -gt 0 ]; then
 fi
 echo ""
 
+echo ""
 echo "*** SYSTEM STATUS (can take some seconds to gather) ***"
 sudo /home/admin/config.scripts/blitz.statusscan.sh
 echo ""
 
+echo ""
 echo "***************************************************************"
 echo "* ---------------- END RASPIBLITZ DEBUG LOGS ---------------- *"
 echo "***************************************************************"
