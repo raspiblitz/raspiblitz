8e49984e7ddbff47d2fc5d97650f3e4be6a5f42d
rootzoll <christian@geektank.de>
2021-03-22 23:43:37 +0100

    adapt dialog UI

--- home.admin/config.scripts/bonus.whitepaper.sh
+++ home.admin/config.scripts/bonus.whitepaper.sh
@@ -28,7 +28,7 @@ This service downloads Satoshi's Whitepaper directly from the blockchain.
 When enabled, the Whitepaper is downloaded to $DownloadPath$WhitepaperFilename
 When disabled, the Whitepaper is deleted from $DownloadPath$WhitepaperFilename
 Also, use the command 'whitepaper' from the command line to download the whitepaper directly
-" 10 75
+" 11 78
   exit 0
 fi
 
