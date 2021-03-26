b761e830d33c0c09177d747adba7db2737f8995a
/rootzoll <christian@geektank.de>
2021-03-26 03:17:13 +0100

    1341 acme over tor by curl default proxy (#2110)

--- home.admin/config.scripts/internet.tor.sh
+++ home.admin/config.scripts/internet.tor.sh
@@ -173,6 +173,16 @@ fi
 if [ "$1" = "1" ] || [ "$1" = "on" ]; then
   echo "# switching Tor ON"
 
+  # *** CURL TOR PROXY ***
+  echo "socks5-hostname localhost:9050" > .curlrc.tmp
+  sudo cp ./.curlrc.tmp /root/.curlrc
+  sudo chown root:root /home/admin/.curlrc
+  sudo cp ./.curlrc.tmp /home/pi/.curlrc
+  sudo chown pi:pi /home/pi/.curlrc
+  sudo cp ./.curlrc.tmp /home/admin/.curlrc
+  sudo chown admin:admin /home/admin/.curlrc
+  rm .curlrc.tmp
+
   # make sure the network was set (by sourcing raspiblitz.conf)
   if [ ${#network} -eq 0 ]; then
     echo "!! FAIL - unknown network due to missing /mnt/hdd/raspiblitz.conf"
@@ -383,6 +393,11 @@ if [ "$1" = "0" ] || [ "$1" = "off" ]; then
 
   # setting value in raspi blitz config
   sudo sed -i "s/^runBehindTor=.*/runBehindTor=off/g" /mnt/hdd/raspiblitz.conf
+
+  # *** CURL TOR PROXY ***
+  sudo rm /root/.curlrc
+  sudo rm /home/pi/.curlrc
+  sudo rm /home/admin/.curlrc
 
   # disable TOR service
   echo "# *** Disable Tor service ***"
