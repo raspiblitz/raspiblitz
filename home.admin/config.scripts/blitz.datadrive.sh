63d3ac751a9691f80600466741ef21c2f69803b8
rootzoll <christian@geektank.de>
2021-03-26 18:33:44 +0100

    move code block

--- home.admin/config.scripts/blitz.datadrive.sh
+++ home.admin/config.scripts/blitz.datadrive.sh
@@ -160,13 +160,6 @@ if [ "$1" = "status" ]; then
     echo "hddGigaBytes=${hddGigaBytes}"
 
     echo "hddPartitionCandidate='${hddDataPartition}'"
-
-    # determine UAS string --> https://www.pragmaticlinux.com/2021/03/fix-for-getting-your-ssd-working-via-usb-3-on-your-raspberry-pi/
-    rawdata=$(sudo udevadm test /block/${hdd} 2> /dev/null)
-    vendorID=$(echo "${rawdata}" | grep 'ID_VENDOR_ID' | sed 's/ID_VENDOR_ID=//')
-    modelID=$(echo "${rawdata}" | grep 'ID_MODEL_ID' | sed 's/ID_MODEL_ID=//')
-    echo "hddVendorID=${vendorID}"
-    echo "hddModelID=${modelID}"
     
     if [ ${#hddDataPartition} -gt 0 ]; then
 
@@ -317,6 +310,15 @@ if [ "$1" = "status" ]; then
     fi
     echo "hddUsedInfo='${hddUsedInfo}'"
 
+  fi
+
+  # determine UAS string --> https://www.pragmaticlinux.com/2021/03/fix-for-getting-your-ssd-working-via-usb-3-on-your-raspberry-pi/
+  if [ ${#hdd} -gt 0 ]; then
+    rawdata=$(sudo udevadm test /block/${hdd} 2> /dev/null)
+    vendorID=$(echo "${rawdata}" | grep 'ID_VENDOR_ID' | sed 's/ID_VENDOR_ID=//')
+    modelID=$(echo "${rawdata}" | grep 'ID_MODEL_ID' | sed 's/ID_MODEL_ID=//')
+    echo "hddVendorID=${vendorID}"
+    echo "hddModelID=${modelID}"
   fi
 
   echo
