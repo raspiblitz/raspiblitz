145baf993f5e13b7d05304771100528b2fef563a
nyxnor <69700936+nyxnor@users.noreply.github.com>
2021-04-27 02:34:39 +0000

    make the commands easier to read, clean blank lines, add bitcoin.txt with ASCII format

--- home.admin/config.scripts/bonus.whitepaper.sh
+++ home.admin/config.scripts/bonus.whitepaper.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 WhitepaperVersion="v0.1"
-DownloadPath="/home/admin/"
-WhitepaperFilename="bitcoin.pdf"
+DownloadPath="/home/admin"
+WhitepaperFilename="/bitcoin.pdf"
 
 # command info
 if [ $# -eq 0 ] || [ "$1" = "-h" ] || [ "$1" = "-help" ]; then
@@ -28,27 +28,30 @@ This service downloads Satoshi's Whitepaper directly from the blockchain.
 When enabled, the Whitepaper is downloaded to $DownloadPath$WhitepaperFilename
 When disabled, the Whitepaper is deleted from $DownloadPath$WhitepaperFilename
 Also, use the command 'whitepaper' from the command line to download the whitepaper directly.
-" 11 78
+From the user home folder, call an editor 'nano bitcoin.pdf' to enter the bitcoin.txt or use 'less bitcoin.txt' command, both to view the file in ASCII format.
+" 13 78
   exit 0
 fi
 
-
 # switch on
 if [ "$1" = "1" ] || [ "$1" = "on" ]; then
-
     echo ""
     echo "# ***"
     echo "# Downloading the Whitepaper to $DownloadPath$WhitepaperFilename..."
     echo "# ***"
     echo ""
-
-    sudo -u bitcoin bitcoin-cli getblock 00000000000000ecbbff6bafb7efa2f7df05b227d5c73dca8f2635af32a2e949 0 | tail -c+92167 | for ((o=0;o<946;++o)) ; do read -rN420 x ; echo -n ${x::130}${x:132:130}${x:264:130} ; done | xxd -r -p | tail -c+9 | head -c184292 > $DownloadPath/$WhitepaperFilename
-   
-   # setting value in raspi blitz config
+    # download from blockchain and transform into pdf
+    sudo -u bitcoin bitcoin-cli getblock 00000000000000ecbbff6bafb7efa2f7df05b227d5c73dca8f2635af32a2e949 0 |\
+    tail -c+92167 |\
+    for ((o=0;o<946;++o)) ; do read -rN420 x ; echo -n ${x::130}${x:132:130}${x:264:130} ; done |\
+    xxd -r -p |\
+    tail -c+9 |\
+    head -c184292 > $DownloadPath/$WhitepaperFilename
+    # transfor pdf to txt to view with less through the terminal
+    pdftotext $DownloadPath$WhitepaperFilename
+    # setting value in raspi blitz config
     sudo sed -i "s/^whitepaper=.*/whitepaper=on/g" /mnt/hdd/raspiblitz.conf
-   
     echo "# OK - Whitepaper downloaded to $DownloadPath$WhitepaperFilename"
-
   exit 0
 fi
 
@@ -56,7 +59,6 @@ fi
 if [ "$1" = "0" ] || [ "$1" = "off" ]; then
   isInstalled=1
   if [ ${isInstalled} -eq 1 ]; then
-    
     echo ""
     echo "# ***"
     echo "# Removing the Whitepaper from $DownloadPath$WhitepaperFilename..."
@@ -64,9 +66,8 @@ if [ "$1" = "0" ] || [ "$1" = "off" ]; then
     echo ""
     # setting value in raspi blitz config
     sudo sed -i "s/^whitepaper=.*/whitepaper=off/g" /mnt/hdd/raspiblitz.conf
-    
-    rm $DownloadPath/$WhitepaperFilename
-
+    rm $DownloadPath$WhitepaperFilename
+    rm $DownloadPath/bitcoin.txt
     echo "# OK - Whitepaper removed."
   else
     echo "# The Whitepaper has not been downloaded yet."
