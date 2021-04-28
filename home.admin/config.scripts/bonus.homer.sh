509826539b714f20a3647191c9c67e915a34bfd8
Deverick <deverick@apollolabs.io>
2021-04-28 14:54:25 +0200

    Adds persistent storage of config + assets

--- home.admin/config.scripts/bonus.homer.sh
+++ home.admin/config.scripts/bonus.homer.sh
@@ -71,13 +71,22 @@ if [ "$1" = "status" ]; then
   exit 0
 fi
 
-# update (quick hack)
+# Update (quick hack)
+# Stores configuration and assets found in /var/www/homer
 if [ "$1" = "update" ]; then
 
   cd /home/homer/homer
   sudo -u homer git fetch
+  # Comment out until official release that fixes issue #206 
   # sudo -u homer git reset --hard $remoteVersion
 
+  # Save config file
+  sudo cp /var/www/homer/assets/config.yml /mnt/hdd/app-data/homer/config.yml
+
+  # Save any icons
+  sudo rsync -av /var/www/homer/assets/icons/ /mnt/hdd/app-data/homer/icons
+
+  # Install Homer
   sudo -u homer NG_CLI_ANALYTICS=false npm install
   if ! [ $? -eq 0 ]; then
       echo "FAIL - npm install did not run correctly, aborting"
@@ -94,10 +103,13 @@ if [ "$1" = "update" ]; then
 
   # copy new dist over to nginx
   sudo rsync -av --delete dist/ /var/www/homer/
-  sudo chown -R www-data:www-data /var/www/homer
 
   # link config again
   sudo -u www-data ln -s /mnt/hdd/app-data/homer/config.yml /var/www/homer/assets/config.yml
+
+  #restore saved icons
+  sudo rsync -av /mnt/hdd/app-data/homer/icons/ /var/www/homer/assets/icons/
+  sudo chown -R www-data:www-data /var/www/homer/
 
   echo "# OK - Homer should now be serving latest code from Version $remoteVersion"
   exit 0
@@ -139,7 +151,7 @@ if [ "$1" = "1" ] || [ "$1" = "on" ]; then
     # if no persistent data exists - create data dir & use standard config
     oldConfigExists=$(ls /mnt/hdd/app-data/homer/config.yml 2>/dev/null | grep -c 'config.yml')
     if [ "${oldConfigExists}" == "0" ]; then
-      # sudo -u homer cp /home/homer/homer/dist/assets/config.yml.dist /home/homer/homer/dist/assets/config.yml
+      sudo -u homer cp /home/homer/homer/dist/assets/config.yml.dist /home/homer/homer/dist/assets/config.yml
       sudo mkdir -p /mnt/hdd/app-data/homer
       sudo cp /home/homer/homer/dist/assets/config.yml.dist /mnt/hdd/app-data/homer/config.yml
     fi
@@ -195,6 +207,7 @@ fi
 
 
 # switch off
+# Stores configuration on persistent disk for quick restore
 if [ "$1" = "0" ] || [ "$1" = "off" ]; then
 
   # setting value in raspi blitz config
@@ -202,11 +215,16 @@ if [ "$1" = "0" ] || [ "$1" = "off" ]; then
 
   isInstalled=$(sudo ls /var/www/homer/assets/config.yml.dist 2>/dev/null | grep -c 'config.yml.dist')
   if [ ${isInstalled} -eq 1 ]; then
+
+    echo "Saving assets to external drive. If you no longer require these files, it is safe delete from persistent disk at /mnt/hdd/app-data/homer."
+    sudo cp /var/www/homer/assets/config.yml /mnt/hdd/app-data/homer/config.yml
+    sudo rsync -av --delete /var/www/homer/assets/icons/ /mnt/hdd/app-data/homer/icons/
+
+
     echo "# *** REMOVING homer ***"
     # delete user and home directory
     sudo userdel -rf homer
     sudo rm -rf /var/www/homer
-
 
     # remove nginx symlinks
     sudo rm -f /etc/nginx/sites-enabled/homer_ssl.conf
