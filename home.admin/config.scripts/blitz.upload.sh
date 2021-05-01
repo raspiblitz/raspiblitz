30b1cf804f51a59abe694f8940f8f8557070987c
rootzoll <christian@geektank.de>
2021-05-01 20:17:48 +0200

    blitz upload flexible user

--- home.admin/config.scripts/blitz.upload.sh
+++ home.admin/config.scripts/blitz.upload.sh
@@ -15,9 +15,11 @@ source <(/home/admin/config.scripts/internet.sh status local)
 if [ -d "/mnt/hdd/temp" ]; then
   # HDD with temp directory is connected - the use it
   defaultUploadPath="/mnt/hdd/temp/upload"
+  defaultUploadUser="bitcoin"
 else
   # fallback if no HDD is connected
   defaultUploadPath="/home/bitcoin/temp/upload"
+  defaultUploadUser="bitcoin"
 fi
 
 
@@ -26,13 +28,14 @@ action="$1"
 
 if [ ${action} = "prepare-upload" ]; then
 
-  # make sure that temp directory exists, is clear and can be written by user bitcoin
+  # make sure that temp directory exists, is clear and can be written by ${defaultUploadUser}
   sudo mkdir -p ${defaultUploadPath} 2>/dev/null
   sudo rm ${defaultUploadPath}/* 2>/dev/null
-  sudo chown -R bitcoin:bitcoin ${defaultUploadPath} 2>/dev/null
+  sudo chown -R ${defaultUploadUser}:${defaultUploadUser} ${defaultUploadPath} 2>/dev/null
 
   echo "localip='${localip}'"
   echo "defaultUploadPath='${defaultUploadPath}'"
+  echo "defaultUploadUser='${defaultUploadUser}'"
   exit 0
 fi
 
