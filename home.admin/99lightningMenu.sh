9d72e3b91a4c866174f905f19a67dcf979dc3584
rootzoll <christian@geektank.de>
2021-05-04 19:01:56 +0200

    test exit code submenu

--- home.admin/99lightningMenu.sh
+++ home.admin/99lightningMenu.sh
@@ -107,6 +107,7 @@ case $CHOICE in
               sudo /home/admin/XXshutdown.sh reboot
               exit 0
             fi
+            exit 1
             ;;
         CLOSEALL)
             /home/admin/BBcloseAllChannels.sh
