e68daf21dea6d426a6f21f276114a1ba68d0cc19
rootzoll <christian@geektank.de>
2021-05-22 13:01:17 -0500

    fix typo

--- home.admin/setup.scripts/dialogRecovery.sh
+++ home.admin/setup.scripts/dialogRecovery.sh
@@ -8,7 +8,7 @@ whiptail --title " RASPIBLITZ RECOVERY " --yes-button "Start Recovery" --no-butt
 
 You can now start RECOVERY to freshly build your system based on existing configuration & data. This process is often used to repair broken features or clean the system up.
 
-You will need to set a new Password A for the SSH login. All other passwords will stay the same and yout channels will stay open.
+You will need to set a new Password A for the SSH login. All other passwords will stay the same and channels will stay open.
 
 Please make sure to have your seed words & static channel backup file (just in case).
 
