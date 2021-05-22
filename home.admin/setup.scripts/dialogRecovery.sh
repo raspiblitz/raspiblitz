8d2532bf084b73988ecdbc58866b9bb1a53148fd
rootzoll <christian@geektank.de>
2021-05-22 13:00:07 -0500

    fix recovery dialog

--- home.admin/setup.scripts/dialogRecovery.sh
+++ home.admin/setup.scripts/dialogRecovery.sh
@@ -6,14 +6,14 @@ source /home/admin/raspiblitz.info
 
 whiptail --title " RASPIBLITZ RECOVERY " --yes-button "Start Recovery" --no-button "Other Options" --yesno "We found data from an existing RaspiBlitz on your HDD/SSD.
 
-You can start RECOVERY now to freshly build your system to this old configuration. This process is often used to repair broken features or clean the system up.
+You can now start RECOVERY to freshly build your system based on existing configuration & data. This process is often used to repair broken features or clean the system up.
 
-You will need to set a new Password A for the SSH login. All your channels will stay open and other passwords will stay the same.
+You will need to set a new Password A for the SSH login. All other passwords will stay the same and yout channels will stay open.
 
 Please make sure to have your seed words & static channel backup file (just in case).
 
 Do you want to start RECOVERY of your RaspiBlitz now?
-      " 18 65
+      " 20 68
 
 if [ "$?" == "0" ]; then
     # 0 --> run recover
