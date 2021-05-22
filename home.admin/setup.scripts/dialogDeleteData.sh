3163b44da91428f351f01f1f5aced4a80fcfc985
rootzoll <christian@geektank.de>
2021-05-22 12:56:13 -0500

    fix dialogs

--- home.admin/setup.scripts/dialogDeleteData.sh
+++ home.admin/setup.scripts/dialogDeleteData.sh
@@ -9,10 +9,10 @@ if [ ${blockchainName} != "" ]; then
 
     whiptail --title " BLOCKCHAIN DATA FOUND " --yes-button "USE BLOCKCHAIN" --no-button "DELETE" --yesno "We found ${blockchainName} blockchain data on your HDD/SSD.
 
-Using existing blockchain data can reduce your setup/sync time. But if you didnt validated that blockchain yourself there is a level of trust involved.
+Using existing blockchain data can reduce the setup/sync time. But if you didnt validated the blockchain yourself there is a level of trust involved.
 
 Do you want to use that blockchain data and run ${blockchainName}? 
-      " 10 65
+      " 14 68
 
     if [ "$?" == "0" ]; then
         # 0 --> use blockchain & delete all other data
@@ -29,7 +29,7 @@ if [ "${keepBlockchain}" == "1" ]; then
     whiptail --title " DELETING ALL OTHER DATA " --yes-button "DELETE DATA" --no-button "STOP SETUP" --yesno "OK we will keep the blockchain data - but all other data on your HDD/SSD will get deleted on setup. Make sure that there is no important data or old funds on that data drive.
 
 Are you sure to DELETE ALL OTHER DATA on the HDD/SSD?
-      " 12 65
+      " 11 65
 
     if [ "$?" == "0" ]; then
         # 0 --> keep blockchain + delete all other data
@@ -42,10 +42,10 @@ Are you sure to DELETE ALL OTHER DATA on the HDD/SSD?
 else
 
     # normal formatting data drive security question
-    whiptail --title " FORMATTING DATA DRVE " --yes-button "DELETE DATA" --no-button "STOP SETUP" --yesno "Your data drive will now be formatted. This will delete all data on your connected HDD/SSD. Make sure that there is no important data or old funds on that data drive.
+    whiptail --title " FORMATTING DATA DRVE " --yes-button "DELETE DATA" --no-button "STOP SETUP" --yesno "Your data drive will now get formatted. This will delete all data on your connected HDD/SSD. Make sure that there is no important data or old funds on that data drive.
 
 Are you sure to format the HDD/SSD and DELETE ALL DATA on it?
-      " 12 65
+      " 11 65
 
     if [ "$?" == "0" ]; then
         # 0 --> format drive
