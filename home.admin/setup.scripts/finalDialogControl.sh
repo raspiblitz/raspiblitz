8bddde6ed02b379886394698e20b0e99c3785b5b
rootzoll <christian@geektank.de>
2021-05-25 18:04:01 -0500

    fix readkey

--- home.admin/setup.scripts/finalDialogControl.sh
+++ home.admin/setup.scripts/finalDialogControl.sh
@@ -9,18 +9,13 @@ source /home/admin/raspiblitz.info
 SETUPFILE="/var/cache/raspiblitz/temp/raspiblitz.setup"
 source ${SETUPFILE}
 
-
-clear
-echo "TEST"
-sleep 3
-
 ############################################
 # SHOW SEED WORDS AFTER SETUP
 if [ "${setupPhase}" == "setup" ]; then
 
   echo "Write down your seedwords: ${seedwords6x4NEW}"
   echo "PRESS ENTER"
-  readkey
+  read key
 
 fi
 
@@ -32,16 +27,16 @@ if [ "${setupPhase}" == "setup" ];
 
   echo "Hooray :) Everything is Setup!"
   echo "PRESS ENTER"
-  readkey
+  read key
 
 elif [ "${setupPhase}" == "migration" ]; then
 
   echo "Hooray :) Your Migration to RaspiBlitz is Done!"
   echo "PRESS ENTER"
-  readkey
+  read key
 
 else
   echo "Missing Final Done Dialog for: ${setupPhase}"
   echo "PRESS ENTER"
-  readkey
+  read key
 fi
