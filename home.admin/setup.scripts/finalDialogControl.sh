0758a4bd0968f3bbd6a52f3465509a7bd7be544d
rootzoll <christian@geektank.de>
2021-06-19 00:06:58 +0200

    blockchain choice

--- home.admin/setup.scripts/finalDialogControl.sh
+++ home.admin/setup.scripts/finalDialogControl.sh
@@ -11,7 +11,7 @@ source ${SETUPFILE}
 
 ############################################
 # SHOW SEED WORDS AFTER SETUP
-if [ "${setupPhase}" == "setup" ]; then
+if [ "${setupPhase}" == "setup" ] && [ "${seedwords6x4NEW}" != "" ]; then
     ack=0
     while [ ${ack} -eq 0 ]
     do
@@ -26,12 +26,16 @@ fi
 ############################################
 # BLOCKCHAIN INFO & OPTIONS
 
-if [ ${syncProgress} -lt 75 ]; then
+if [ ${syncProgress} -lt 99 ] && [ "${network}" == "bitcoin" ]; then
   clear
-  echo "Your Blockchain is at ${syncProgress}% - this might take multiple days to validate."
-  echo "TODO: Option COPY OVER LAN IF BITCOIN"
-  echo "TODO: MAKE SURE THAT background.service is running from beginng!"
-  echo "PRESS ENTER"
+
+  # offer choice to copy blockchain over LAN
+  OPTIONS=()
+  OPTIONS+=(VALIDATE "Run full self sync/validation (takes long)")
+  OPTIONS+=(COPY "Copy from Computer/RaspiBlitz over LAN (~6h)")
+  CHOICESUB=$(dialog --backtitle "RaspiBlitz" --clear --title "Blockchain Sync/Validation" --menu "Your Blockchain sync is at ${syncProgress}% - this might take multiple days to validate.\n\nHow do you want to proceed:" 11 60 6 "${OPTIONS[@]}" 2>&1 >/dev/tty)
+
+  echo "CHOICESUB: ${CHOICESUB}"
   read key
 fi
 
@@ -41,17 +45,20 @@ fi
 # when coming from fresh setup
 if [ "${setupPhase}" == "setup" ]; then
   clear
-  whiptail --title "Setup Done" --yes-button "Show Again" --no-button "CONTINUE" --yesno "  Are you sure that you wrote down the word list?" 8 55
-  echo "Hooray :) Everything is Setup!"
-  echo "PRESS ENTER"
-  read key
+  whiptail --title " SetUp Done " --msgbox "\
+Your RaspiBlitz Setup is done. Welcome new node operator! :D\n
+There might now be some waiting time until your Blockchain is fully synced before you can enter the RaspiBlitz user menu.\n
+Its safe to logout during sync and return later.\n
+" 12 65
 
 # when coming from migration from other node
 elif [ "${setupPhase}" == "migration" ]; then
   clear
-  echo "Hooray :) Your Migration to RaspiBlitz is Done!"
-  echo "PRESS ENTER"
-  read key
+  whiptail --title " Migration Done " --msgbox "\
+Your running now RaspiBlitz. Welcome to the family! :D\n
+There might now be some waiting time until your Blockchain is fully synced before you can enter the RaspiBlitz user menu.\n
+Its safe to logout during sync and return later.\n
+" 12 65
 
 # just in case then from another phase
 else
