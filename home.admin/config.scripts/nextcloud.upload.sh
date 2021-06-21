a798126b4b52a8817083e611de8f1286f58ade5b
Igor Bubelov <igor@bubelov.com>
2021-06-21 21:13:10 +0700

    Fix potential issue with url substitution

--- home.admin/config.scripts/nextcloud.upload.sh
+++ home.admin/config.scripts/nextcloud.upload.sh
@@ -11,103 +11,92 @@ fi
 
 source /mnt/hdd/raspiblitz.conf
 
-MODE=$1
+case "${1}" in
+  on)
+    sudo touch /home/admin/.tmp
+    sudo chmod 777 /home/admin/.tmp
 
-if test $MODE = "on"; then
-  sudo touch /home/admin/.tmp
-  sudo chmod 777 /home/admin/.tmp
-
-  SERVER=$2
-  if [ -z $SERVER ]; then
-    whiptail --title "Static Channel Backup on Nextcloud" --inputbox "Enter your Nextcloud server URL\nExample: https://cloud.johnsmith.com" 11 70 2>/home/admin/.tmp
-    SERVER=$(cat /home/admin/.tmp)
-  fi
-
-  USER=$3
-  if [ -z $USER ]; then
-    whiptail --title "Static Channel Backup on Nextcloud" --inputbox "Enter your Nextcloud username" 11 70 2>/home/admin/.tmp
-    USER=$(cat /home/admin/.tmp)
-  fi
-
-  PASSWORD=$4
-  if [ -z $PASSWORD ]; then
-    whiptail --title "Static Channel Backup on Nextcloud" --inputbox "Enter your Nextcloud password" 11 70 2>/home/admin/.tmp
-    PASSWORD=$(cat /home/admin/.tmp)
-  fi
-
-  shred -u /home/admin/.tmp
-
-  if [ $SERVER ] && [ $USER ] && [ $PASSWORD ]; then
-    if [ -z $nextcloudBackupServer ]; then
-      echo "nextcloudBackupServer=$SERVER" >> /mnt/hdd/raspiblitz.conf
+    SERVER="${2}"
+    if [ -z "${SERVER}" ]; then
+      whiptail --title "Static Channel Backup on Nextcloud" --inputbox "Enter your Nextcloud server URL\nExample: https://cloud.johnsmith.com" 11 70 2>/home/admin/.tmp
+      SERVER=$(cat /home/admin/.tmp)
     fi
-    sudo sed -i "s/^nextcloudBackupServer=.*/nextcloudBackupServer=$SERVER/g" /mnt/hdd/raspiblitz.conf
 
-    if [ -z $nextcloudBackupUser ]; then
-      echo "nextcloudBackupUser=$USER" >> /mnt/hdd/raspiblitz.conf
+    USER="${3}"
+    if [ -z "${USER}" ]; then
+      whiptail --title "Static Channel Backup on Nextcloud" --inputbox "Enter your Nextcloud username" 11 70 2>/home/admin/.tmp
+      USER=$(cat /home/admin/.tmp)
     fi
-    sudo sed -i "s/^nextcloudBackupUser=.*/nextcloudBackupUser=$USER/g" /mnt/hdd/raspiblitz.conf
 
-    if [ -z $nextcloudBackupPassword ]; then
-      echo "nextcloudBackupPassword=$PASSWORD" >> /mnt/hdd/raspiblitz.conf
+    PASSWORD="${4}"
+    if [ -z "${PASSWORD}" ]; then
+      whiptail --title "Static Channel Backup on Nextcloud" --inputbox "Enter your Nextcloud password" 11 70 2>/home/admin/.tmp
+      PASSWORD=$(cat /home/admin/.tmp)
     fi
-    sudo sed -i "s/^nextcloudBackupPassword=.*/nextcloudBackupPassword=$PASSWORD/g" /mnt/hdd/raspiblitz.conf
-  else
-    echo "Please provide nextcloud server, username and password"
-  fi
 
-elif test $MODE = "off"; then
+    shred -u /home/admin/.tmp
 
-  sudo sed -i '/nextcloudBackupServer=.*/d' /mnt/hdd/raspiblitz.conf
-  sudo sed -i '/nextcloudBackupUser=.*/d' /mnt/hdd/raspiblitz.conf
-  sudo sed -i '/nextcloudBackupPassword=.*/d' /mnt/hdd/raspiblitz.conf
+    if [ "${SERVER}" ] && [ "${USER}" ] && [ "${PASSWORD}" ]; then
+      sudo sed -i '/nextcloudBackupServer=.*/d' /mnt/hdd/raspiblitz.conf
+      echo "nextcloudBackupServer=${SERVER}" >> /mnt/hdd/raspiblitz.conf
 
-elif test $MODE = "upload"; then
+      sudo sed -i '/nextcloudBackupUser=.*/d' /mnt/hdd/raspiblitz.conf
+      echo "nextcloudBackupUser=${USER}" >> /mnt/hdd/raspiblitz.conf
 
-  if [ -z $nextcloudBackupServer ] || [ -z $nextcloudBackupUser ] || [ -z $nextcloudBackupPassword ]; then
-    echo "err='nextcloud credentials are missing'"
+      sudo sed -i '/nextcloudBackupPassword=.*/d' /mnt/hdd/raspiblitz.conf
+      echo "nextcloudBackupPassword=${PASSWORD}" >> /mnt/hdd/raspiblitz.conf
+    else
+      echo "Please provide nextcloud server, username and password"
+    fi
+  ;;
+  off)
+    sudo sed -i '/nextcloudBackupServer=.*/d' /mnt/hdd/raspiblitz.conf
+    sudo sed -i '/nextcloudBackupUser=.*/d' /mnt/hdd/raspiblitz.conf
+    sudo sed -i '/nextcloudBackupPassword=.*/d' /mnt/hdd/raspiblitz.conf
+  ;;
+  upload)
+    if [ -z "${nextcloudBackupServer}" ] || [ -z "${nextcloudBackupUser}" ] || [ -z "${nextcloudBackupPassword}" ]; then
+      echo "err='nextcloud credentials are missing'"
+      exit 1
+    fi
+
+    FILEPATH="${2}"
+    if [ -z "${FILEPATH}" ]; then
+      echo "err='filepath is missing'"
+      exit 1
+    fi
+
+    REMOTE_DIR_URL="$nextcloudBackupServer/remote.php/dav/files/$nextcloudBackupUser/raspiblitz/"
+
+    # checking if remote directory exists
+    RESPONSE=$(curl "${REMOTE_DIR_URL}" \
+      --user "${nextcloudBackupUser}:${nextcloudBackupPassword}" \
+      --request PROPFIND \
+      --silent)
+
+    # if remote directory doesn't exist, we need to create it
+    if [[ "${RESPONSE}" = *DAV\\Exception\\NotFound* ]]; then
+      curl "${REMOTE_DIR_URL}" \
+        --user "${nextcloudBackupUser}:${nextcloudBackupPassword}" \
+        --request MKCOL \
+        --silent
+    fi
+
+    if curl "${REMOTE_DIR_URL}" \
+      --user "${nextcloudBackupUser}:${nextcloudBackupPassword}" \
+      --upload-file "${FILEPATH}" \
+      --silent;
+    then
+      echo "# Great success!"
+      echo "upload=1"
+    else
+      echo "err='File upload failed'"
+      echo "upload=0"
+      exit 1
+    fi
+  ;;
+  *)
+    echo "err='Unknown action'"
     exit 1
-  fi
-
-  FILEPATH=$2
-  if [ -z $FILEPATH ]; then
-    echo "err='filepath is missing'"
-    exit 1
-  fi
-
-  REMOTE_DIR_URL="$nextcloudBackupServer/remote.php/dav/files/$nextcloudBackupUser/raspiblitz/"
-
-  # checking if remote directory exists
-  RESPONSE=$(curl $REMOTE_DIR_URL \
-    --user "$nextcloudBackupUser:$nextcloudBackupPassword" \
-    --request PROPFIND \
-    --silent)
-
-  # if remote directory doesn't exist, we need to create it
-  if [[ $RESPONSE = *DAV\\Exception\\NotFound* ]]; then
-    curl "$REMOTE_DIR_URL" \
-      --user "$nextcloudBackupUser:$nextcloudBackupPassword" \
-      --request MKCOL \
-      --silent
-  fi
-
-  sudo curl "$REMOTE_DIR_URL" \
-    --user "$nextcloudBackupUser:$nextcloudBackupPassword" \
-    --upload-file $FILEPATH \
-    --silent
-
-  if test $? = 0; then
-    echo "# Great success!"
-    echo "upload=1"
-  else
-    echo "err='file upload failed'"
-    echo "upload=0"
-    exit 1
-  fi
-
-else
-
-  echo "err='unkown mode'"
-  exit 1
-
-fi
+  ;;
+esac
