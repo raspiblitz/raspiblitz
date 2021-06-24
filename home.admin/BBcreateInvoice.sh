e85e118e5b8a6fbc605e390f3a78d8bf7b96d214
rootzoll <christian@geektank.de>
2021-06-24 21:25:38 +0200

    remove chain in sync

--- home.admin/BBcreateInvoice.sh
+++ home.admin/BBcreateInvoice.sh
@@ -14,9 +14,30 @@ if [ ${#chain} -eq 0 ]; then
   chain=$(${network}-cli getblockchaininfo | jq -r '.chain')
 fi
 
-# Check if ready (chain in sync and channels open)
-./XXchainInSync.sh $network $chain
-if [ $? != 0 ]; then
+# check if chain is in sync
+cmdChainInSync="lncli --chain=${network} --network=${chain}net getinfo | grep '"synced_to_chain": true' -c"
+chainInSync=${cmdChainInSync}
+while [ "${chainInSync}" == "0" ]; do
+  dialog --title "Fail: not in sync" \
+	 --ok-label "Try now" \
+	 --cancel-label "Give up" \
+	 --pause "\n\n'lncli getinfo' shows 'synced_to_chain': false\n\nTry again in a few seconds." 15 60 5
+  
+  if [ $? -gt 0 ]; then
+      exit 1
+  fi
+  chainInSync=${cmdChainInSync}
+done
+
+# check number of connected peers
+echo "check for open channels"
+openChannels=$(sudo -u bitcoin /usr/local/bin/lncli --chain=${network} --network=${chain}net listchannels 2>/dev/null | grep chan_id -c)
+if [ ${openChannels} -eq 0 ]; then
+  echo ""
+  echo "!!!!!!!!!!!!!!!!!!!"
+  echo "FAIL - You have NO ESTABLISHED CHANNELS .. open a channel first."
+  echo "!!!!!!!!!!!!!!!!!!!"
+  echo ""
   exit 1
 fi
 
