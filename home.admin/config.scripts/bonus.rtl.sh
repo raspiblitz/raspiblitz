4a6db42ddf51701c8d5ce42bed055f59e64933cd
nyxnor <69700936+nyxnor@users.noreply.github.com>
2021-07-07 23:56:30 +0000

    correct to path docs/Sample-RTL-Config.json
    Issue https://github.com/rootzoll/raspiblitz/issues/2412

--- home.admin/config.scripts/bonus.rtl.sh
+++ home.admin/config.scripts/bonus.rtl.sh
@@ -87,14 +87,14 @@ function configRTL() {
   # prepare RTL-Config.json file
   echo "# RTL.conf"
   # change of config: https://github.com/Ride-The-Lightning/RTL/tree/v0.6.4
-  sudo cp /home/rtl/RTL/sample-RTL-Config.json /home/admin/RTL-Config.json
+  sudo cp /home/rtl/RTL/docs/Sample-RTL-Config.json /home/admin/RTL-Config.json
   sudo chown admin:admin /home/admin/RTL-Config.json
   sudo chmod 600 /home/admin/RTL-Config.json || exit 1
   PASSWORD_B=$(sudo cat /mnt/hdd/${network}/${network}.conf | grep rpcpassword | cut -c 13-)
   # modify sample-RTL-Config.json and save in RTL-Config.json
   sudo node > /home/admin/RTL-Config.json <<EOF
 //Read data
-var data = require('/home/rtl/RTL/sample-RTL-Config.json');
+var data = require('/home/rtl/RTL/docs/Sample-RTL-Config.json');
 //Manipulate data
 data.nodes[0].lnNode = '$hostname'
 data.nodes[0].Authentication.macaroonPath = '/home/rtl/.lnd/data/chain/${network}/${chain}net/'
