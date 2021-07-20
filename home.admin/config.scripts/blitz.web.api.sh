f7dd2bea0edff5840743b44e566f71e252853b8f
rootzoll <christian@geektank.de>
2021-07-20 15:07:31 +0200

    new line on last edit

--- home.admin/config.scripts/blitz.web.api.sh
+++ home.admin/config.scripts/blitz.web.api.sh
@@ -111,7 +111,7 @@ if [ "$1" = "update-config" ]; then
   cd /home/admin/blitz_api
   dateStr=$(date)
   echo "# Update Web API CONFIG (${dateStr})"
-  echo "LAST EDITED (${dateStr})" >> ./.env
+  echo "\nLAST EDITED (${dateStr})" >> ./.env
   RPCUSER=$(sudo cat /mnt/hdd/${network}/${network}.conf | grep rpcuser | cut -c 9-)
   RPCPASS=$(sudo cat /mnt/hdd/${network}/${network}.conf | grep rpcpassword | cut -c 13-)
   if [ "${RPCUSER}" == "" ]; then
