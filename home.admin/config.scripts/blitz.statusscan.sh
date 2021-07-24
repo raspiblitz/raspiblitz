af4b604050eac540000450eba92d1225b42d4c81
rootzoll <christian@geektank.de>
2021-07-25 00:50:57 +0200

    try scanprogress

--- home.admin/config.scripts/blitz.statusscan.sh
+++ home.admin/config.scripts/blitz.statusscan.sh
@@ -274,15 +274,14 @@ echo "clnActive=${clnRunning}"
 if [ ${clnRunning} -eq 1 ]; then
   clnInfo=$(sudo -u bitcoin lightning-cli getinfo)
   clnBlockHeight=$(echo "${clnInfo}" | jq -r '.blockheight' | tr -cd '[[:digit:]]')
-  echo "clnBlockHeight=${clnBlockHeight}"
   clnBlockHeightPlusOne=$(expr $clnBlockHeight + 1)
-  echo "clnBlockHeightPlusOne=${clnBlockHeightPlusOne}"
-  echo "total=${total}"
   if [ "${total}" == "${clnBlockHeight}" ] || [ "${total}" == "${clnBlockHeightPlusOne}" ]; then
       echo "syncedToChain=1"
   else
       echo "syncedToChain=0"
   fi
+  scanProgress=$(echo "scale=2; $clnBlockHeight*100/$total" | bc)
+  echo "scanProgress=${scanProgress}"
 fi
 
 # touchscreen statistics
