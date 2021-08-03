cd1fe0bce41e4dd1dca3d66be908d3575a74a087
Wolf <vv01f@users.noreply.github.com>
2021-08-03 20:29:37 +0200

    ssmtp=>msmtp,sendxmpp
    * start replacing ssmtp with msmtp
    * add sendxmpp

--- home.admin/config.scripts/blitz.notify.sh
+++ home.admin/config.scripts/blitz.notify.sh
@@ -6,6 +6,9 @@ if [ $# -eq 0 ] || [ "$1" = "-h" ] || [ "$1" = "-help" ]; then
  echo "blitz.notify.sh on"
  echo "blitz.notify.sh off"
  echo "blitz.notify.sh send \"Message to be send via configured method\""
+# todo: enable explicit sending method so user can be informed (messenger) and have log term storage (email)
+# echo "blitz.notify.sh mail \"Message to be send via mail method\""
+# echo "blitz.notify.sh xmpp \"Message to be send via xmpp method\""
  exit 1
 fi
 
@@ -25,6 +28,11 @@ fi
 # check all other settings and add if missing
 if ! grep -Eq "^notifyMethod=.*" /mnt/hdd/raspiblitz.conf; then
     echo "notifyMethod=mail" | sudo tee -a /mnt/hdd/raspiblitz.conf >/dev/null
+elif [ $(grep "notifyMethod=" /mnt/hdd/raspiblitz.conf|cut -d"=" -f2) = "xmpp" ]; then
+    # XMPP
+    if ! grep -Eq "^notifyXMPPTo=.*" /mnt/hdd/raspiblitz.conf; then
+        echo "notifyXMPPTo=user@domain.tld" | sudo tee -a /mnt/hdd/raspiblitz.conf >/dev/null
+    fi
 fi
 
 # Mail
@@ -79,34 +87,59 @@ source /mnt/hdd/raspiblitz.conf 2>/dev/null
 if [ "$1" = "1" ] || [ "$1" = "on" ]; then
   echo "switching the NOTIFY ON"
 
-  # install sstmp if not already present
-  if ! command -v ssmtp >/dev/null; then
+  # install sendxmpp
+  [ -z "$(find -H /usr/share/doc/sendxmpp/README -maxdepth 0 -mtime -7)" ] && sudo apt-get update
+  if ! command -v msmtp >/dev/null; then
+      sudo apt-get install -y sendxmpp
+  fi
+
+  # install mstmp if not already present
+  if ! command -v msmtp >/dev/null; then
     [ -z "$(find -H /var/lib/apt/lists -maxdepth 0 -mtime -7)" ] && sudo apt-get update
-    sudo apt-get install -y ssmtp
+    sudo apt-get install -y msmtp msmtp-mta mailutils
+    # todo: configuration for msmtp
+    #sudo wget -O /etc/msmtprc https://marlam.de/msmtp/msmtprc.txt
+    #sudo chmod 600 /etc/msmtprc
+    #sudo cp /etc/msmtprc ~/.msmtprc
   fi
 
   # install python lib for smime into virtual env
   sudo -H /usr/bin/python3 -m pip install smime
 
-  # write ssmtp config
-  cat << EOF | sudo tee /etc/ssmtp/ssmtp.conf >/dev/null
-#
+  # write msmtp config
+#  cat << EOF | sudo tee /etc/ssmtp/ssmtp.conf >/dev/null
+  cat << EOF | sudo tee /etc/msmtprc >/dev/null
+# outdated service configuration #
 # Config file for sSMTP sendmail
 #
 # The person who gets all mail for userids < 1000
 # Make this empty to disable rewriting.
-Root=${notifyMailTo}
+#Root=${notifyMailTo}
 
 # hostname of this system
-Hostname=${notifyMailHostname}
+#Hostname=${notifyMailHostname}
 
 # relay/smarthost server settings
-Mailhub=${notifyMailServer}
-AuthUser=${notifyMailUser}
-AuthPass=${notifyMailPass}
-UseSTARTTLS=YES
-FromLineOverride=YES
+#Mailhub=${notifyMailServer}
+#AuthUser=${notifyMailUser}
+#AuthPass=${notifyMailPass}
+#UseSTARTTLS=YES
+#FromLineOverride=YES
+
+# possible successor service cnfiguration #
+# mSMTP configuration, acc. to binfalse.de/2020/02/17/migrating-from-ssmtp-to-msmtp/
+defaults
+port 25
+tls on
+
+account default
+auth off
+host RELAY
+domain HOSTNAME
+from webserver@HOSTNAME
+add_missing_date_header on
 EOF
+  chmod 600 /etc/msmtprc
 
   # edit raspi blitz config
   echo "editing /mnt/hdd/raspiblitz.conf"
@@ -130,6 +163,20 @@ fi
 ###################
 # send the message
 ###################
+
+#case "$1" in
+#  send)
+#    ;;
+#  xmpp)
+#    notifyMethod=xmpp
+#    1=send
+#    ;;
+#  *)
+#    notifyMethod=mail
+#    1=send
+#    ;;  
+#esac
+
 if [ "$1" = "send" ]; then
   # check if "notify" is enabled - if not exit
   if ! grep -Eq "^notify=on" /mnt/hdd/raspiblitz.conf; then
@@ -137,20 +184,25 @@ if [ "$1" = "send" ]; then
     exit 1
   fi
 
-  if ! command -v ssmtp >/dev/null; then
-    echo "please run \"on\" first"
-    exit 1
-  fi
-
-  # now parse settings from config and use to send the message
-  if [ "${notifyMethod}" = "ext" ]; then
-    /usr/bin/python3 /home/admin/XXsendNotification.py ext ${notifyExtCmd} "$2"
-  elif [ "${notifyMethod}" = "mail" ]; then
+ # now parse settings from config and use to send the message
+  if [ "${notifyMethod}" = "mail" ]; then
+    if ! command -v msmtp >/dev/null; then
+      echo "please make sure msmtp is configured first"
+      exit 1
+    fi
     if [ "${notifyMailEncrypt}" = "on" ]; then
       /usr/bin/python3 /home/admin/XXsendNotification.py mail --from-address "${notifyMailFromAddress}" --from-name "${notifyMailFromName}" --cert "${notifyMailToCert}" --encrypt ${notifyMailTo} "${@:3}" "$2"
     else
       /usr/bin/python3 /home/admin/XXsendNotification.py mail --from-address "${notifyMailFromAddress}" --from-name "${notifyMailFromName}" "${notifyMailTo}" "${@:3}" "$2"
     fi
+  elif [ "${notifyMethod}" = "xmpp" ]; then
+    if ! command -v sendxmpp >/dev/null; then
+      echo "please make sure sendxmpp is installed and configured first."
+    fi
+    echo -e $(whoami)"@"$(hostname)":\n\n---\n## ${2}\n${@:3}" | sendxmpp "${notifyXMPPTo}" || { echo "error sending XMPP message"; exit 1; }
+    # 
+  elif [ "${notifyMethod}" = "ext" ]; then
+    /usr/bin/python3 /home/admin/XXsendNotification.py ext ${notifyExtCmd} "$2"
   elif [ "${notifyMethod}" = "slack" ]; then
     /usr/bin/python3 /home/admin/XXsendNotification.py slack -h "$2"
   else
@@ -162,4 +214,3 @@ fi
 
 echo "FAIL - Unknown Parameter $1"
 exit 1
-
