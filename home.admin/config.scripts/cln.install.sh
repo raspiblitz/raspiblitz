97db88bcf39cb09ddf82502dc7e106d9cff8c4fa
openoms <oms@tuta.io>
2021-08-10 16:11:40 +0100

    cln update to v0.10.1

--- home.admin/config.scripts/cln.install.sh
+++ home.admin/config.scripts/cln.install.sh
@@ -2,11 +2,11 @@
 # https://lightning.readthedocs.io/
 
 # https://github.com/ElementsProject/lightning/releases
-#CLVERSION=v0.10.0
+CLVERSION=v0.10.1
 
 # install the latest master by using the last commit id
-# https://github.com/ElementsProject/lightning/commit/063366ed7e3b7cc12a8d1681acc2b639cf07fa23
-CLVERSION="063366ed7e3b7cc12a8d1681acc2b639cf07fa23"
+# https://github.com/ElementsProject/lightning/commit/master
+# CLVERSION="063366ed7e3b7cc12a8d1681acc2b639cf07fa23"
 
 # vars
 source /home/admin/raspiblitz.info
