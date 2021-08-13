b34d409f02751b61c314775c6274bfda3a7d7a17
openoms <oms@tuta.io>
2021-08-13 09:30:57 +0100

    use $lightning and $chain for status display

--- home.admin/config.scripts/blitz.statusscan.sh
+++ home.admin/config.scripts/blitz.statusscan.sh
@@ -272,7 +272,7 @@ fi
 clnRunning=$(systemctl status ${netprefix}lightningd.service 2>/dev/null | grep -c running)
 echo "clnActive=${clnRunning}"
 if [ ${clnRunning} -eq 1 ]; then
-  clnInfo=$(sudo -u bitcoin lightning-cli getinfo)
+  clnInfo=$($lightningcli_alias getinfo)
   clnBlockHeight=$(echo "${clnInfo}" | jq -r '.blockheight' | tr -cd '[[:digit:]]')
   scanProgress=$(echo "scale=2; $clnBlockHeight*100/$total" | bc)
   echo "scanProgress=${scanProgress}"
