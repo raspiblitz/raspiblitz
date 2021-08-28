7a0460da96bdf5949c6f081739c8e1d2b9188dd2
RequestPrivacy <github.cylindroconical@aleeas.com>
2021-08-28 16:08:28 +0200

    Identify newest BlitzPy version during sd building
    The -r (reverse) flag in the ls command leads to an install of the oldest, not the newest version of BlitzPy.

--- build_sdcard.sh
+++ build_sdcard.sh
@@ -609,7 +609,7 @@ sudo -u admin cp -r /home/admin/raspiblitz/home.admin/config.scripts /home/admin
 sudo -u admin chmod +x /home/admin/config.scripts/*.sh
 
 # install newest version of BlitzPy
-blitzpy_wheel=$(ls -trR /home/admin/raspiblitz/home.admin/BlitzPy/dist | grep -E "*any.whl" | tail -n 1)
+blitzpy_wheel=$(ls -tR /home/admin/raspiblitz/home.admin/BlitzPy/dist | grep -E "*any.whl" | tail -n 1)
 blitzpy_version=$(echo ${blitzpy_wheel} | grep -oE "([0-9]\.[0-9]\.[0-9])")
 echo ""
 echo "*** INSTALLING BlitzPy Version: ${blitzpy_version} ***"
