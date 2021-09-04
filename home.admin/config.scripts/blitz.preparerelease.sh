433ae886fa0b947b7409dc49aab6e9ee9cdd0fca
rootzoll <christian@geektank.de>
2021-09-04 21:55:56 +0200

    remove raspiblitz info / logs

--- home.admin/config.scripts/blitz.preparerelease.sh
+++ home.admin/config.scripts/blitz.preparerelease.sh
@@ -3,6 +3,11 @@
 # Just run this script once after a fresh sd card build
 # to prepare the image for release as a downloadable sd card image
 
+# raspiblitz.info & logs
+echo "deleting raspiblitz info & logs ..."
+sudo rm /home/admin/raspiblitz.*
+echo "OK"
+
 # SSH Pubkeys (make unique for every sd card image install)
 echo "deleting SSH Pub keys ..."
 echo "they will get recreated on fresh bootup, by _bootstrap.sh service"
