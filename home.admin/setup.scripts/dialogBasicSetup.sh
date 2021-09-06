37421e46fbe5a7ea187eec96b0db53fd3af971f4
rootzoll <christian@geektank.de>
2021-09-06 20:24:18 +0200

    test ui

--- home.admin/setup.scripts/dialogBasicSetup.sh
+++ home.admin/setup.scripts/dialogBasicSetup.sh
@@ -18,7 +18,7 @@ fi
 OPTIONS+=(FROMBACKUP "Upload Migration Backup")
 OPTIONS+=(SHUTDOWN "Shutdown without Changes")
 
-CHOICE_HEIGHT=$(("${#OPTIONS[@]}/2+2"))
+CHOICE_HEIGHT=$(("${#OPTIONS[@]}/2+1"))
 HEIGHT=$(($CHOICE_HEIGHT+8))
 echo "CHOICE_HEIGHT(${CHOICE_HEIGHT}) HEIGHT(${HEIGHT})"
 sleep 3
