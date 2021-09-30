aaed8c8501156667f3ba3d808600ec48ea502d9e
openoms <oms@tuta.io>
2021-09-30 20:25:28 +0100

    electrs update to v0.9.0   #2548

--- home.admin/config.scripts/bonus.electrs.sh
+++ home.admin/config.scripts/bonus.electrs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # https://github.com/romanz/electrs/releases
-ELECTRSVERSION="v0.9.0-rc2"
+ELECTRSVERSION="v0.9.0"
 # https://github.com/romanz/electrs/commits/master
 # ELECTRSVERSION="3041e89cd2fb377541b929d852ef6298c2d4e60a"
 
