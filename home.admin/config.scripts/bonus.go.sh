7cc3d8a8864db2cade0c63fca1a51ddd497b169b
nyxnor <nyxnor@protonmail.com>
2021-10-27 02:37:08 +0200

    improve go script

--- home.admin/config.scripts/bonus.go.sh
+++ home.admin/config.scripts/bonus.go.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# set version, check: https://github.com/golang/go/tags
+goVersion="1.17.2"
+
 # command info
-if [ $# -eq 0 ] || [ "$1" = "-h" ] || [ "$1" = "-help" ]; then
+if [ -z $1 ] || [ "$1" = "-h" ] || [ "$1" = "-help" ]; then
  echo "config script to install Go"
  echo "bonus.go.sh [on|off]"
  exit 1
@@ -10,40 +13,28 @@ fi
 # switch on
 if [ "$1" = "1" ] || [ "$1" = "on" ]; then
 
-  # set version, check: https://github.com/golang/go/releases 
-  goVersion="1.16.2"
-
-  # get cpu architecture
-  isARM=$(uname -m | grep -c 'arm')
-  isAARCH64=$(uname -m | grep -c 'aarch64')
-  isX86_64=$(uname -m | grep -c 'x86_64')
+  # get Go vars - needed if there was no log-out since Go installed
+  . /etc/profile
 
   # make sure go is installed
-
-  # get Go vars - needed if there was no log-out since Go installed
-  source /etc/profile
-
   echo "Check Framework: Go"
   goInstalled=$(go version 2>/dev/null | grep -c 'go')
-  if [ ${goInstalled} -eq 0 ];then
-    if [ ${isARM} -eq 1 ] ; then
-      goOSversion="armv6l"
-    fi
-    if [ ${isAARCH64} -eq 1 ] ; then
-      goOSversion="arm64"
-    fi
-    if [ ${isX86_64} -eq 1 ] ; then
-      goOSversion="amd64"
-    fi 
+  if [ -z ${goInstalled} ];then
+
+    architecture="$(uname -m)"
+    case "${architecture}" in
+      arm) goOSversion="armv6l";;
+      aarch64) goOSversion="arm64";;
+      x86_64) goOSversion="amd64";;
+      *) echo "Not available for architecture=$architecture"; exit 1
+    esac
 
     echo "*** Installing Go v${goVersion} for ${goOSversion} ***"
-
     # wget https://storage.googleapis.com/golang/go${goVersion}.linux-${goOSversion}.tar.gz
     cd /home/admin/download
     wget https://dl.google.com/go/go${goVersion}.linux-${goOSversion}.tar.gz
-    if [ ! -f "./go${goVersion}.linux-${goOSversion}.tar.gz" ]
-    then
-        echo "!!! FAIL !!! Download not success."
+    if [ ! -f "./go${goVersion}.linux-${goOSversion}.tar.gz" ]; then
+        echo "!!! FAIL !!! Download failed."
         rm -f go${goVersion}.linux-${goOSversion}.tar.gz*
         exit 1
     fi
@@ -58,33 +49,32 @@ if [ "$1" = "1" ] || [ "$1" = "on" ]; then
     if [ $(cat /etc/profile | grep -c "GOROOT=") -eq 0 ]; then
       sudo bash -c "echo 'GOROOT=/usr/local/go' >> /etc/profile"
       sudo bash -c "echo 'PATH=\$PATH:\$GOROOT/bin/' >> /etc/profile"
-      sudo bash -c "echo 'GOPATH=/usr/local/gocode' >> /etc/profile"   
+      sudo bash -c "echo 'GOPATH=/usr/local/gocode' >> /etc/profile"
       sudo bash -c "echo 'PATH=\$PATH:\$GOPATH/bin/' >> /etc/profile"
     fi
-
     # set GOPATH https://github.com/golang/go/wiki/SettingGOPATH
     go env -w GOPATH=/usr/local/gocode
-    
     goInstalled=$(go version 2>/dev/null | grep -c 'go')
   fi
-  if [ ${goInstalled} -eq 0 ];then
+
+  if [ -z ${goInstalled} ];then
     echo "FAIL: Was not able to install Go"
     sleep 4
     exit 1
   fi
 
   correctGoVersion=$(go version | grep -c "go${goVersion}")
-  if [ ${correctGoVersion} -eq 0 ]; then
+  if [ -z ${correctGoVersion} ]; then
     echo "WARNING: You work with an untested version of GO - should be ${goVersion} .. trying to continue"
     go version
     sleep 3
-    echo ""
+    echo
   fi
 
   # setting value in raspi blitz config
-  echo ""
+  echo
   echo "Installed $(go version)"
-  echo ""
+  echo
   exit 0
 fi
 
