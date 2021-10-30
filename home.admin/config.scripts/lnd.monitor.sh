9dadf60e0cfb7c3d7edd85cde95c30567dc4af44
rootzoll <christian@geektank.de>
2021-10-30 16:57:45 +0200

    test lnd.monitor

---
+++ home.admin/config.scripts/lnd.monitor.sh
@@ -0,0 +1,186 @@
+#!/bin/bash
+
+# command info
+if [ $# -eq 0 ] || [ "$1" = "-h" ] || [ "$1" = "-help" ]; then
+ echo "monitor and troubleshot the lnd network"
+ echo "lnd.monitor.sh [mainnet|testnet|signet] status"
+ echo "lnd.monitor.sh [mainnet|testnet|signet] config"
+ echo "lnd.monitor.sh [mainnet|testnet|signet] info"
+ echo "lnd.monitor.sh [mainnet|testnet|signet] wallet"
+ echo "lnd.monitor.sh [mainnet|testnet|signet] channels"
+ echo "lnd.monitor.sh [mainnet|testnet|signet] fees"
+ exit 1
+fi
+
+# check if started with sudo
+if [ "$EUID" -ne 0 ]; then 
+  echo "error='run as root'"
+  exit 1
+fi
+
+lndHomeDir="/home/bitcoin/.lnd"
+# /usr/local/bin/lncli -n=mainnet --rpcserver=localhost:10009 --macaroonpath=/home/bitcoin/.lnd/data/chain/bitcoin/mainnet/readonly.macaroon --tlscertpath=/home/bitcoin/.lnd/tls.cert
+# set based on network type
+if [ "$1" == "mainnet" ]; then
+  lndcli_alias="/usr/local/bin/lncli -n=mainnet --rpcserver=localhost:10009 --macaroonpath=${lndHomeDir}/data/chain/bitcoin/mainnet/readonly.macaroon --tlscertpath=${lndHomeDir}/tls.cert"
+  netprefix=""
+elif [ "$1" == "testnet" ]; then
+  lndcli_alias="/usr/local/bin/lncli -n=mainnet --rpcserver=localhost:10009 --macaroonpath=${lndHomeDir}/data/chain/bitcoin/testnet/readonly.macaroon --tlscertpath=${lndHomeDir}/tls.cert"
+  netprefix="t"
+elif [ "$1" == "signet" ]; then
+  lndcli_alias="/usr/local/bin/lncli -n=mainnet --rpcserver=localhost:10009 --macaroonpath=${lndHomeDir}/data/chain/bitcoin/signet/readonly.macaroon --tlscertpath=${lndHomeDir}/tls.cert"
+  netprefix="s"
+else
+  echo "error='not supported net'"
+  exit 1
+fi
+
+######################################################
+# STATUS
+# check general status info
+######################################################
+
+if [ "$2" = "status" ]; then
+
+  lnd_version=$($lndcli_alias --version 2>/dev/null | cut -d ' ' -f3)
+  lnd_running=$(systemctl status ${netprefix}lnd 2>/dev/null | grep -c "active (running)")
+  lnd_ready="0"
+  lnd_online="0"
+  lnd_error_short=""
+  lnd_error_full=""
+
+  if [ "${lnd_running}" != "0" ]; then
+    lnd_running="1"
+
+    # test connection - record win & fail info
+    randStr=$(echo "$RANDOM")
+    rm /var/cache/raspiblitz/.lnd-${randStr}.out 2>/dev/null
+    rm /var/cache/raspiblitz/.lnd-${randStr}.error 2>/dev/null
+    touch /var/cache/raspiblitz/.lnd-${randStr}.out
+    touch /var/cache/raspiblitz/.lnd-${randStr}.error
+    $lndcli_alias getinfo 1>/var/cache/raspiblitz/.lnd-${randStr}.out 2>/var/cache/raspiblitz/.lnd-${randStr}.error
+    winData=$(cat /var/cache/raspiblitz/.lnd-${randStr}.out 2>/dev/null)
+    failData=$(cat /var/cache/raspiblitz/.lnd-${randStr}.error 2>/dev/null)
+    rm /var/cache/raspiblitz/.lnd-${randStr}.out
+    rm /var/cache/raspiblitz/.lnd-${randStr}.error
+
+    # check for errors
+    if [ "${failData}" != "" ]; then
+      lnd_ready="0"
+      lnd_error_short=""
+      lnd_error_full=$(echo ${failData} | tr -d "'")
+      lnd_ready="0"
+
+    # check results if proof for online
+    else
+      lnd_ready="1"
+      connections=$( echo "${winData}" | grep "num_peers\"" | tr -cd '[[:digit:]]')
+      if [ "${connections}" != "" ] && [ "${connections}" != "0" ]; then
+        lnd_online="1"
+      fi
+    fi
+
+  fi 
+
+  # print results
+  echo "lnd_version='${lnd_version}'"
+  echo "lnd_running='${lnd_running}'"
+  echo "lnd_ready='${lnd_ready}'"
+  echo "lnd_online='${lnd_online}'"
+  echo "lnd_error_short='${lnd_error_short}'"
+  echo "lnd_error_full='${lnd_error_full}'"
+
+  exit 0
+fi   
+
+######################################################
+# CONFIG
+######################################################
+
+if [ "$2" = "config" ]; then
+
+  # get data
+  lndConfigData=$(cat $lndHomeDir/${netprefix}lnd.conf)
+  if [ "${lndConfigData}" == "" ]; then
+    echo "error='no config'"
+    exit 1
+  fi
+
+  # parse data
+  lnd_alias=$( echo "${lndConfigData}" | grep "^alias=*" | cut -f2 -d=)
+
+  # print data
+  echo "lnd_alias='${lnd_alias}'"
+  exit 0
+fi
+
+
+######################################################
+# INFO
+######################################################
+
+if [ "$2" = "info" ]; then
+
+  # get data
+  ln_getInfo=$($lndcli_alias getinfo 2>/dev/null)
+  if [ "${ln_getInfo}" == "" ]; then
+    echo "error='no data'"
+    exit 1
+  fi
+
+  # parse data
+  lnd_address=$(echo "${ln_getInfo}" | grep "uris" -A 1 | tr -d '\n' | cut -d '"' -f4)
+  lnd_tor=$(echo "${lnd_address}" | grep -c ".onion")
+  lnd_sync_chain=$(echo "${ln_getInfo}" | grep "synced_to_chain" | grep "true" -c)
+  lnd_sync_graph=$(echo "${ln_getInfo}" | grep "synced_to_graph" | grep "true" -c)
+  lnd_channels_pending=$(echo "${ln_getInfo}" | jq -r '.num_pending_channels')
+  lnd_channels_active=$(echo "${ln_getInfo}" | jq -r '.num_active_channels')
+  lnd_channels_inactive=$(echo "${ln_getInfo}" | jq -r '.num_inactive_channels')
+  lnd_peers=$(echo "${ln_getInfo}" | jq -r '.num_peers')
+
+  # print data
+  echo "lnd_address='${lnd_address}'"
+  echo "lnd_tor='${lnd_tor}'"
+  echo "lnd_sync_chain='${lnd_sync_chain}'"
+  echo "lnd_sync_graph='${lnd_sync_graph}'"
+  echo "lnd_channels_pending='${lnd_channels_pending}'"
+  echo "lnd_channels_active='${lnd_channels_active}'"
+  echo "lnd_channels_inactive='${lnd_channels_inactive}'"
+  echo "lnd_peers='${lnd_peers}'"
+  exit 0
+  
+fi
+
+######################################################
+# WALLETS
+######################################################
+
+if [ "$2" = "wallet" ]; then
+
+  exit 0
+fi
+
+######################################################
+# CHANNELS
+######################################################
+
+if [ "$2" = "channels" ]; then
+
+  exit 0
+  
+fi
+
+######################################################
+# FEES
+######################################################
+
+if [ "$2" = "fees" ]; then
+
+  exit 0
+  
+fi
+
+echo "FAIL - Unknown Parameter $2"
+exit 1
+
+
