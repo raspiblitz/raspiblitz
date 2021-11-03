42c72707d487e841042c4a52a6b92e2d75771edb
rootzoll <christian@geektank.de>
2021-11-03 21:01:35 +0100

    fix full file

--- home.admin/00infoBlitz.sh
+++ home.admin/00infoBlitz.sh
@@ -126,3 +126,207 @@ if [ "${btc_peers}" != "" ] && [  ${btc_peers} -gt 0 ]; then
 else
   networkConnectionsInfo="${color_red}${btc_peers} ${color_gray}peers"
 fi
+
+#######################
+# LIGHTNING INFO
+
+# default values
+ln_alias=${hostname}
+ln_baseInfo="-"
+ln_channelInfo="\n"
+ln_external="\n"
+ln_feeReport""
+ln_peersInfo=""
+ln_version=""
+ln_publicColor="${color_green}"
+
+if [ "${lightning}" != "" ]; then
+
+  source <(/home/admin/_cache.sh meta ln_${lightning}_${chain}net_version)
+  ln_version="${value}"
+
+  # get alias
+  source <(/home/admin/_cache.sh meta ln_${lightning}_${chain}net_alias)
+  if [ "${value}" != "" ]; then
+    ln_alias="${value}"
+  fi
+
+  # consider tor address green for public
+  # when not Tor use yellow because not sure if public
+  if [ "${runBehindTor}" != "on" ]; then
+    ln_publicColor="${color_yellow}"
+  fi
+
+  # get the public address/URI
+  source <(/home/admin/_cache.sh meta ln_${lightning}_${chain}net_address)
+  ln_external="${value}"
+
+  source <(/home/admin/_cache.sh meta ln_${lightning}_${chain}net_peers)
+  ln_peersInfo="${color_green}${value} ${color_gray}peers"
+
+  source <(/home/admin/_cache.sh meta ln_${lightning}_${chain}net_ready)
+  ln_ready="${value}"
+  source <(/home/admin/_cache.sh meta ln_${lightning}_${chain}net_sync_chain)
+  ln_sync="${value}"
+  source <(/home/admin/_cache.sh meta ln_${lightning}_${chain}net_locked)
+  ln_locked="${value}"
+
+  # lightning is still starting
+  if [ "${ln_ready}" != "1" ]; then
+
+    ln_baseInfo="${color_red} Not Started | Not Ready Yet"
+
+  # lightning is still syncing
+  elif [ "${ln_locked}" != "1" ]; then
+
+      ln_baseInfo="${color_amber} Wallet Locked"
+
+  # lightning is still syncing
+  elif [ "${ln_sync}" != "1" ]; then
+
+    source <(/home/admin/_cache.sh meta ln_${lightning}_${chain}net_sync_progress)
+    ln_syncprogress="${value}"
+    ln_baseInfo="${color_amber} Scanning blocks: ${ln_syncprogress}%"
+
+  # OK lightning is ready - get more details
+  else
+
+    # create fee report
+    source <(/home/admin/_cache.sh meta ln_${lightning}_${chain}net_fees_daily)
+    ln_dailyfees="${value}"
+    source <(/home/admin/_cache.sh meta ln_${lightning}_${chain}net_fees_weekly)
+    ln_weeklyfees="${value}"
+    source <(/home/admin/_cache.sh meta ln_${lightning}_${chain}net_fees_month)
+    ln_monthlyfees="${value}"
+    ln_feeReport="Fee Report (D-W-M): ${color_green}${ln_dailyfees}-${ln_weeklyfees}-${ln_monthlyfees} ${color_gray}sat"
+
+    # on-chain wallet info
+    ln_pendingonchain=""
+    source <(/home/admin/_cache.sh meta ln_${lightning}_${chain}net_wallet_onchain_pending)
+    ln_onchain_pending="${value}"
+    if [ "${ln_onchain_pending}" != "" ] && [ ${ln_onchain_pending} -gt 0 ]; then ln_pendingonchain=" (+${ln_onchain_pending})"; fi
+    source <(/home/admin/_cache.sh meta ln_${lightning}_${chain}net_wallet_onchain_balance)
+    ln_walletbalance="${value}"
+    ln_baseInfo="${color_gray}Wallet ${ln_walletbalance} ${netprefix}sat ${ln_pendingonchain}"
+
+    # channel pending info
+    ln_channelbalance_pending=""
+    source <(/home/admin/_cache.sh meta ln_${lightning}_${chain}net_wallet_channels_pending)
+    ln_channels_pending="${value}"
+    if [ "${ln_channels_pending}" != "" ] && [ ${ln_channels_pending} -gt 0 ]; then ln_channelbalance_pending=" (+${ln_channels_pending})"; fi
+
+    # get channel infos
+    source <(/home/admin/_cache.sh meta ln_${lightning}_${chain}net_wallet_channels_balance)
+    ln_channels_balance="${value}"
+    source <(/home/admin/_cache.sh meta ln_${lightning}_${chain}net_channels_active)
+    ln_channels_online="${value}"
+    source <(/home/admin/_cache.sh meta ln_${lightning}_${chain}net_channels_total)
+    ln_channels_total="${value}"
+
+    # construct channel info string
+    ln_channelInfo="${ln_channels_online}/${ln_channels_total} Channels ${ln_channels_balance} ${netprefix}sat${ln_channelbalance_pending}"
+  fi
+
+fi
+
+# show JoinMarket stats in place of the LND URI only if the Yield Generator is running
+source /home/joinmarket/joinin.conf 2>/dev/null
+if [ "${joinmarket}" = "on" ] && [ $(sudo -u joinmarket pgrep -f "python yg-privacyenhanced.py $YGwallet --wallet-password-stdin" 2>/dev/null | wc -l) -gt 2 ]; then
+  trap 'rm -f "$JMstats"' EXIT
+  JMstats=$(mktemp -p /dev/shm)
+  sudo -u joinmarket /home/joinmarket/info.stats.sh > $JMstats
+  JMstatsL1=$(sed -n 1p < "$JMstats")
+  JMstatsL2=$(sed -n 2p < "$JMstats")
+  JMstatsL3=$(sed -n 3p < "$JMstats")
+  JMstatsL4=$(sed -n 4p < "$JMstats")
+  lastLine="\
+${color_gray}
+${color_gray}     ╦╔╦╗      ${color_gray}$JMstatsL1
+${color_gray}     ║║║║      ${color_gray}$JMstatsL2
+${color_gray}    ╚╝╩ ╩      ${color_gray}$JMstatsL3
+${color_gray}  ◎=◎=◎=◎=◎    ${color_gray}$JMstatsL4"
+else
+    lastLine="\
+${color_yellow}
+${color_yellow}${ln_publicColor}${ln_external}${color_gray}"
+fi
+
+if [ "${lightning}" == "cl" ]; then
+  LNline="C-LIGHTNING ${color_green}${ln_version}\n               ${ln_baseInfo}"
+elif [ "${lightning}"  == "lnd" ]; then
+  LNline="LND ${color_green}${ln_version} ${ln_baseInfo}"
+fi
+
+LNinfo=" + Lightning Network"
+if [ "${lightning}" == "" ]; then
+  LNinfo=""  
+fi
+
+## get uptime and current date & time
+uptime=$(uptime --pretty)
+datetime=$(date -R)
+
+stty sane
+sleep 1
+clear
+
+printf "
+${color_yellow}
+${color_yellow}
+${color_yellow}
+${color_yellow}               ${color_amber}%s ${color_green} ${ln_alias} ${upsInfo}
+${color_yellow}               ${color_gray}${network^} Fullnode${LNinfo} ${torInfo}
+${color_yellow}        ,/     ${color_yellow}%s
+${color_yellow}      ,'/      ${color_gray}%s
+${color_yellow}    ,' /       ${color_gray}%s, temp %s°C %s°F
+${color_yellow}  ,'  /_____   ${color_gray}Free Mem ${color_ram}${ram} ${color_gray} HDDuse ${color_hdd}%s${color_gray}
+${color_yellow},'_____    ,'  ${color_gray}SSH admin@${color_green}${internet_localip}${color_gray} d${network_rx} u${network_tx}
+${color_yellow}      /  ,'    ${color_gray}
+${color_yellow}     / ,'      ${color_gray}${network} ${color_green}${networkVersion} ${color_gray}${chain}net ${networkConnectionsInfo}
+${color_yellow}    /,'        ${color_gray}Blocks ${blockInfo} ${color_gray}Sync ${sync_color}${sync} %s
+${color_yellow}   /'          ${color_gray}
+${color_yellow}               ${color_gray}${LNline}
+${color_yellow}               ${color_gray}${ln_channelInfo} ${ln_peersInfo}
+${color_yellow}               ${color_gray}${ln_feeReport}
+$lastLine
+" \
+"RaspiBlitz v${codeVersion}" \
+"-------------------------------------------" \
+"Refreshed: ${datetime}" \
+"CPU load${system_cpu_load##up*,  }" "${system_temp_celsius}" "${system_temp_fahrenheit}" \
+"${hdd_used_info}" "${sync_percentage}"
+
+if [ ${#undervoltageReports} -gt 0 ] && [ "${undervoltageReports}" != "0" ]; then
+  echo "${undervoltageReports} undervoltage reports - run 'Hardware Test' in menu"
+elif [ ${#ups} -gt 1 ] && [ "${upsStatus}" = "n/a" ]; then
+  echo "UPS service activated but not running"
+else
+
+  # checking status of apps and display if in sync or problems
+  appInfoLine=""
+
+  # Electrum Server - electrs
+  if [ "${ElectRS}" = "on" ]; then
+    error=""
+    source <(sudo /home/admin/config.scripts/bonus.electrs.sh status 2>/dev/null)
+    if [ ${#infoSync} -gt 0 ]; then
+      appInfoLine="Electrum: ${infoSync}"
+    fi
+  fi
+
+  # BTC RPC EXPLORER
+  if [ "${BTCRPCexplorer}" = "on" ]; then
+    error=""
+    source <(sudo /home/admin/config.scripts/bonus.btc-rpc-explorer.sh status 2>/dev/null)
+    if [ ${#error} -gt 0 ]; then
+      appInfoLine="ERROR BTC-RPC-Explorer: ${error} (try restart)"
+    elif [ "${isIndexed}" = "0" ]; then
+      appInfoLine="BTC-RPC-Explorer: ${indexInfo}"
+    fi
+  fi
+
+  if [ ${#appInfoLine} -gt 0 ]; then
+    echo "${appInfoLine}"
+  fi
+
+fi
