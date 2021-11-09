c58d456616cf32dd30d73bf9fd979fbd5d9b0394
openoms <oms@tuta.io>
2021-11-09 12:30:08 +0000

    git-verify: add verify-tag and examples do not break if running recovery

--- home.admin/config.scripts/blitz.git-verify.sh
+++ home.admin/config.scripts/blitz.git-verify.sh
@@ -2,18 +2,33 @@
 
 # command info
 if [ $# -lt 3 ] || [ "$1" = "-h" ] || [ "$1" = "-help" ]; then
-  echo "script use to verify git commits"
+  echo "script use to verify a git commit or tag"
   echo "Usage:"
   echo "Run after 'git reset --hard VERSION' with the user running the installation"
+  echo "To verify the checked out commit:"
   echo "blitz.git-verify.sh [PGPsigner] [PGPpubkeyLink] [PGPpubkeyFingerprint]"
+  echo "To use 'git verify-tag' add the 'tag':"
+  echo "blitz.git-verify.sh [PGPsigner] [PGPpubkeyLink] [PGPpubkeyFingerprint] <tag>"
   exit 1
 fi
+
+# Example for commits created on GitHub:
+# PGPsigner="web-flow"
+# PGPpubkeyLink="https://github.com/${PGPsigner}.gpg"
+# PGPpubkeyFingerprint="4AEE18F83AFDEB23"
+
+# Example for commits signed with a personal PGP key:
+# PGPsigner="janoside"
+# PGPpubkeyLink="https://github.com/${PGPsigner}.gpg"
+# PGPpubkeyFingerprint="F579929B39B119CC7B0BB71FB326ACF51F317B69"
+
+# Run with the insatting user to clear permissions:
+# sudo -u btcrpcexplorer /home/admin/config.scripts/blitz.git-verify.sh \
+#  "${PGPsigner}" "${PGPpubkeyLink}" "${PGPpubkeyFingerprint}" || exit 1
 
 PGPsigner="$1"
 PGPpubkeyLink="$2"
 PGPpubkeyFingerprint="$3"
-
-commitHash="$(git log --oneline | head -1 | awk '{print $1}')"
 
 wget -O pgp_keys.asc "${PGPpubkeyLink}"
 gpg --import --import-options show-only ./pgp_keys.asc
@@ -22,20 +37,34 @@ if [ "${fingerprint}" -lt 1 ]; then
   echo
   echo "# !!! WARNING --> the PGP fingerprint is not as expected for ${PGPsigner}"
   echo "# Should contain PGP: ${PGPpubkeyFingerprint}"
-  echo "# Type: 'Accept risk' and press ENTER to TAKE THE RISK if you think all is OK"
-  read -r confirmation
-  if [ "$confirmation" != "Accept risk" ]; then exit 1; fi
+  source /home/admin/raspiblitz.info
+  if [ "${state}" == "ready" ]; then
+    echo "# Type: 'Accept risk' and press ENTER to TAKE THE RISK if you think all is OK"
+    read -r confirmation
+    if [ "$confirmation" != "Accept risk" ]; then exit 1; fi
+  else
+    echo "# Exiting"
+    exit 1
+  fi
 fi
 gpg --import ./pgp_keys.asc
 
 trap 'rm -f "$_temp"' EXIT
 _temp="$(mktemp -p /dev/shm/)"
 
-if git verify-commit "$commitHash" 2>&1 >&"$_temp"; then
+if [ $# -eq 3 ]; then
+  commitHash="$(git log --oneline | head -1 | awk '{print $1}')"
+  gitCommand="git verify-commit $commitHash"
+elif [ $# -eq 4 ]; then
+  gitCommand="git verify-tag $4"
+fi
+if ${gitCommand} 2>&1 >&"$_temp"; then
   goodSignature=1
 else
   goodSignature=0
 fi
+echo
+cat $_temp
 echo "# goodSignature(${goodSignature})"
 
 correctKey=$(tr -d " \t\n\r" < "$_temp" | grep "${PGPpubkeyFingerprint}" -c)
