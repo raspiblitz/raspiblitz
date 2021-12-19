34b54f0553308b0a721a5e959ad7d1fe425f795d
openoms <oms@tuta.io>
2021-12-19 22:56:30 +0000

    lnd.setname: add alias if not present and set

--- home.admin/config.scripts/lnd.setname.sh
+++ home.admin/config.scripts/lnd.setname.sh
@@ -2,15 +2,32 @@
 
 # command info
 if [ "$1" = "-h" ] || [ "$1" = "-help" ]; then
- echo "small config script to set a alias of LND (and hostname of raspi)"
- echo "lnd.setname.sh [mainnet|testnet|signet] [?newName] [?forceHostname]"
- exit 1
+  echo "small config script to set a alias of LND (and hostname of raspi)"
+  echo "lnd.setname.sh [mainnet|testnet|signet] [?newName] [?forceHostname]"
+  exit 1
 fi
 
 # 1. parameter [?newName]
 newName=$2
 
-source <(/home/admin/config.scripts/network.aliases.sh getvars $2)
+source <(/home/admin/config.scripts/network.aliases.sh getvars lnd $1)
+
+function setting() # FILE LINENUMBER NAME VALUE
+{
+  FILE=$1
+  LINENUMBER=$2
+  NAME=$3
+  VALUE=$4
+  settingExists=$(cat ${FILE} | grep -c "^${NAME}=")
+  echo "# setting ${FILE} ${LINENUMBER} ${NAME} ${VALUE}"
+  echo "# ${NAME} exists->(${settingExists})"
+  if [ "${settingExists}" == "0" ]; then
+    echo "# adding setting (${NAME})"
+    sudo -u bitcoin sed -i "${LINENUMBER}i${NAME}=" ${FILE}
+  fi
+  echo "# updating setting (${NAME}) with value(${VALUE})"
+  sudo -u bitcoin sed -i "s/^${NAME}=.*/${NAME}=${VALUE}/g" ${FILE}
+}
 
 # run interactive if 'turn on' && no further parameters
 if [ ${#newName} -eq 0 ]; then
@@ -27,27 +44,21 @@ if [ ${#newName} -eq 0 ]; then
 fi
 
 # lnd conf file
-lndConfig="/mnt/hdd/lnd/${netprefix}lnd.conf"
+lndConfFile="/mnt/hdd/lnd/${netprefix}lnd.conf"
 
 # check if lnd config file exists
-configExists=$(ls ${lndConfig} | grep -c '.conf')
+configExists=$(ls ${lndConfFile} | grep -c '.conf')
 if [ ${configExists} -eq 0 ]; then
- echo "FAIL - missing ${lndConfig}"
- exit 1
+  echo "FAIL - missing ${lndConfFile}"
+  exit 1
 fi
 
-# make sure entry line for 'alias' exists 
-entryExists=$(cat ${lndConfig} | grep -c 'alias=')
-if [ ${entryExists} -eq 0 ]; then
-  echo "alias=" >> ${lndConfig}
-fi
-
-# stop services
-echo "making sure services are not running"
-sudo systemctl stop ${netprefix}lnd 2>/dev/null
+sectionLine=$(cat ${lndConfFile} | grep -n "^\[Application Options\]" | cut -d ":" -f1)
+echo "# sectionLine(${sectionLine})"
+insertLine=$(expr $sectionLine + 1)
 
 # lnd.conf: change name
-sudo sed -i "s/^alias=.*/alias=${newName}/g" ${lndConfig}
+setting ${lndConfFile} ${insertLine} "alias" "${newName}"
 
 # raspiblitz.conf: change name
 /home/admin/config.scripts/blitz.conf.sh set hostname "${newName}"
@@ -72,5 +83,9 @@ fi
 #  fi
 #fi
 
-echo "needs reboot to run normal again"
+echo
+echo "# To activate the new alias:"
+echo "# reboot or restart the lnd.service and unlock with:"
+echo "'sudo systemctl restart lnd && lncli unlock'"
+echo "# Either way it can take hours for the gossip to propagate."
 exit 0
