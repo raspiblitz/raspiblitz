8edaa3dbe03e067919d9c51a4bd777c8c9053c68
openoms <oms@tuta.io>
2021-12-20 08:34:43 +0000

    fix lndlog alias check

--- home.admin/config.scripts/lnd.install.sh
+++ home.admin/config.scripts/lnd.install.sh
@@ -316,7 +316,7 @@ alias ${netprefix}lncli=\"sudo -u bitcoin /usr/local/bin/lncli\
  -n=${CHAIN} --rpcserver localhost:1${rpcportmod}009\"\
 " | sudo tee -a /home/admin/_aliases
   fi
-  if [ $(grep -c "alias ${netprefix}lncli" < /home/admin/_aliases) -eq 0 ];then 
+  if [ $(grep -c "alias ${netprefix}lndlog" < /home/admin/_aliases) -eq 0 ];then 
     echo "\
 alias ${netprefix}lndlog=\"sudo tail -n 30 -f /mnt/hdd/lnd/logs/${network}/${CHAIN}/lnd.log\"\
 " | sudo tee -a /home/admin/_aliases
