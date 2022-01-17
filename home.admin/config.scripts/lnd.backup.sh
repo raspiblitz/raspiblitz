df4f835cb586baf115e5e7bf66a458abc6b58420
openoms <oms@tuta.io>
2022-01-17 20:10:38 +0000

    lnd seedwords not BIP39, skip test #2832

--- home.admin/config.scripts/lnd.backup.sh
+++ home.admin/config.scripts/lnd.backup.sh
@@ -517,26 +517,8 @@ if [ ${mode} = "seed-import-gui" ]; then
       # check correct number of words
       wordcount=$(echo "${wordstring}" | wc -w)
       if [ ${wordcount} -eq 24 ]; then
-
-        # check if words are valid seed
-        source <(python /home/admin/config.scripts/blitz.mnemonic.py test "${wordstring}")
-        if [ "${valid}" == "0" ]; then
-          whiptail --title " WARNING " --yes-button "Try Again" --no-button "Cancel" --yesno "
-The word list has 24 words BUT its not a
-valid seed word list by our test.
-
-Please check for typos.
-
-" 12 52
-	        if [ $? -eq 1 ]; then
-            clear
-            echo "# CANCEL empty results in: ${RESULTFILE}"
-            exit 1
-	        fi
-        else
           echo "OK - 24 words"
           wordsCorrect=1
-        fi
       else
         whiptail --title " WARNING " \
 			  --yes-button "Try Again" \
@@ -545,8 +527,8 @@ Please check for typos.
 The word list has ${wordcount} words. But it must be 24.
 Please check your list and try again.
 
-Best is to write words in external editor 
-and then copy and paste them into dialog.
+Best is to write words in an external editor 
+and then copy and paste them into the dialog.
 
 The word list should look like this:
 wordone wordtwo wordthree ...
