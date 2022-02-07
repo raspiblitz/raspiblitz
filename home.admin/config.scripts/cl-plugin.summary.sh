dfe0b69c9967b5a6fb6123ae08547d69202e7d84
openoms <oms@tuta.io>
2022-02-07 11:20:33 +0000

    remove notes

--- home.admin/config.scripts/cl-plugin.summary.sh
+++ home.admin/config.scripts/cl-plugin.summary.sh
@@ -43,27 +43,3 @@ echo
 if [ "$(echo "$@" | grep -c "runonce")" -gt 0 ];then
   $lightningcli_alias plugin stop /home/bitcoin/cl-plugins-available/plugins/summary/summary.py
 fi
-
-
-# set PATH so it includes user's private bin if it exists
-if [ -d "$HOME/.local/lib/python3.9/site-packages" ] ; then
-  PATH="$HOME/.local/lib/python3.9/site-packages:$PATH"
-fi
-/home/bitcoin/.local/lib/python3.9/site-packages
-
-# https://docs.python.org/3/library/site.html
-echo "/home/bitcoin/.local/lib/python3.9/site-packages/" | sudo tee /usr/local/lib/python3.9/bitcoin.pth
-
-/usr/local/lib/python3.9/site-packages 
-/usr/local/lib/python3.9
-
-# stop plugin
-if [ $($lightningcli_alias | grep -c "summary") -gt 0 ];then
-  $lightningcli_alias plugin stop -H /home/bitcoin/cl-plugins-available/plugins/summary/summary.py
-fi
-# uninstall user packages only
-sudo -u bitcoin pip uninstall -r /home/bitcoin/cl-plugins-available/plugins/summary/requirements.txt
-sudo rm -rf  /home/bitcoin/cl-plugins-available/plugins
-or to update 
-cd  /home/bitcoin/cl-plugins-available/plugins
-sudo -u bitcoin git pull
