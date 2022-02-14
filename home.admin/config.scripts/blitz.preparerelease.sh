4070ecf8c21cd5c1837543b310ca0bc3d1085c2e
openoms <oms@tuta.io>
2022-02-14 14:49:01 +0000

    reset wifi config for release #1371 the config in /boot/wpa_supplicant.conf is  moved to /etc/wpa_supplicant/wpa_supplicant.conf during boot so both should be deleted

--- home.admin/config.scripts/blitz.preparerelease.sh
+++ home.admin/config.scripts/blitz.preparerelease.sh
@@ -20,25 +20,29 @@ echo "cpu=${cpu}" >> /home/admin/raspiblitz.info
 echo "displayClass=${displayClass}" >> /home/admin/raspiblitz.info
 
 # SSH Pubkeys (make unique for every sd card image install)
-echo ""
+echo
 echo "deleting SSH Pub keys ..."
 echo "they will get recreated on fresh bootup, by _bootstrap.sh service"
 sudo rm /etc/ssh/ssh_host_*
 echo "OK"
 
 # https://github.com/rootzoll/raspiblitz/issues/1068#issuecomment-599267503
-echo ""
+echo
 echo "deleting local DNS confs ..."
 sudo rm /etc/resolv.conf
 echo "OK"
 
 # https://github.com/rootzoll/raspiblitz/issues/1371
-echo ""
+echo
 echo "deleting local WIFI conf ..."
 sudo rm /boot/wpa_supplicant.conf 2>/dev/null
+# reset entries
+echo "ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev
+update_config=1
+country=US" | sudo tee /etc/wpa_supplicant/wpa_supplicant.conf  2>/dev/null
 echo "OK"
 
-echo " "
+echo
 echo "Will shutdown now."
 echo "Wait until Raspberry LEDs show no activity anymore."
 echo "Then remove SD card and make an release image from it."
