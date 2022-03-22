72198d8a28eaeabc10ce07db9faddf93b9507d50
Oliver Gugger <gugger@gmail.com>
2022-03-22 22:19:53 +0100

    chantools update to v0.10.3

--- home.admin/config.scripts/bonus.chantools.sh
+++ home.admin/config.scripts/bonus.chantools.sh
@@ -6,7 +6,7 @@
 
 lndVersion=$(lncli -v | cut -d " " -f 3 | cut -d"." -f2)
 if [ $lndVersion -eq 14 ]; then
-  pinnedVersion="0.10.1"
+  pinnedVersion="0.10.3"
 else
   echo "# LND not installed or a version not tested with chantools"
   lncli -v
