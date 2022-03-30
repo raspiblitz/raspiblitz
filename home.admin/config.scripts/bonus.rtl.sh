3fdb30ffeff72099101c533d5f6fc5b0396e5685
rootzoll <christian@geektank.de>
2022-03-30 23:35:20 +0200

    fix address name

--- home.admin/config.scripts/bonus.rtl.sh
+++ home.admin/config.scripts/bonus.rtl.sh
@@ -61,7 +61,7 @@ if [ "$1" = "status" ] || [ "$1" = "menu" ]; then
     echo "installed='${isInstalled}'"
     echo "localIP='${localip}'"
     echo "httpPort='${RTLHTTP}'"
-    echo "toraddres='${toraddres}'"
+    echo "toraddreds='${toraddres}'"
     exit
   fi
 fi
