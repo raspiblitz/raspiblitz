2d222ca3137a670424d60af06ec695f1be62ffdb
cercatrova <63446299+cercatrova21@users.noreply.github.com>
2022-05-07 19:02:25 +0200

    Update btcpayserver to 1.5.1 (#3096)
    Tested, no issues so far

--- home.admin/config.scripts/bonus.btcpayserver.sh
+++ home.admin/config.scripts/bonus.btcpayserver.sh
@@ -5,7 +5,7 @@
 # https://github.com/dgarage/NBXplorer/tags
 NBXplorerVersion="v2.2.20"
 # https://github.com/btcpayserver/btcpayserver/releases
-BTCPayVersion="v1.4.4"
+BTCPayVersion="v1.5.1"
 
 PGPsigner="nicolasdorier"
 PGPpubkeyLink="https://keybase.io/nicolasdorier/pgp_keys.asc"
