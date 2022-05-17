ed711c03978dac4799659a94851945132cda2c60
Tracachang <54238558+Tracachang@users.noreply.github.com>
2022-05-17 19:31:27 +0000

    disable password auth btc-rpc-explorer (#2373)
    By default password it is not activated, so I think it should be disabled since the script already create a specific user to run this service and rpc commands are disabled by default.

--- home.admin/config.scripts/bonus.btc-rpc-explorer.sh
+++ home.admin/config.scripts/bonus.btc-rpc-explorer.sh
@@ -245,7 +245,7 @@ BTCEXP_BITCOIND_RPC_TIMEOUT=10000
 BTCEXP_PRIVACY_MODE=true
 # Password protection for site via basic auth (enter any username, only the password is checked)
 # Default: none
-BTCEXP_BASIC_AUTH_PASSWORD=$PASSWORD_B
+#BTCEXP_BASIC_AUTH_PASSWORD=$PASSWORD_B
 # Select optional "address API" to display address tx lists and balances
 # Options: electrumx, blockchain.com, blockchair.com, blockcypher.com
 # If electrumx set, the BTCEXP_ELECTRUMX_SERVERS variable must also be
