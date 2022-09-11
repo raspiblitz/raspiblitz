fd42baafdda147a263020c11e8f70ae0835143f1
openoms <oms@tuta.io>
2022-09-11 21:55:17 +0100

    cln update to v0.12.0

--- home.admin/config.scripts/cl.install.sh
+++ home.admin/config.scripts/cl.install.sh
@@ -2,20 +2,16 @@
 # https://lightning.readthedocs.io/
 
 # https://github.com/ElementsProject/lightning/releases
-CLVERSION=v0.11.2
+CLVERSION=v0.12.0
 
 # install the latest master by using the last commit id
 # https://github.com/ElementsProject/lightning/commit/master
 # CLVERSION="063366ed7e3b7cc12a8d1681acc2b639cf07fa23"
 
 # https://github.com/ElementsProject/lightning/tree/master/contrib/keys
-PGPsigner="rustyrussel"
-PGPpubkeyLink="https://raw.githubusercontent.com/ElementsProject/lightning/master/contrib/keys/rustyrussell.txt"
-PGPpubkeyFingerprint="D9200E6CD1ADB8F1"
-
-#PGPsigner="cdecker"
-#PGPpubkeyLink="https://raw.githubusercontent.com/ElementsProject/lightning/master/contrib/keys/${PGPsigner}.txt"
-#PGPpubkeyFingerprint="A26D6D9FE088ED58"
+PGPsigner="niftynei" # rustyrussel D9200E6CD1ADB8F1 # cdecker A26D6D9FE088ED58
+PGPpubkeyLink="https://raw.githubusercontent.com/ElementsProject/lightning/master/contrib/keys/${PGPsigner}.txt"
+PGPpubkeyFingerprint="BFF0F67810C1EED1"
 
 # help
 if [ $# -eq 0 ]||[ "$1" = "-h" ]||[ "$1" = "--help" ];then
