0fc8e101670977360398cd9a2275e13b3a81639a
openoms <43343391+openoms@users.noreply.github.com>
2022-11-15 12:32:37 -0500

    lit update to v0.8.3-alpha (#3430)

--- home.admin/config.scripts/bonus.lit.sh
+++ home.admin/config.scripts/bonus.lit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # https://github.com/lightninglabs/lightning-terminal/releases
-LITVERSION="0.7.0-alpha"
+LITVERSION="0.8.3-alpha"
 
 # command info
 if [ $# -eq 0 ] || [ "$1" = "-h" ] || [ "$1" = "-help" ]; then
@@ -12,13 +12,16 @@ if [ $# -eq 0 ] || [ "$1" = "-h" ] || [ "$1" = "-help" ]; then
 fi
 
 # check who signed the release in https://github.com/lightninglabs/lightning-terminal/releases
-PGPsigner="guggero"
+PGPsigner="ellemouton"
 
-if [ $PGPsigner = guggero ];then
-  PGPpkeys="https://keybase.io/guggero/pgp_keys.asc"
+if [ $PGPsigner = ellemouton ];then
+  PGPpkeys="https://github.com/${PGPsigner}.gpg"
+  PGPcheck="D7D916376026F177"
+elif [ $PGPsigner = guggero ];then
+  PGPpkeys="https://keybase.io/${PGPsigner}/pgp_keys.asc"
   PGPcheck="03DB6322267C373B"
 elif [ $PGPsigner = roasbeef ];then
-  PGPpkeys="https://keybase.io/roasbeef/pgp_keys.asc "
+  PGPpkeys="https://keybase.io/${PGPsigner}/pgp_keys.asc "
   PGPcheck="3BBD59E99B280306"
 fi
 
@@ -183,7 +186,7 @@ if [ "$1" = "1" ] || [ "$1" = "on" ]; then
 
     echo "# check binary was not manipulated (checksum test)"
     wget -N https://github.com/lightninglabs/lightning-terminal/releases/download/v${LITVERSION}/manifest-v${LITVERSION}.sig
-    wget --no-check-certificate ${PGPpkeys}
+    wget --no-check-certificate -O ./pgp_keys.asc ${PGPpkeys}
     binaryChecksum=$(sha256sum ${binaryName} | cut -d " " -f1)
     if [ "${binaryChecksum}" != "${SHA256}" ]; then
       echo "# FAIL # Downloaded LiT BINARY not matching SHA256 checksum: ${SHA256}"
