29a29b90303d3b5d2527770fdad5157fefc73ca2
rootzoll <christian@geektank.de>
2022-11-18 10:29:19 +0100

    fix bloitz api tag

--- home.admin/config.scripts/blitz.web.api.sh
+++ home.admin/config.scripts/blitz.web.api.sh
@@ -18,6 +18,7 @@ fi
 DEFAULT_GITHUB_USER="fusion44"
 DEFAULT_GITHUB_REPO="blitz_api"
 DEFAULT_GITHUB_BRANCH="main"
+DEFAULT_GITHUB_COMMITORTAG="v0.5.0-beta"
 
 ###################
 # UPDATE CONFIG
@@ -155,6 +156,10 @@ if [ "$1" = "1" ] || [ "$1" = "on" ]; then
     DEFAULT_GITHUB_BRANCH="$4"
   fi
 
+  if [ "$5" != "" ]; then
+    DEFAULT_GITHUB_COMMITORTAG="$5"
+  fi
+
   echo "# INSTALL Web API ..."
   # clean old source
   rm -r /root/blitz_api 2>/dev/null
@@ -193,6 +198,10 @@ if [ "$1" = "1" ] || [ "$1" = "on" ]; then
   cd blitz_api || exit 1
   if ! git checkout ${DEFAULT_GITHUB_BRANCH}; then
     echo "error='git checkout failed'"
+    exit 1
+  fi
+  if ! git reset --hard ${DEFAULT_GITHUB_COMMITORTAG}; then
+    echo "error='git reset failed'"
     exit 1
   fi
   if ! pip install -r requirements.txt --no-deps; then
