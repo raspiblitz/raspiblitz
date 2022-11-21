4ce743a939918774358f23a8cc72f9b2fbf6eedd
steepdawn974 <92274076+steepdawn974@users.noreply.github.com>
2022-11-21 20:15:59 +0100

    Update cl.update.sh menu entries (#3459)

--- home.admin/config.scripts/cl.update.sh
+++ home.admin/config.scripts/cl.update.sh
@@ -20,7 +20,7 @@ mode="$1"
 
 # RECOMMENDED UPDATE BY RASPIBLITZ TEAM
 # comment will be shown as "BEWARE Info" when option is choosen (can be multiple lines) 
-clUpdateVersion="0.10.2" # example: 0.10.1 .. keep empty if no newer version as sd card build is available
+clUpdateVersion="" # example: 0.12.1 .. keep empty if no newer version as sd card build is available
 clUpdateComment="Please keep in mind that downgrading afterwards is not tested. Also not all additional apps are fully tested with the this update - but it looked good on first tests."
 
 # GATHER DATA
@@ -36,7 +36,7 @@ clUpdateInstalled=$(echo "${clInstalledVersion}" | grep -c "${clUpdateVersion}")
 
 # get latest release from Core Lightning GitHub releases without release candidates
 clLatestVersion=$(curl -s https://api.github.com/repos/ElementsProject/lightning/releases | jq -r '.[].tag_name' | grep -v "rc" | head -n1)
-# example: v0.10.2
+# example: v0.12.1
 
 # INFO
 if [ "${mode}" = "info" ]; then
