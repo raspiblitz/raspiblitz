2610758faa110dc6458252ac193caa55600d6419
/rootzoll <christian@geektank.de>
2022-12-16 15:50:03 +0100

    #3482 btcpayserver 1.7.2 (#3533)

--- home.admin/config.scripts/bonus.btcpayserver.sh
+++ home.admin/config.scripts/bonus.btcpayserver.sh
@@ -5,7 +5,7 @@
 # https://github.com/dgarage/NBXplorer/tags
 NBXplorerVersion="v2.3.49"
 # https://github.com/btcpayserver/btcpayserver/releases
-BTCPayVersion="v1.7.1"
+BTCPayVersion="v1.7.2"
 
 # command info
 if [ $# -eq 0 ] || [ "$1" = "-h" ] || [ "$1" = "-help" ]; then
