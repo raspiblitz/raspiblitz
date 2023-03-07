6b6ced559eb23f6ff755eeb4d8787a47672d8f4d
openoms <oms@tuta.io>
2023-03-07 06:45:25 +0000

    btcpayserver update to v1.8.2

--- home.admin/config.scripts/bonus.btcpayserver.sh
+++ home.admin/config.scripts/bonus.btcpayserver.sh
@@ -5,7 +5,7 @@
 # https://github.com/dgarage/NBXplorer/tags
 NBXplorerVersion="v2.3.62"
 # https://github.com/btcpayserver/btcpayserver/releases
-BTCPayVersion="v1.8.0"
+BTCPayVersion="v1.8.2"
 
 # command info
 if [ $# -eq 0 ] || [ "$1" = "-h" ] || [ "$1" = "-help" ]; then
