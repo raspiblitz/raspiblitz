821023e62f77406cf019d65efa67782663a899fd
rootzoll <christian@geektank.de>
2023-03-10 12:57:55 +0100

    #1186 FinTS install script first draft

--- CHANGES.md
+++ CHANGES.md
@@ -11,6 +11,7 @@
 - New: BOS Telegram Bot Support (see OPTIONS on LND Balance of Satoshis menu entry)
 - New: LightningTipBot v0.5 [details](https://github.com/LightningTipBot/LightningTipBot)
 - New: CLI shortcut for ↬lnproxy [details](https://github.com/rootzoll/raspiblitz/pull/3333)
+- New: Homebanking Interface FinTS/HBCI (experimental) [details](https://github.com/rootzoll/raspiblitz/issues/1186)
 - New on WebUI: Jam (JoinMarket Web UI) v0.1.4 [details](https://github.com/joinmarket-webui/joinmarket-webui/releases/tag/v0.1.4)
 - Update: Bitcoin Core v24.0.1 [details](https://github.com/bitcoin/bitcoin/blob/master/doc/release-notes/release-notes-24.0.1.md)
 - Update: LND v0.15.5 [details](https://github.com/lightningnetwork/lnd/releases/tag/v0.15.5-beta)
--- home.admin/config.scripts/bonus.fints.sh
+++ home.admin/config.scripts/bonus.fints.sh
@@ -1,45 +1,34 @@
 #!/bin/bash
 
-# This is a template bonus script you can use to add your own app to RaspiBlitz.
-# So just copy it within the `/home.admin/config.scripts` directory and
-# rename it for your app - example: `bonus.myapp.sh`.
-# Then go thru this script and delete parts/comments you dont need or add
-# needed configurations.
-
-# id string of your app (short single string unique in raspiblitz)
-# should be same as used in name if script
-APPID="template" # one-word lower-case no-specials
-
-# clean human readable version - will be displayed in UI
-# just numbers only seperated by dots (2 or 0.1 or 1.3.4 or 3.4.5.2)
-VERSION="0.1"
+APPID="fints"
+VERSION="2.23"
 
 # the git repo to get the source code from for install
-GITHUB_REPO="https://github.com/rootzoll/webapp-template"
+GITHUB_REPO="https://github.com/drmartinberger/FueliFinTS"
 
 # the github tag of the version of the source code to install
 # can also be a commit hash
 # if empty it will use the latest source version
-GITHUB_TAG="v0.1"
+GITHUB_TAG=""
 
 # the github signature to verify the author
 # leave GITHUB_SIGN_AUTHOR empty to skip verifying
-GITHUB_SIGN_AUTHOR="web-flow"
+GITHUB_SIGN_AUTHOR="" #web-flow
 GITHUB_SIGN_PUBKEYLINK="https://github.com/web-flow.gpg"
 GITHUB_SIGN_FINGERPRINT="4AEE18F83AFDEB23"
 
 # port numbers the app should run on
 # delete if not an web app
-PORT_CLEAR="12345"
-PORT_SSL="12346"
-PORT_TOR_CLEAR="12347"
-PORT_TOR_SSL="12348"
+PORT_CLEAR="3110"
+PORT_SSL="3111"
 
 # BASIC COMMANDLINE OPTIONS
 # you can add more actions or parameters if needed - for example see the bonus.rtl.sh
 # to see how you can deal with an app that installs multiple instances depending on
 # lightning implementation or testnets - but this should be OK for a start:
 if [ $# -eq 0 ] || [ "$1" = "-h" ] || [ "$1" = "-help" ]; then
+  echo "# Github Repo: ${GITHUB_REPO}"
+  echo "# Telegram Community Support: https://t.me/LN_FinTS"
   echo "# bonus.${APPID}.sh status    -> status information (key=value)"
   echo "# bonus.${APPID}.sh on        -> install the app"
   echo "# bonus.${APPID}.sh off       -> uninstall the app"
@@ -118,12 +107,7 @@ if [ "$1" = "menu" ]; then
   dialogTitle=" ${APPID} "
 
   # basic info text - for an web app how to call with http & self-signed https
-  dialogText="Open in your local web browser:
-http://${localIP}:${PORT_CLEAR}\n
-https://${localIP}:${PORT_SSL} with Fingerprint:
-${fingerprint}\n
-Use your Password B to login.\n
-"
+  dialogText="Follow information on ${GITHUB_REPO} on how use the FinTS API or say hello on the Telegram Community Chat: https://t.me/LN_FinTS\n"
 
   # add tor info (if available)
   if [ "${toraddress}" != "" ]; then
@@ -131,7 +115,43 @@ Use your Password B to login.\n
   fi
 
   # use whiptail to show SSH dialog & exit
-  whiptail --title "${dialogTitle}" --msgbox "${dialogText}" 18 67
+  whiptail --title "${dialogTitle}" --yes-button "OK" --no-button "OPTIONS" --yesno "${dialogText}" 18 67
+  result=$?
+  if [ ${result} -eq 0 ]; then
+    exit 0
+  fi
+
+  OPTIONS=()
+  OPTIONS+=(LNBITS "Edit lnbits.properties")
+  OPTIONS+=(DEBUG "Print Logs")
+
+  WIDTH=66
+  CHOICE_HEIGHT=$(("${#OPTIONS[@]}/2+1"))
+  HEIGHT=$((CHOICE_HEIGHT+7))
+  CHOICE=$(dialog --clear \
+                --title " ${APPID} - Options" \
+                --ok-label "Select" \
+                --cancel-label "Back" \
+                --menu "Choose one of the following options:" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+  case $CHOICE in
+    DEBUG)
+      echo "# sudo tail -n 100 /home/fints/log/fuelifints.log"
+      sudo tail -n 100 /home/fints/log/fuelifints.log
+      echo "# PRESS ENTER to continue"
+      read key
+      ;;
+    LNBITS)
+      edittemp=$(mktemp -p /dev/shm/)
+      sudo -u fints dialog --title "Editing /home/fints/lnbits.properties" --editbox "/home/fints/lnbits.properties" 200 200 2> "${edittemp}"
+      result=$?
+      echo "result=${result}"
+      echo "edittemp=${edittemp}"
+      ;;
+  esac
+
   echo "please wait ..."
   exit 0
 fi
@@ -154,46 +174,30 @@ if [ "$1" = "1" ] || [ "$1" = "on" ]; then
 
   echo "# Installing ${APPID} ..."
 
-  # check and install NodeJS - if already installed it will skip
-  /home/admin/config.scripts/bonus.nodejs.sh on
+  # install java & build tool
+  sudo apt install -y default-jdk
+  sudo apt install -y maven
+
+  # make sure mysql/myria db is available
+  sudo apt-get install -y mariadb-server mariadb-client
 
   # create a dedicated user for the app
-  # BACKGROUND is here to seperate running apps by unix users
-  # and only give file write access to the rest of the system where needed.
   echo "# create user"
   sudo adduser --disabled-password --gecos "" ${APPID} || exit 1
 
   # add user to special groups with special access rights
-  # BACKGROUND there are some unix groups available that will give the access to
-  # like for example to the lnd admin macaroons - to check all groups available use:
-  # `cut -d: -f1 /etc/group | sort` command on raspiblitz commandline
-  echo "# add use to special groups"
-  sudo /usr/sbin/usermod --append --groups lndadmin ${APPID}
+  # echo "# add use to special groups"
+  # sudo /usr/sbin/usermod --append --groups lndadmin ${APPID}
 
   # create a data directory on /mnt/hdd/app-data/ for the app
-  # BACKGROUND is that any critical data that needs to survive an update should
-  # be stored in that app-data directory. All data there will also be part of
-  # any raspiblitz data migration. Also on install handle the case that there
-  # is already data from a pervious install available the user wants to
-  # continue to use and even may come from an older version from your app.
-
   if ! [ -d /mnt/hdd/app-data/${APPID} ]; then
-
     echo "# create app-data directory"
     sudo mkdir /mnt/hdd/app-data/${APPID} 2>/dev/null
     sudo chown ${APPID}:${APPID} -R /mnt/hdd/app-data/${APPID}
-
   else
-
     echo "# reuse existing app-directory"
     sudo chown ${APPID}:${APPID} -R /mnt/hdd/app-data/${APPID}
-
   fi
-
-  # make sure needed debian packages are installed
-  # 'fbi' is here just an example - change to what you need or delete
-  echo "# install from source code"
-  sudo apt install -y fbi
 
   # download source code and verify
   # BACKGROUND is that now you download the code from github, reset to a given version tag/commit,
@@ -202,6 +206,10 @@ if [ "$1" = "1" ] || [ "$1" = "on" ]; then
   echo "# download the source code & verify"
   sudo -u ${APPID} git clone ${GITHUB_REPO} /home/${APPID}/${APPID}
   cd /home/${APPID}/${APPID}
+
+  # TODO remove later and use master branch
+  sudo -u fints git checkout lnbits-via-mt940
+
   if [ "${GITHUB_TAG}" != "" ]; then
     sudo -u ${APPID} git reset --hard $GITHUB_TAG
   fi
@@ -211,15 +219,33 @@ if [ "$1" = "1" ] || [ "$1" = "on" ]; then
   fi
 
   # compile/install the app
-  # BACKGROUND on this example is a web app that compiles with NodeJS. But of course
-  # your app could have a complete other way to install - check other install scripts as examples.
   echo "# compile/install the app"
   cd /home/${APPID}/${APPID}
-  sudo -u ${APPID} npm install --only=prod --logLevel warn
+  # install dependencies from pom.xml
+  sudo -u fints mvn package
   if ! [ $? -eq 0 ]; then
-      echo "# FAIL - npm install did not run correctly - deleting code & exit"
+      echo "# FAIL - mvn package did not run correctly - deleting code & exit"
       sudo rm -r /home/${APPID}/${APPID}
       exit 1
+  fi
+  sudo -u fints cp /home/fints/fints/target/LN-FinTS-jar-with-dependencies.jar /home/fints/fints-fat.jar
+  if ! [ $? -eq 0 ]; then 
+      echo "# FAIL - was not able to copy /home/fints/fints-fat.jar"
+      sudo rm -r /home/${APPID}/${APPID}
+      exit 1
+  fi
+
+  # init database
+  sudo mariadb -e "DROP DATABASE IF EXISTS fints;"
+  sudo mariadb -e "CREATE DATABASE fints;"
+  sudo mariadb -e "GRANT ALL PRIVILEGES ON fints.* TO 'fintsuser' IDENTIFIED BY 'fints';"
+  sudo mariadb -e "FLUSH PRIVILEGES;"
+  if [ -f "dbsetup.sql" ]; then
+    mariadb -ufintsuser -pfints fints < dbsetup.sql
+  else
+    echo "# FAIL - dbsetup.sql not found - deleting code & exit"
+    sudo rm -r /home/${APPID}/${APPID}
+    exit 1
   fi
 
   # open the ports in the firewall
@@ -227,10 +253,8 @@ if [ "$1" = "1" ] || [ "$1" = "on" ]; then
   sudo ufw allow ${PORT_CLEAR} comment "${APPID} HTTP"
   sudo ufw allow ${PORT_SSL} comment "${APPID} HTTPS"
 
-
-  # every app should have their own systemd service that cares about starting &
+  # every app has their own systemd service that cares about starting &
   # running the app in the background - see the PRESTART section for adhoc config
-  # please config this systemd template to your needs
   echo "# create systemd service: ${APPID}.service"
   echo "
 [Unit]
@@ -242,7 +266,7 @@ After=bitcoind
 WorkingDirectory=/home/${APPID}
 Environment=\"HOME_PATH=/mnt/hdd/app-data/${APPID}\"
 ExecStartPre=-/home/admin/config.scripts/bonus.${APPID}.sh prestart
-ExecStart=/usr/bin/node /home/${APPID}/${APPID}/${APPID}
+ExecStart=java -jar /home/${APPID}/fints-fat.jar
 User=${APPID}
 Restart=always
 TimeoutSec=120
@@ -264,102 +288,45 @@ WantedBy=multi-user.target
   # when tor is set on also install the hidden service
   if [ "${runBehindTor}" = "on" ]; then
     # activating tor hidden service
-    /home/admin/config.scripts/tor.onion-service.sh ${APPID} 80 ${PORT_TOR_CLEAR} 443 ${PORT_TOR_SSL}
+    /home/admin/config.scripts/tor.onion-service.sh ${APPID} 80 ${PORT_CLEAR} 443 ${PORT_SSL}
   fi
 
-  # nginx configuration
-  # BACKGROUND is that the plain HTTP is served by your web app, but thru the nginx proxy it will be available
-  # with (self-signed) HTTPS and with separate configs for Tor & Tor+HTTPS.
+  # create keystore if needed
+  keystoreExists=$(sudo ls /mnt/hdd/app-data/fints/keystore.jks 2>/dev/null | grep -c 'keystore.jks')
+  if [ ${keystoreExists} -eq 0 ]; then
+    echo "# creating keystore"
+    sudo -u fints keytool -genkey -keyalg RSA -alias fints -keystore /mnt/hdd/app-data/fints/keystore.jks -storepass raspiblitz -noprompt -dname "CN=raspiblitz, OU=IT, O=raspiblitz, L=world, S=world, C=BZ"
+  else
+    echo "# keystore already exists"
+  fi
   
-  echo "# setup nginx confing"
+  # config app basics: lnbits.properties
+  sudo -u fints mkdir /home/fints/config
+  sudo -u fints cp /home/fints/fints/config/fuelifints.properties /home/fints/config/fuelifints.properties
+  sudo sed -i "s/^productinfo.csv.check=.*/productinfo.csv.check=false/g" /home/fints/config/fuelifints.properties
+  sudo sed -i "s/^rdh_port =.*/rdh_port = ${PORT_CLEAR}/g" /home/fints/config/fuelifints.properties
+  sudo sed -i "s/^ssl_port =.*/ssl_port = ${PORT_SSL}/g" /home/fints/config/fuelifints.properties
+  sudo sed -i "s/^keystore_location =.*/keystore_location = \/mnt\/hdd\/app-data\/fints\/keystore.jks/g" /home/fints/config/fuelifints.properties
+  sudo sed -i "s/^keysotre_password =.*/keysotre_password = raspiblitz/g" /home/fints/config/fuelifints.properties
 
-  # write the HTTPS config
-  echo "
-server {
-    listen ${PORT_SSL} ssl;
-    listen [::]:${PORT_SSL} ssl;
-    server_name _;
-    include /etc/nginx/snippets/ssl-params.conf;
-    include /etc/nginx/snippets/ssl-certificate-app-data.conf;
-    access_log /var/log/nginx/access_${APPID}.log;
-    error_log /var/log/nginx/error_${APPID}.log;
-    location / {
-        proxy_pass http://127.0.0.1:${PORT_CLEAR};
-        include /etc/nginx/snippets/ssl-proxy-params.conf;
-    }
-}
-" | sudo tee /etc/nginx/sites-available/${APPID}_ssl.conf
-  sudo ln -sf /etc/nginx/sites-available/${APPID}_ssl.conf /etc/nginx/sites-enabled/
+  # config app basics: blz.banking2.properties.example
+  sudo -u fints cp /home/fints/fints/config/blz.banking2.properties.example /home/fints/config/blz.banking2.properties
 
-  # write the Tor config
-  echo "
-server {
-    listen ${PORT_TOR_CLEAR};
-    server_name _;
-    access_log /var/log/nginx/access_${APPID}.log;
-    error_log /var/log/nginx/error_${APPID}.log;
-    location / {
-        proxy_pass http://127.0.0.1:${PORT_CLEAR};
-        include /etc/nginx/snippets/ssl-proxy-params.conf;
-    }
-}
-" | sudo tee /etc/nginx/sites-available/${APPID}_tor.conf
-  sudo ln -sf /etc/nginx/sites-available/${APPID}_tor.conf /etc/nginx/sites-enabled/
-
-  # write the Tor+HTTPS config
-  echo "
-server {
-    listen ${PORT_TOR_SSL} ssl;
-    server_name _;
-    include /etc/nginx/snippets/ssl-params.conf;
-    include /etc/nginx/snippets/ssl-certificate-app-data-tor.conf;
-    access_log /var/log/nginx/access_${APPID}.log;
-    error_log /var/log/nginx/error_${APPID}.log;
-    location / {
-        proxy_pass http://127.0.0.1:${PORT_CLEAR};
-        include /etc/nginx/snippets/ssl-proxy-params.conf;
-    }
-}
-" | sudo tee /etc/nginx/sites-available/${APPID}_tor_ssl.conf
-  sudo ln -sf /etc/nginx/sites-available/${APPID}_tor_ssl.conf /etc/nginx/sites-enabled/
-
-  # test nginx config & activate thru reload
-  sudo nginx -t
-  sudo systemctl reload nginx
+  # config app basics: lnbits.properties
+  sudo -u fints cp /home/fints/fints/config/lnbits.properties.example /home/fints/config/lnbits.properties
+  # in file lnbits.properties replace the line starting with lnbitsUrl with the following line 'lnbitsUrl = http://127.0.0.1:5000'
+  sudo sed -i "s/lnbitsUrl =.*/lnbitsUrl = http:\/\/127.0.0.1:5000/g" /home/fints/config/lnbits.properties   
 
   # mark app as installed in raspiblitz config
   /home/admin/config.scripts/blitz.conf.sh set ${APPID} "on"
 
   # start app up thru systemd
   sudo systemctl enable ${APPID}
+  echo "# OK - the ${APPID}.service is now enabled"
+
   sudo systemctl start ${APPID}
-  echo "# OK - the ${APPID}.service is now enabled & started"
   echo "# Monitor with: sudo journalctl -f -u ${APPID}"
   exit 0
-
-  # OK so your app is now installed, but there please also check the following parts to ensure a propper integration
-  # into the raspiblitz system:
-
-  # PROVISION - reinstall on updates & recovery
-  # Take a look at `_provision_.sh` script - you can see that there all bonus apps install scripts get called if
-  # they have an active entry in the raspiblitz config. This is needed so that on sd card image update or recovery
-  # all apps get installed again. So add your app there accordantly so its install will survive an sd card update.
-
-  # MAINMENU - show users that app is installed
-  # Take a look at the `00mainmenu.sh` script - you can see there almost all bonus apps add a menu entry there if
-  # they are installed that then is calling this script with the `menu` parameter. Add your app accordingly.
-
-  # SERVICES MENU - add your app for onclick install
-  # Take a look at the `00settingsMenuServices.sh` script - you can there almost all bonus apps added themselves
-  # as an option in to be easily installed & deinstalled. Add your app there accordantly.
-
-  # DEBUGLOGS - add some status information
-  # Take a look at the `blitz.debug.sh` script - you can see there that apps if they are installed give some
-  # information on their latest logs and where to find them in the case that the user is searching for an  error.
-  # So its best practice to also add your app there with some small info to help on debug & finding error logs.
-
-  # PRESTART & DEINSTALL
-  # see the following sections of the template
 
 fi
 
@@ -386,10 +353,7 @@ if [ "$1" = "prestart" ]; then
   fi
 
   echo "## PRESTART CONFIG START for ${APPID} (called by systemd prestart)"
-
-  # so if you have anything to configure before service starts, do it here
-  echo "# no need for adhoc config needed so far"
-
+  # at the moment no on the fly config is needed
   echo "## PRESTART CONFIG DONE for ${APPID}"
   exit 0
 fi
@@ -411,15 +375,15 @@ if [ "$1" = "0" ] || [ "$1" = "off" ]; then
   sudo systemctl disable ${APPID}.service
   sudo rm /etc/systemd/system/${APPID}.service
 
-  echo "# remove nginx symlinks"
-  sudo rm -f /etc/nginx/sites-enabled/${APPID}_ssl.conf 2>/dev/null
-  sudo rm -f /etc/nginx/sites-enabled/${APPID}_tor.conf 2>/dev/null
-  sudo rm -f /etc/nginx/sites-enabled/${APPID}_tor_ssl.conf 2>/dev/null
-  sudo rm -f /etc/nginx/sites-available/${APPID}_ssl.conf 2>/dev/null
-  sudo rm -f /etc/nginx/sites-available/${APPID}_tor.conf 2>/dev/null
-  sudo rm -f /etc/nginx/sites-available/${APPID}_tor_ssl.conf 2>/dev/null
-  sudo nginx -t
-  sudo systemctl reload nginx
+  #echo "# remove nginx symlinks"
+  #sudo rm -f /etc/nginx/sites-enabled/${APPID}_ssl.conf 2>/dev/null
+  #sudo rm -f /etc/nginx/sites-enabled/${APPID}_tor.conf 2>/dev/null
+  #sudo rm -f /etc/nginx/sites-enabled/${APPID}_tor_ssl.conf 2>/dev/null
+  #sudo rm -f /etc/nginx/sites-available/${APPID}_ssl.conf 2>/dev/null
+  #sudo rm -f /etc/nginx/sites-available/${APPID}_tor.conf 2>/dev/null
+  #sudo rm -f /etc/nginx/sites-available/${APPID}_tor_ssl.conf 2>/dev/null
+  #sudo nginx -t
+  #sudo systemctl reload nginx
 
   echo "# close ports on firewall"
   sudo ufw deny "${PORT_CLEAR}"
@@ -448,7 +412,3 @@ fi
 # just a basic error message when unknown action parameter was given
 echo "# FAIL - Unknown Parameter $1"
 exit 1
-
-# LAST NOTES:
-# Best is to contribute a new app install script as a PR to the raspiblitz GitHub repo.
-# Please base your PR on the `dev` branch - not on the default branch displayed.
