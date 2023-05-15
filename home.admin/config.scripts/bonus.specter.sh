de5e579e6c187ede4af3d24a08848138ed6568d4
openoms <43343391+openoms@users.noreply.github.com>
2023-05-15 21:12:22 +0100

    keep version v1.8.1

--- home.admin/config.scripts/bonus.specter.sh
+++ home.admin/config.scripts/bonus.specter.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # https://github.com/cryptoadvance/specter-desktop
 
-pinnedVersion="1.14.5"
+pinnedVersion="1.8.1"
 
 # command info
 if [ $# -eq 0 ] || [ "$1" = "-h" ] || [ "$1" = "-help" ]; then
