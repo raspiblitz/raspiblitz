12a8d59e1784029200a49358eb898dccd6eaf395
geco91 <red78@web.de>
2023-07-18 18:52:48 +0200

    Remove Bitcoin recommended Update for v1.10.0 (#4015)
    * adapt version
    
    * correct versions
    
    * remove recommended bitcoind update

--- home.admin/config.scripts/bitcoin.update.sh
+++ home.admin/config.scripts/bitcoin.update.sh
@@ -18,7 +18,7 @@ mode="$1"
 
 # RECOMMENDED UPDATE BY RASPIBLITZ TEAM (just possible once per sd card update)
 # comment will be shown as "BEWARE Info" when option is choosen (can be multiple lines)
-bitcoinVersion="24.1" # example: 22.0 .. keep empty if no newer version as sd card build is available
+bitcoinVersion="" # example: 22.0 .. keep empty if no newer version as sd card build is available
 
 # needed to check code signing
 # https://github.com/emzy.gpg
