1153008cfb350d36d37eae1514081f44d235d89a
rootzoll <christian@geektank.de>
2023-08-25 17:23:48 +0200

    #4116 adding status to zram script

--- home.admin/config.scripts/blitz.zram.sh
+++ home.admin/config.scripts/blitz.zram.sh
@@ -3,10 +3,36 @@
 # using https://github.com/foundObjects/zram-swap
 VERSION="205ea1ec5b169f566e5e98ead794e9daf90cf245"
 
+if [ "$1" = status ]; then
+
+  # check if file /home/admin/download/zram-swap/install.sh exists
+  if [ ! -f /home/admin/download/zram-swap/install.sh ]; then
+    echo "downloaded=1"
+  else
+    echo "downloaded=0"
+  fi
+
+  # check if service zram-swap is loaded/active
+  serviceLoaded='sudo systemctl status zram-swap | grep -c loaded'
+  if [ ${serviceLoaded} -gt 0 ]; then
+    echo "serviceLoaded=1"
+  else
+    echo "serviceLoaded=0"
+  fi  
+  serviceActive='sudo systemctl status zram-swap | grep -c active'
+  if [ ${serviceActive} -gt 0 ]; then
+    echo "serviceActive=1"
+  else
+    echo "serviceActive=0"
+  fi
+
+  exit 0
+fi
+
 # command info
 if [ $# -eq 0 ] || [ "$1" = "-h" ] || [ "$1" = "-help" ]; then
   echo "config script to install ZRAM"
-  echo "blitz.zram.sh [on|off]"
+  echo "blitz.zram.sh [on|off|status]"
   echo "using https://github.com/foundObjects/zram-swap"
   exit 1
 fi
