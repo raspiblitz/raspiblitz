837f8883a2d55a8b8cc8b6222bd87058b00d2c75
/rootzoll <christian@geektank.de>
2023-08-27 17:39:56 +0200

    Update & fix configure of CLN v23.08 (#4134)
    * update cln v23.08
    
    * remove  --enable-experimental-features

--- home.admin/config.scripts/cl.install.sh
+++ home.admin/config.scripts/cl.install.sh
@@ -2,7 +2,7 @@
 # https://lightning.readthedocs.io/
 
 # https://github.com/ElementsProject/lightning/releases
-CLVERSION="v23.05.2"
+CLVERSION="v23.08"
 
 # install the latest master by using the last commit id
 # https://github.com/ElementsProject/lightning/commit/master
@@ -58,7 +58,7 @@ function installDependencies() {
 function buildAndInstallCLbinaries() {
   echo "- Configuring EXPERIMENTAL_FEATURES enabled"
   echo
-  sudo -u bitcoin ./configure --enable-experimental-features
+  sudo -u bitcoin ./configure
   echo
   echo "- Building Core lightning from source"
   echo
