3a9ef38861b7b4b7294e1f35ae6269739dcbd196
Akihiko <105069903+0xakihiko@users.noreply.github.com>
2023-11-09 09:09:52 +0000

    Fixed missing dependency cl.install.sh

--- home.admin/config.scripts/cl.install.sh
+++ home.admin/config.scripts/cl.install.sh
@@ -42,7 +42,7 @@ function installDependencies() {
     libsqlite3-dev python3 net-tools zlib1g-dev libsodium-dev \
     gettext
   # additional requirements
-  sudo apt-get install -y postgresql libpq-dev
+  sudo apt-get install -y postgresql libpq-dev psutil
   # upgrade pip
   sudo pip3 install --upgrade pip
   sudo -u bitcoin pip install mako
