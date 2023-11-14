481c3c2d7dd132a70c2fbfa7686d9754d2a475ad
openoms <oms@tuta.io>
2023-11-14 21:03:56 +0100

    update btcpayserver v1.11.7, nbxplorer v2.3.67

--- home.admin/config.scripts/bonus.btcpayserver.sh
+++ home.admin/config.scripts/bonus.btcpayserver.sh
@@ -3,9 +3,9 @@
 # Based on: https://gist.github.com/normandmickey/3f10fc077d15345fb469034e3697d0d0
 
 # https://github.com/dgarage/NBXplorer/tags
-NBXplorerVersion="v2.3.66"
+NBXplorerVersion="v2.3.67"
 # https://github.com/btcpayserver/btcpayserver/releases
-BTCPayVersion="v1.11.6"
+BTCPayVersion="v1.11.7"
 
 # check who signed the release (person that published release)
 PGPsigner="nicolasdorier"
