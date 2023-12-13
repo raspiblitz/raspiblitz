ff48bf3c30378794e177b3fc0fc05750f44ddde6
rootzoll <christian@geektank.de>
2023-12-13 22:11:44 +0100

    more debug

--- home.admin/config.scripts/blitz.datadrive.sh
+++ home.admin/config.scripts/blitz.datadrive.sh
@@ -746,7 +746,7 @@ if [ "$1" = "format" ]; then
      mkdir -p /tmp/ext4 1>/dev/null
      if [ $ext4IsPartition -eq 0 ]; then
         # write new EXT4 partition
-        >&2 echo "# Creating the one big partition hdd(${hdd})"
+        >&2 echo "# Creating the one big partition - hdd(${hdd})"
         parted -s /dev/${hdd} mkpart primary ext4 1024KiB 100% 1>&2
         sleep 6
         >&2 echo "# sync"
