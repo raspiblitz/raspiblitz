e7183c2bc527b5951af4206e6c2ada73f28208dc
openoms <oms@tuta.io>
2024-01-03 14:22:56 +0100

    check files in boot

--- build_sdcard.sh
+++ build_sdcard.sh
@@ -437,13 +437,15 @@ if [ "${baseimage}" = "raspios_arm64" ]; then
   # see https://github.com/rootzoll/raspiblitz/issues/428#issuecomment-472822840
 
   configFile="/boot/config.txt"
+  ls -la /boot/firmware
+  ls -la /boot
   if ! grep "Raspiblitz" $configFile; then
     echo "# Adding Raspiblitz Edits to $configFile"
     echo | tee -a $configFile
     echo "# Raspiblitz" | tee -a $configFile
     # ensure that kernel8.img is used to set PAGE_SIZE to 4K
     # https://github.com/raspiblitz/raspiblitz/issues/4346
-    if [ -f /boot/firmware/kernel8.img ]; then
+    if ls /boot/firmware/kernel8.img; then
       echo 'kernel=kernel8.img' | tee -a $configFile
     fi
     echo "max_usb_current=1" | tee -a $configFile
