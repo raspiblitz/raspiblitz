1c6ddb6aaf755b35fb2a24ea347a4608cfc5f05f
openoms <oms@tuta.io>
2024-03-13 15:13:42 +0100

    format the bclitz.fatpack.sh to increase visibility in logs

--- home.admin/config.scripts/blitz.fatpack.sh
+++ home.admin/config.scripts/blitz.fatpack.sh
@@ -4,8 +4,8 @@
 echo -e "\n*** FATPACK ***"
 
 # check if su
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root (with sudo)"
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root (with sudo)"
   exit 1
 fi
 
@@ -16,31 +16,31 @@ fi
 source <(sudo /home/admin/config.scripts/blitz.bootdrive.sh status)
 if [ "${needsExpansion}" == "1" ]; then
 
-    echo "################################################"
-    echo "# SD CARD NEEDS EXPANSION BEFORE FATPACK"
-    echo "# this will be done now ... and trigger a reboot"
-    echo "# after reboot run this script again"
-    echo "################################################"
+  echo "################################################"
+  echo "# SD CARD NEEDS EXPANSION BEFORE FATPACK"
+  echo "# this will be done now ... and trigger a reboot"
+  echo "# after reboot run this script again"
+  echo "################################################"
 
-    # write a stop file to prevent full bootstrap
-    # after fsexpand reboot
-    touch /boot/firmware/stop
+  # write a stop file to prevent full bootstrap
+  # after fsexpand reboot
+  touch /boot/firmware/stop
 
-    # trigger fsexpand
-    /home/admin/config.scripts/blitz.bootdrive.sh fsexpand
+  # trigger fsexpand
+  /home/admin/config.scripts/blitz.bootdrive.sh fsexpand
 
-    # make sure this expand is not marked (because its not done after release)
-    sed -i "s/^fsexpanded=.*/fsexpanded=0/g" /home/admin/raspiblitz.info
+  # make sure this expand is not marked (because its not done after release)
+  sed -i "s/^fsexpanded=.*/fsexpanded=0/g" /home/admin/raspiblitz.info
 
-    echo "################################################"
-    echo "# SD CARD GOT EXPANSION BEFORE FATPACK"
-    echo "# triggering a reboot"
-    echo "# after reboot run this script again"
-    echo "################################################"
+  echo "################################################"
+  echo "# SD CARD GOT EXPANSION BEFORE FATPACK"
+  echo "# triggering a reboot"
+  echo "# after reboot run this script again"
+  echo "################################################"
 
-    # trigger reboot
-    shutdown -h -r now
-    exit 0
+  # trigger reboot
+  shutdown -h -r now
+  exit 0
 fi
 
 apt_install() {
@@ -63,15 +63,23 @@ echo "# defaultAPIrepo(${defaultAPIrepo})"
 echo "# defaultWEBUIuser(${defaultWEBUIuser})"
 echo "# defaultWEBUIrepo(${defaultWEBUIrepo})"
 
+echo
+echo "*******************************"
 echo "* Adding nodeJS Framework ..."
 /home/admin/config.scripts/bonus.nodejs.sh on || exit 1
 
+echo
+echo "*******************************"
 echo "* Optional Packages (may be needed for extended features)"
 apt_install qrencode secure-delete fbi msmtp unclutter xterm python3-pyqt5 xfonts-terminus apache2-utils nginx python3-jinja2 socat libatlas-base-dev hexyl autossh
 
+echo
+echo "*******************************"
 echo "* Adding LND ..."
 /home/admin/config.scripts/lnd.install.sh install || exit 1
 
+echo
+echo "*******************************"
 echo "* Adding Core Lightning ..."
 /home/admin/config.scripts/cl.install.sh install || exit 1
 
@@ -82,8 +90,13 @@ sudo -u admin curl -H "Accept: application/json; indent=4" https://bitnodes.io/a
 # Fallback Nodes List from Bitcoin Core
 sudo -u admin curl https://raw.githubusercontent.com/bitcoin/bitcoin/master/contrib/seeds/nodes_main.txt -o /home/admin/fallback.bitcoin.nodes
 
+echo
+echo "*******************************"
 echo "* Adding Raspiblitz API ..."
 sudo /home/admin/config.scripts/blitz.web.api.sh on "${defaultAPIuser}" "${defaultAPIrepo}" "blitz-${branch}" || exit 1
+
+echo
+echo "*******************************"
 echo "* Adding Raspiblitz WebUI ..."
 sudo /home/admin/config.scripts/blitz.web.ui.sh on "${defaultWEBUIuser}" "${defaultWEBUIrepo}" "release/${branch}" || exit 1
 
@@ -94,20 +107,43 @@ sudo cp -a /home/blitzapi/blitz_web/build/* /home/admin/assets/nginx/www_public
 sudo chown admin:admin /home/admin/assets/nginx/www_public
 sudo rm -r /home/blitzapi/blitz_web/build/*
 
+echo
+echo "*******************************"
 echo "* Adding Code&Compile for WEBUI-APP: LNBITS"
 /home/admin/config.scripts/bonus.lnbits.sh install || exit 1
+
+echo
+echo "*******************************"
 echo "* Adding Code&Compile for WEBUI-APP: JAM"
 /home/admin/config.scripts/bonus.jam.sh install || exit 1
+
+echo
+echo "*******************************"
 echo "* Adding Code&Compile for WEBUI-APP: BTCPAYSERVER"
 /home/admin/config.scripts/bonus.btcpayserver.sh install || exit 1
+
+echo
+echo "*******************************"
 echo "* Adding Code&Compile for WEBUI-APP: RTL"
 /home/admin/config.scripts/bonus.rtl.sh install || exit 1
+
+echo
+echo "*******************************"
 echo "* Adding Code&Compile for WEBUI-APP: THUNDERHUB"
 /home/admin/config.scripts/bonus.thunderhub.sh install || exit 1
+
+echo
+echo "*******************************"
 echo "* Adding Code&Compile for WEBUI-APP: BTC RPC EXPLORER"
 /home/admin/config.scripts/bonus.btc-rpc-explorer.sh install || exit 1
+
+echo
+echo "*******************************"
 echo "* Adding Code&Compile for WEBUI-APP: MEMPOOL"
 /home/admin/config.scripts/bonus.mempool.sh install || exit 1
+
+echo
+echo "*******************************"
 echo "* Adding Code&Compile for WEBUI-APP: ELECTRS"
 /home/admin/config.scripts/bonus.electrs.sh install || exit 1
 
