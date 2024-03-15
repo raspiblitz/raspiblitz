67902e78fc74d9928d6595dae25dd4f182f80b38
rootzoll <christian@geektank.de>
2024-03-15 15:36:21 +0100

    change git command

--- build_sdcard.sh
+++ build_sdcard.sh
@@ -56,7 +56,7 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 if [ "$1" = "-EXPORT" ] || [ "$1" = "EXPORT" ]; then
-  activeBranch=$(git -C /home/admin/raspiblitz rev-parse --abbrev-ref HEAD 2>/dev/null)
+  activeBranch=$(git -C /home/admin/raspiblitz branch --show-current 2>/dev/null)
   echo "activeBranch='${activeBranch}'"
   if [ "${activeBranch}" == "" ]; then
     activeBranch="${defaultBranch}"
