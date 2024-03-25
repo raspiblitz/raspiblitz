0ebc94821d8348fac79602c6bff0dbbb2b098637
rootzoll <christian@geektank.de>
2024-03-25 20:03:26 +0100

    show lndg again

--- home.admin/00settingsMenuServices.sh
+++ home.admin/00settingsMenuServices.sh
@@ -60,7 +60,7 @@ if [ "${lightning}" == "lnd" ] || [ "${lnd}" == "on" ]; then
   OPTIONS+=(ra 'LND RTL Webinterface' ${rtlWebinterface})
   OPTIONS+=(ta 'LND ThunderHub' ${thunderhub})
   OPTIONS+=(la 'LND LIT (loop, pool, faraday)' ${lit})
-  #OPTIONS+=(gb 'LND LNDg (auto-rebalance, auto-fees)' ${lndg})
+  OPTIONS+=(gb 'LND LNDg (auto-rebalance, auto-fees)' ${lndg})
   OPTIONS+=(oa 'LND Balance of Satoshis' ${bos})
   OPTIONS+=(ya 'LND PyBLOCK' ${pyblock})
   OPTIONS+=(ha 'LND ChannelTools (Fund Rescue)' ${chantools})
