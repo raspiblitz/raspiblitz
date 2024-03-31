6ddc42baa19f3257969f98f57c650c80cc3a9611
openoms <oms@tuta.io>
2024-03-31 21:38:22 +0200

    postgres install refactor, add bats tests

--- home.admin/config.scripts/bonus.postgresql.sh
+++ home.admin/config.scripts/bonus.postgresql.sh
@@ -18,57 +18,129 @@ db_backupfile=$5
 
 # switch on
 if [ "$command" = "1" ] || [ "$command" = "on" ]; then
-
-  # https://github.com/rootzoll/raspiblitz/issues/3218
   echo "# Install PostgreSQL"
-
   sudo apt install -y postgresql
-  postgres_datadir="/var/lib/postgresql" # default data dir
 
-  # sudo -u postgres psql -c "show data_directory"
-  if [ ! -d $postgres_datadir ]; then
-    # Get the default or highest version of PostgreSQL installed
-    PG_VERSION=$(psql -V | awk '{print $3}' | cut -d'.' -f1)
-    echo "Detected PostgreSQL version: $PG_VERSION"
+  PG_VERSION=$(psql -V | awk '{print $3}' | cut -d'.' -f1)
+  echo "Detected PostgreSQL version: $PG_VERSION"
+
+  postgres_datadir="/var/lib/postgresql" # default data dir
+  postgres_confdir="/etc/postgresql"     # default conf dir
+
+  sudo systemctl stop postgresql
+
+  if [ ! -d /mnt/hdd/app-data/postgresql ]; then
+    # there is no old data
+
+    # symlink conf dir
+    sudo mkdir -p /mnt/hdd/app-data/postgresql-conf/postgresql
+    sudo chown -R postgres:postgres /mnt/hdd/app-data/postgresql-conf # fix ownership
+    sudo mv $postgres_confdir /etc/postgresql.bak                     # backup new empty dir
+    sudo rm -rf $postgres_confdir                                     # not a symlink.. delete it silently
+    sudo ln -s /mnt/hdd/app-data/postgresql-conf/postgresql /etc/     # create symlink
+
+    # symlink data dir
+    sudo mkdir -p /mnt/hdd/app-data/postgresql
+    sudo chown -R postgres:postgres /mnt/hdd/app-data/postgresql # fix ownership
+    sudo mv $postgres_datadir /var/lib/postgresql.bak            # backup new empty dir
+    sudo rm -rf $postgres_datadir                                # not a symlink.. delete it silently
+    sudo ln -s /mnt/hdd/app-data/postgresql /var/lib/            # create symlink
 
     echo "# Create PostgreSQL data"
     sudo mkdir -p $postgres_datadir/$PG_VERSION/main
     sudo chown -R postgres:postgres $postgres_datadir
 
-    # create cluster
+    echo "# Create cluster"
     sudo pg_createcluster $PG_VERSION main --start
-  fi
 
-  fix_postgres=0
-  if [ -L $postgres_datadir ]; then
-    if [ -e $postgres_datadir ]; then
-      echo "# Good link in $postgres_datadir"
+  elif [ -d /mnt/hdd/app-data/postgresql/$PG_VERSION/main ]; then
+    if [ -d /mnt/hdd/app-data/postgresql-conf ]; then
+      # symlink conf dir
+      sudo mkdir -p /mnt/hdd/app-data/postgresql-conf/postgresql
+      sudo chown -R postgres:postgres /mnt/hdd/app-data/postgresql-conf # fix ownership
+      sudo mv $postgres_confdir /etc/postgresql.bak                     # backup new empty dir
+      sudo rm -rf $postgres_confdir                                     # not a symlink.. delete it silently
+      sudo ln -s /mnt/hdd/app-data/postgresql-conf/postgresql /etc/     # create symlink
     else
-      echo "# Broken link in $postgres_datadir"
-      fix_postgres=1
+      # generate new cluster and use default config
+      echo "# Create PostgreSQL data"
+      sudo mkdir -p $postgres_datadir/$PG_VERSION/main
+      sudo chown -R postgres:postgres $postgres_datadir
+      sudo pg_createcluster $PG_VERSION main --start
+      echo "Setting default password for postgres user"
+      # start cluster temporarily
+      sudo systemctl start postgresql
+      sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'postgres';"
+      sudo systemctl stop postgresql
+      # move and symlink conf dir
+      sudo mkdir -p /mnt/hdd/app-data/postgresql-conf
+      sudo mv /etc/postgresql /mnt/hdd/app-data/postgresql-conf/
+      sudo chown -R postgres:postgres /mnt/hdd/app-data/postgresql-conf
+      sudo ln -s /mnt/hdd/app-data/postgresql-conf/postgresql /etc/ # create symlink
     fi
-  elif [ -e $postgres_datadir ]; then
-    echo "# Not a link in $postgres_datadir"
-    fix_postgres=1
-  else
-    echo "# Missing Link in $postgres_datadir"
-    fix_postgres=1
+
+    # symlink data dir
+    sudo mkdir -p /mnt/hdd/app-data/postgresql
+    sudo chown -R postgres:postgres /mnt/hdd/app-data/postgresql # fix ownership
+    sudo mv $postgres_datadir /var/lib/postgresql.bak            # backup new empty dir
+    sudo rm -rf $postgres_datadir                                # not a symlink.. delete it silently
+    sudo ln -s /mnt/hdd/app-data/postgresql /var/lib/            # create symlink
+
+    sudo chown -R postgres:postgres $postgres_datadir
+    sudo pg_createcluster $PG_VERSION main --start
+
+  elif [ -d /mnt/hdd/app-data/postgresql/13/main ]; then
+    # if there is old data for pg 13 start and upgrade cluster
+    # /usr/bin/pg_upgradecluster [OPTIONS] <old version> <cluster name> [<new data directory>]
+    if [ ! -f /etc/apt/trusted.gpg.d/postgresql.gpg ]; then
+      curl -fsSL https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo gpg --dearmor -o /etc/apt/trusted.gpg.d/postgresql.gpg
+      echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" | sudo tee /etc/apt/sources.list.d/pgdg.list
+      sudo apt update
+    fi
+    sudo apt install -y postgresql-13 || exit 1
+    if [ -d /mnt/hdd/app-data/postgresql-conf ]; then
+      # symlink conf dir
+      sudo mkdir -p /mnt/hdd/app-data/postgresql-conf/postgresql
+      sudo chown -R postgres:postgres /mnt/hdd/app-data/postgresql-conf # fix ownership
+      sudo mv $postgres_confdir /etc/postgresql.bak                     # backup new empty dir
+      sudo rm -rf $postgres_confdir                                     # not a symlink.. delete it silently
+      sudo ln -s /mnt/hdd/app-data/postgresql-conf/postgresql /etc/     # create symlink
+    else
+      # generate new cluster and use default config
+      echo "# Create PostgreSQL data"
+      sudo mkdir -p $postgres_datadir/13/main
+      sudo chown -R postgres:postgres $postgres_datadir
+      sudo pg_createcluster 13 main --start
+      echo "Setting default password for postgres user"
+      # start cluster temporarily
+      sudo systemctl start postgresql
+      sudo -u postgres psql -c "ALTER USER postgres WITH PASSWORD 'postgres';"
+      sudo systemctl stop postgresql
+      # move and symlink conf dir
+      sudo mkdir -p /mnt/hdd/app-data/postgresql-conf
+      sudo mv /etc/postgresql /mnt/hdd/app-data/postgresql-conf/
+      sudo chown -R postgres:postgres /mnt/hdd/app-data/postgresql-conf
+      sudo ln -s /mnt/hdd/app-data/postgresql-conf/postgresql /etc/ # create symlink
+    fi
+
+    # symlink data dir
+    sudo mkdir -p /mnt/hdd/app-data/postgresql
+    sudo chown -R postgres:postgres /mnt/hdd/app-data/postgresql # fix ownership
+    sudo mv $postgres_datadir /var/lib/postgresql.bak            # backup new empty dir
+    sudo rm -rf $postgres_datadir                                # not a symlink.. delete it silently
+    sudo ln -s /mnt/hdd/app-data/postgresql /var/lib/            # create symlink
+
+    sudo chown -R postgres:postgres $postgres_datadir
+    sudo systemctl start postgresql
+    sudo pg_createcluster 13 main --start
+
+    sudo pg_upgradecluster 13 main $postgres_datadir/$PG_VERSION/main || exit 1
+    sudo systemctl disable --now postgresql@13-main
+    sudo apt remove -y postgresql-13
   fi
 
-  if [ ${fix_postgres} = 1 ] || [ ! -d /mnt/hdd/app-data/postgresql ]; then
-    echo "# Move the PostgreSQL data to /mnt/hdd/app-data/postgresql"
-    sudo systemctl stop postgresql 2>/dev/null
-    sudo rsync -av $postgres_datadir /mnt/hdd/app-data
-    sudo mv $postgres_datadir /var/lib/postgresql.bak
-    sudo rm -rf $postgres_datadir # not a symlink.. delete it silently
-    sudo ln -s /mnt/hdd/app-data/postgresql /var/lib/
-  fi
-
-  # always fix ownership
-  sudo chown -R postgres:postgres /mnt/hdd/app-data/postgresql
-
-  sudo systemctl enable postgresql
-  sudo systemctl start postgresql
+  # start cluster
+  sudo systemctl enable --now postgresql
 
   # check if PostgreSQL was installed
   if psql --version; then
@@ -101,13 +173,13 @@ fi
 # switch off
 if [ "$command" = "0" ] || [ "$command" = "off" ]; then
   echo "*** REMOVING POSTGRESQL ***"
-  sudo systemctl stop postgresql
-  sudo systemctl disable postgresql
+  sudo systemctl disable --now postgresql
+  sudo systemctl disable --now postgresql@15-main
+  sudo systemctl disable --now postgresql@13-main
   sudo apt remove -y postgresql
   echo "# remove symlink /var/lib/postgresql"
   sudo rm /var/lib/postgresql
-  # would delete all pg data: 'sudo pg_dropcluster $PG_VERSION main'
-  echo "# OK PostgreSQL is removed."
+  sudo rm /etc/postgresql
   exit 0
 fi
 
@@ -120,7 +192,6 @@ fi
 
 # https://www.postgresql.org/docs/current/backup-dump.html
 if [ "$command" = "backup" ] && [ "$db_name" != "" ]; then
-
   echo "*** BACKUP POSTGRESQL $db_name ***"
   sudo -u postgres pg_dump $db_name >$backup_target/${backup_file}.sql || exit 1
   # Delete old backups (keep last 3 backups)
@@ -128,12 +199,10 @@ if [ "$command" = "backup" ] && [ "$db_name" != "" ]; then
   ls -tp $backup_target/*.sql | grep -v '/$' | tail -n +4 | tr '\n' '\0' | xargs -0 rm -- 2>/dev/null
   echo "OK - backup finished, file saved as $backup_target/${backup_file}.sql"
   exit 0
-
 fi
 
 # restore
 if [ "$command" = "restore" ] && [ "$db_name" != "" ] && [ "$db_user" != "" ] && [ "$db_user_pw" != "" ]; then
-
   echo "*** RESTORE POSTGRESQL $db_name ***"
   # find recent backup
   if [ "$db_backupfile" != "" ]; then
@@ -168,11 +237,9 @@ if [ "$command" = "restore" ] && [ "$db_name" != "" ] && [ "$db_user" != "" ] &&
   echo "$backup_target/sql_import.log written"
   echo "OK - database $db_name restored from ${backup_file}"
   exit 0
-
 fi
 
 if [ "$command" = "info" ]; then
-
   check=$(sudo -u postgres psql -c "show data_directory;" | grep data_directory)
   if [ "$check" = "" ]; then
     echo "show data_directory failed, PostgreSQL not installed?!"
@@ -182,7 +249,6 @@ if [ "$command" = "info" ]; then
     sudo -u postgres psql -c "SELECT datname FROM pg_database;"
   fi
   exit 0
-
 fi
 
 echo "FAIL - Unknown Parameter $command"
