5354d35a54b660032b4a047edd000aec66e5c179
openoms <oms@tuta.io>
2024-04-07 14:02:54 +0200

    fix(_provision.sh): unicode space

--- home.admin/_provision_.sh
+++ home.admin/_provision_.sh
@@ -276,7 +276,7 @@ else
 fi
 
 # LND binary install
-if [ "${lightning}" == "lnd" ] || [ "${lnd}" == "on" ] || [ "${tlnd}" == "on" ] || [ "${slnd}" == "on" ]; then
+if [ "${lightning}" == "lnd" ] || [ "${lnd}" == "on" ] || [ "${tlnd}" == "on" ] || [ "${slnd}" == "on" ]; then
   # if already installed by fatpack will skip
   echo "Provisioning LND Binary - run config script" >> ${logFile}
   /home/admin/config.scripts/lnd.install.sh install >> ${logFile} 2>&1
