89429cfebab45cea9d9417e1baa174e91babdd95
rootzoll <christian@geektank.de>
2024-05-02 18:01:44 +0200

    reduce logs rotation & size

--- home.admin/config.scripts/lnd.install.sh
+++ home.admin/config.scripts/lnd.install.sh
@@ -317,8 +317,8 @@ tlskeypath=/home/bitcoin/.lnd/tls.key
 sync-freelist=true
 
 # Specify the maximum number of logfiles retained in rotation and the threshold size for rotation initiation.
-maxlogfiles=7
-maxlogfilesize=500
+maxlogfiles=5
+maxlogfilesize=400
 
 [Bitcoin]
 bitcoin.active=1
