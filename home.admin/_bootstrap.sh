b82c6d202894e2fd50be15e4f05b52635967c2fd
rootzoll <christian@geektank.de>
2024-05-20 22:34:44 +0200

    add debug

--- home.admin/_bootstrap.sh
+++ home.admin/_bootstrap.sh
@@ -132,6 +132,7 @@ flagExists=$(ls ${raspi_bootdir}/stop 2>/dev/null | grep -c 'stop')
 if [ "${flagExists}" == "1" ]; then
   # set state info
   source <(/home/admin/config.scripts/internet.sh status local)
+  echo "# localip(${localip})" >> $logFile
   /home/admin/_cache.sh set internet_localip "${localip}"
   /home/admin/_cache.sh set state "stop"
   /home/admin/_cache.sh set message "stopped for manual provision"
