143a0505853d87134aa6fc5b0af4bb8eddbb9fd6
rootzoll <christian@geektank.de>
2024-05-30 01:47:33 +0200

    feat: Add blockheight information to electrs status command

--- home.admin/config.scripts/bonus.electrs.sh
+++ home.admin/config.scripts/bonus.electrs.sh
@@ -54,6 +54,15 @@ if [ "$1" = "status" ]; then
     echo "portTCP='50001'"
     localPortRunning=$(sudo netstat -an | grep -c '0.0.0.0:50001')
     echo "localTCPPortActive=${localPortRunning}"
+    if [ ${localPortRunning} -eq 1 ]; then
+
+      # get the synced blockheight
+      syncedBlock=$(echo '{"id": 1, "method": "blockchain.headers.subscribe", "params": []}' | nc -w 2 localhost 50001 | jq '.result.height')
+      echo "blockheight=${syncedBlock}"
+    
+    else
+      echo "blockheight=0"
+    fi
 
     publicPortRunning=$(
       nc -z -w6 ${publicip} 50001 2>/dev/null
@@ -62,15 +71,9 @@ if [ "$1" = "status" ]; then
     if [ "${publicPortRunning}" == "0" ]; then
       # OK looks good - but just means that something is answering on that port
       echo "publicTCPPortAnswering=1"
-
-      # get the synced blockheight
-      syncedBlock=$(echo '{"id": 1, "method": "blockchain.headers.subscribe", "params": []}' | nc -w 2 localhost 50001 | jq '.result.height')
-      echo "blockheight=${syncedBlock}"
-
     else
       # no answer on that port
       echo "publicTCPPortAnswering=0"
-      echo "blockheight=0"
     fi
     echo "portSSL='50002'"
     localPortRunning=$(sudo netstat -an | grep -c '0.0.0.0:50002')
