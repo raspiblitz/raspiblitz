d746bf3353cdee63cf8d3b3bcab50b285dc46789
/rootzoll <christian@geektank.de>
2024-06-10 16:41:06 +0200

    update LIT v0.12.5 (#4617)

--- home.admin/config.scripts/bonus.lit.sh
+++ home.admin/config.scripts/bonus.lit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # https://github.com/lightninglabs/lightning-terminal/releases
-LITVERSION="0.10.1-alpha"
+LITVERSION="0.12.5-alpha"
 
 # command info
 if [ $# -eq 0 ] || [ "$1" = "-h" ] || [ "$1" = "-help" ]; then
