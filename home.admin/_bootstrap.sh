f58c64b5a68b09f0937c828fab159f23455dc248
rootzoll <christian@geektank.de>
2024-09-03 23:53:18 +0200

    show local ip

--- home.admin/_bootstrap.sh
+++ home.admin/_bootstrap.sh
@@ -126,8 +126,9 @@ echo "# raspi_bootdir(${raspi_bootdir})" >> $logFile
 flagExists=$(ls ${raspi_bootdir}/stop 2>/dev/null | grep -c 'stop')
 if [ "${flagExists}" == "1" ]; then
   # set state info
+  localip=$(hostname -I | awk '{print $1}')
   /home/admin/_cache.sh set state "stop"
-  /home/admin/_cache.sh set message "stopped for manual provision"
+  /home/admin/_cache.sh set message "stopped for manual provision ${localip}"
   systemctl stop background.service
   systemctl stop background.scan.service
   # log info
