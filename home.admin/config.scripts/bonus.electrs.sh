264076b799cf4383f83cc8a7980287547d55e535
rootzoll <christian@geektank.de>
2024-10-02 13:26:39 +0200

    Test Electrs 0.10.6

--- home.admin/config.scripts/bonus.electrs.sh
+++ home.admin/config.scripts/bonus.electrs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # https://github.com/romanz/electrs/releases
-ELECTRSVERSION="v0.10.4"
+ELECTRSVERSION="v0.10.6"
 
 # command info
 if [ $# -eq 0 ] || [ "$1" = "-h" ] || [ "$1" = "-help" ]; then
