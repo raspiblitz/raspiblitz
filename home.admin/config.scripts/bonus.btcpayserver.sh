75615fa5c585a56dc5426fab1291b4b8b1a662df
rootzoll <christian@geektank.de>
2024-10-28 11:48:35 +0100

    #4817 add debug info

--- home.admin/config.scripts/bonus.btcpayserver.sh
+++ home.admin/config.scripts/bonus.btcpayserver.sh
@@ -487,7 +487,7 @@ if [ "$1" = "install" ]; then
   echo "# Install NBXplorer $NBXplorerVersion"
   cd /home/btcpay || exit 1
   echo "# Download the NBXplorer source code $NBXplorerVersion"
-  sudo -u btcpay git clone https://github.com/dgarage/NBXplorer.git 2>/dev/null
+  sudo -u btcpay git clone https://github.com/dgarage/NBXplorer.git 2>&1 || echo "NBXplorer git clone failed" && exit 1
   cd NBXplorer || exit 1
   sudo -u btcpay git reset --hard $NBXplorerVersion
   # PGP verify
