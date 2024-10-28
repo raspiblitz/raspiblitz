915522832a9709eab80075b25191d976ae3fe2b6
rootzoll <christian@geektank.de>
2024-10-28 18:40:42 +0100

    backup snapshot download

--- home.admin/config.scripts/bonus.btcpayserver.sh
+++ home.admin/config.scripts/bonus.btcpayserver.sh
@@ -487,15 +487,20 @@ if [ "$1" = "install" ]; then
   echo "# Install NBXplorer $NBXplorerVersion"
   cd /home/btcpay || exit 1
   echo "# Download the NBXplorer source code $NBXplorerVersion"
+  sudo -u btcpay ssh-keyscan github.com >> ~/.ssh/known_hosts
   sudo -u btcpay git clone https://github.com/dgarage/NBXplorer.git
   if [ ! -d "/home/btcpay/NBXplorer" ]; then
-    echo "# FAIL! on first git clone - retrying with SSH."
-    sudo -u btcpay git clone git@github.com:dgarage/NBXplorer.git
+    echo "# FAIL! on first git clone - retrying with snapshot download."
+    sudo -u btcpay curl -L https://github.com/dgarage/NBXplorer/archive/refs/tags/$NBXplorerVersion.tar.gz -o NBXplorer.tar.gz
+    sudo -u btcpay tar -xzvf NBXplorer.tar.gz
+    sudo -u btcpay mv NBXplorer-* NBXplorer
     if [ ! -d "/home/btcpay/NBXplorer" ]; then
       echo "# FAIL! also on second git clone of NBXplorer - uninstall & exiting."
       /home/admin/config.scripts/bonus.btcpayserver.sh uninstall
       exit 1
     fi
+  else
+    echo "# OK - git clone of NBXplorer successful."
   fi
   cd NBXplorer
   sudo -u btcpay git reset --hard $NBXplorerVersion
