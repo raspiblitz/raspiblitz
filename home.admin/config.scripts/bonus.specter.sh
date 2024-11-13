9ff98233d887f6f8f4307e2770f22e0e35ef1599
openoms <oms@tuta.io>
2024-11-13 12:49:56 +0000

    specter update to 2.0.5

--- home.admin/config.scripts/bonus.specter.sh
+++ home.admin/config.scripts/bonus.specter.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # https://github.com/cryptoadvance/specter-desktop
 
-pinnedVersion="2.0.4"
+pinnedVersion="2.0.5"
 
 # command info
 if [ $# -eq 0 ] || [ "$1" = "-h" ] || [ "$1" = "-help" ]; then
