2558e22a59f1063c10043aefd0bc87ba32b24bc7
rootzoll <christian@geektank.de>
2024-12-19 15:09:00 +0100

    #4882 revert back to CLN v24.11

--- home.admin/config.scripts/cl.install.sh
+++ home.admin/config.scripts/cl.install.sh
@@ -2,7 +2,7 @@
 # https://lightning.readthedocs.io/
 
 # https://github.com/ElementsProject/lightning/releases
-CLVERSION="v24.11.1"
+CLVERSION="v24.11"
 
 # https://github.com/ElementsProject/lightning/tree/master/contrib/keys
 # rustyrussell D9200E6CD1ADB8F1
