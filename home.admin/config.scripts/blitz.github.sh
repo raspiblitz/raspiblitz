ddc5bb83fc277a906d33732c96d2cd550a65a5cf
rootzoll <christian@geektank.de>
2025-02-04 13:07:46 +0100

    clean blitz.github.sh

--- home.admin/config.scripts/blitz.github.sh
+++ home.admin/config.scripts/blitz.github.sh
@@ -3,8 +3,6 @@
 # This is for developing on your RaspiBlitz.
 # THIS IS NOT THE REGULAR UPDATE MECHANISM
 # and can lead to dirty state of your scripts.
-# IF YOU WANT TO UPDATE YOUR RASPIBLITZ:
-# https://github.com/rootzoll/raspiblitz/blob/dev/FAQ.md#how-to-update-my-raspiblitz-after-version-098
 
 # command info
 if [ $# -eq 0 ] || [ "$1" = "-h" ] || [ "$1" = "--help" ] || [ "$1" = "-help" ]; then
@@ -36,7 +34,6 @@ if [ "$1" == "info" ]; then
 fi
 
 # change branch if set as parameter
-vagrant=0
 clean=0
 install=0
 wantedBranch="$1"
@@ -45,12 +42,6 @@ if [ "${wantedBranch}" = "-run" ]; then
   # "-run" its just used by "patch" command and will ignore all further parameter
   wantedBranch="${activeBranch}"
   wantedGitHubUser="${activeGitHubUser}"
-  # detect if running in vagrant VM
-  vagrant=$(df | grep -c "/vagrant")
-  if [ "$2" = "git" ]; then 
-    echo "# forcing github over vagrant sync"
-    vagrant=0
-  fi
 fi
 if [ "${wantedBranch}" = "-install" ]; then
   install=1
@@ -65,7 +56,7 @@ if [ "${wantedBranch}" = "-justinstall" ]; then
 fi
 
 # set to another GutHub repo as origin
-if [ ${#wantedGitHubUser} -gt 0 ] && [ ${vagrant} -eq 0 ]; then
+if [ ${#wantedGitHubUser} -gt 0 ]; then
   echo "# your active GitHubUser is: ${activeGitHubUser}"
   echo "# your wanted GitHubUser is: ${wantedGitHubUser}"
   if [ "${activeGitHubUser}" = "${wantedGitHubUser}" ]; then
@@ -85,7 +76,7 @@ if [ ${#wantedGitHubUser} -gt 0 ] && [ ${vagrant} -eq 0 ]; then
   fi
 fi
 
-if [ ${#wantedBranch} -gt 0 ] && [ ${vagrant} -eq 0 ]; then
+if [ ${#wantedBranch} -gt 0 ]; then
   echo "# your active branch is: ${activeBranch}"
   echo "# your wanted branch is: ${wantedBranch}"
   if [ "${wantedBranch}" = "${activeBranch}" ]; then
@@ -117,35 +108,29 @@ if [ ${#wantedBranch} -gt 0 ] && [ ${vagrant} -eq 0 ]; then
   fi
 fi
 
-checkSumBlitzPyBefore=$(find /home/admin/raspiblitz/home.admin/BlitzPy -type f -exec md5sum {} \; | md5sum)
-checkSumBlitzTUIBefore=$(find /home/admin/raspiblitz/home.admin/BlitzTUI -type f -exec md5sum {} \; | md5sum)
-
-if [ ${vagrant} -eq 0 ]; then
-  origin=$(git remote -v | grep 'origin' | tail -n1)
-  echo "# *** SYNCING RASPIBLITZ CODE WITH GITHUB ***"
-  echo "# This is for developing on your RaspiBlitz."
-  echo "# THIS IS NOT THE REGULAR UPDATE MECHANISM"
-  echo "# and can lead to dirty state of your scripts."
-  echo "# REPO ----> ${origin}"
-  echo "# BRANCH --> ${activeBranch}"
-  echo "# ******************************************"
-  git config pull.rebase true
-  git pull 1>&2
-  cd ..
+origin=$(git remote -v | grep 'origin' | tail -n1)
+echo "# *** SYNCING RASPIBLITZ CODE WITH GITHUB ***"
+echo "# This is for developing on your RaspiBlitz."
+echo "# THIS IS NOT THE REGULAR UPDATE MECHANISM"
+echo "# and can lead to dirty state of your scripts."
+echo "# REPO ----> ${origin}"
+echo "# BRANCH --> ${activeBranch}"
+echo "# ******************************************"
+git config pull.rebase true
+if [ $? -eq 0 ]; then
+  echo "# OK - git config pull.rebase true"
 else
-  cd ..
-  echo "# --> VAGRANT IS ACTIVE"
-  echo "# *** SYNCING RASPIBLITZ CODE WITH VAGRANT LINKED DIRECTORY ***"
-  echo "# This is for developing on your RaspiBlitz with a VM."
-  echo "# - delete /home/admin/raspiblitz"
-  sudo rm -r /home/admin/raspiblitz
-  sudo mkdir /home/admin/raspiblitz
-  echo "# - copy from vagrant new raspiblitz files (ignore hidden dirs)"
-  sudo cp -R /vagrant/* /home/admin/raspiblitz
-  echo "# - set admin as owner of files"
-  sudo chown admin:admin -R /home/admin/raspiblitz
+  echo "# FAIL - git config pull.rebase true"
+  exit
 fi
-
+git pull 1>&2
+if [ $? -eq 0 ]; then
+  echo "# OK - git pull"
+else
+  echo "# FAIL - git pull"
+  exit
+fi
+cd ..
 echo "# COPYING from GIT-Directory to /home/admin/"
 echo "# - basic admin files"
 sudo rm -f *.sh
@@ -165,34 +150,6 @@ sudo rm -rf /home/admin/setup.scripts
 sudo -u admin cp -R /home/admin/raspiblitz/home.admin/setup.scripts /home/admin/setup.scripts
 sudo -u admin chmod 755 /home/admin/setup.scripts/*.sh
 sudo -u admin chmod 755 /home/admin/config.scripts/*.py
-echo "# ******************************************"
-
-echo "# Checking if the content of BlitzPy changed .."
-checkSumBlitzPyAfter=$(find /home/admin/raspiblitz/home.admin/BlitzPy -type f -exec md5sum {} \; | md5sum)
-echo "# checkSumBlitzPyBefore = ${checkSumBlitzPyBefore}"
-echo "# checkSumBlitzPyAfter  = ${checkSumBlitzPyAfter}"
-if [ "${checkSumBlitzPyBefore}" = "${checkSumBlitzPyAfter}" ] && [ ${install} -eq 0 ]; then
-  echo "# BlitzPy did not changed."
-else
-  blitzpy_wheel=$(ls -trR /home/admin/raspiblitz/home.admin/BlitzPy/dist | grep -E ".*any.whl" | tail -n 1)
-  blitzpy_version=$(echo ${blitzpy_wheel} | grep -oE "([0-9]\.[0-9]\.[0-9])")
-  echo "# BlitzPy changed --> UPDATING to Version ${blitzpy_version}"
-  sudo pip config set global.break-system-packages true
-  sudo -H /usr/bin/python -m pip install "/home/admin/raspiblitz/home.admin/BlitzPy/dist/${blitzpy_wheel}" >/dev/null 2>&1
-fi
-
-if [ "${touchscreen}" = "1" ]; then
-  echo "# Checking if the content of BlitzTUI changed .."
-  checkSumBlitzTUIAfter=$(find /home/admin/raspiblitz/home.admin/BlitzTUI -type f -exec md5sum {} \; | md5sum)
-  echo "# checkSumBlitzTUIBefore = ${checkSumBlitzTUIBefore}"
-  echo "# checkSumBlitzTUIAfter  = ${checkSumBlitzTUIAfter}"
-  if [ "${checkSumBlitzTUIBefore}" = "${checkSumBlitzTUIAfter}" ] && [ ${install} -eq 0 ] && [ ${clean} -eq 0 ]; then
-    echo "# BlitzTUI did not changed."
-  else
-    echo "# BlitzTUI changed --> UPDATING TOUCHSCREEN INSTALL ..."
-    sudo /home/admin/config.scripts/blitz.touchscreen.sh update
-  fi
-fi
 echo "# ******************************************"
 echo "# OK - shell scripts and assets are synced"
 echo "# Reboot recommended"
