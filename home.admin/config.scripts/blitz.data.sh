0a0c5833213e14bb7bc4c7e642c9b49851869510
rootzoll <christian@geektank.de>
2025-04-10 17:29:14 +0200

    fix param

--- home.admin/config.scripts/blitz.data.sh
+++ home.admin/config.scripts/blitz.data.sh
@@ -1071,7 +1071,7 @@ if [ "$1" = "migration" ] && [ "$2" = "hdd" ]; then
 
     if [ "${action}" = "menu-prepare" ]; then
         
-        dialog --title " Migrate Data to new HDD/SSD/NVMe " --yesno --yes-label "Start Migration" --no-label "Back" "\nTo migrate your RaspiBlitz data from your old HDD/SSD/NVMe to a new bigger drive, please make sure of the following:\n\n- Have your old drive replaced with the new one\n  or start with complete new hardware.\n\n- Have your old drive connected via USB3\n  where you may need an USB adapter and on\n  RasperryPi4 power old drive seperately.\n\nPress 'Start Migration' if everything is setup or go back." 17 70
+        dialog --title " Migrate Data to new HDD/SSD/NVMe " --yes-label "Start Migration" --no-label "Back" --yesno "\nTo migrate your RaspiBlitz data from your old HDD/SSD/NVMe to a new bigger drive, please make sure of the following:\n\n- Have your old drive replaced with the new one\n  or start with complete new hardware.\n\n- Have your old drive connected via USB3\n  where you may need an USB adapter and on\n  RasperryPi4 power old drive seperately.\n\nChoose 'Start Migration' if everything is setup or go back." 17 70
         if [ $? -gt 0 ]; then
             # user canceled
             exit 1
