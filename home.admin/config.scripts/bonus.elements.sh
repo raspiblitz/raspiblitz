aa6e49387e46c6a3ad824ca8a51641fd6dd6b24b
HODLmeTight <35168804+TrezorHannes@users.noreply.github.com>
2025-04-12 14:01:34 +0200

    bumped elements latest release tag

--- home.admin/config.scripts/bonus.elements.sh
+++ home.admin/config.scripts/bonus.elements.sh
@@ -2,7 +2,7 @@
 
 # set version (change if update is available)
 # https://github.com/ElementsProject/elements/releases
-VERSION="elements-23.2.1"
+VERSION="elements-23.2.7"
 SIG_PUBKEY="BD0F3062F87842410B06A0432F656B0610604482" # Pablo Greco <pgreco@blockstream.com>
 
 # command info
