7f68825df0cfb788f89b2c7a1e66810055a425fc
rootzoll <christian@geektank.de>
2025-04-19 20:22:55 +0200

    fix partition

--- home.admin/config.scripts/blitz.data.sh
+++ home.admin/config.scripts/blitz.data.sh
@@ -1455,7 +1455,7 @@ if [ "$1" = "migration" ] && [ "$2" = "hdd" ]; then
         fi
 
         # get the biggest partition of the source hdd (thats the data or storage partition with data)
-        sourcePartition=$(lsblk -no NAME,SIZE,TYPE | grep "${hddMigrateDeviceFrom}" | grep "part" | sort -k2 -h | tail -1 | awk '{print $1}')
+        sourcePartition=$(lsblk -no NAME,SIZE,TYPE | grep "${hddMigrateDeviceFrom}" | grep "part" | sort -k2 -h | tail -1 | awk '{print $1}' | tr -cd '[:alnum:]')
         if [ ${#sourcePartition} -eq 0 ]; then
             echo "error='no source partition found'"
             exit 1
