3b481e8e9d6cd494978e432a27d7a3de931c7e6d
rootzoll <christian@geektank.de>
2025-04-19 13:27:29 +0200

    change default

--- home.admin/setup.scripts/dialogSystemCopy.sh
+++ home.admin/setup.scripts/dialogSystemCopy.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-whiptail --title " BOOT FROM SSD/NVME " --yes-button "YES - BOOT SSD/NVME" --no-button "NO" --yesno "Your system allows to BOOT FROM SSD/NVME - which provides better performance but is still experimental.\n\nDo you want to copy RaspiBlitz system to SSD/NVME and boot from it?" 11 65
+whiptail --title " BOOT FROM SSD/NVME " --yes-button "NO" --no-button "YES - BOOT SSD/NVME" --yesno "Your system allows to BOOT FROM SSD/NVME - which provides better performance but is still experimental.\n\nDo you want to copy RaspiBlitz system to SSD/NVME and boot from it?" 11 65
 
 if [ "$?" == "0" ]; then
-    echo "# 0 --> Yes"
-    exit 0
-else
     echo "# 1 --> No"
     exit 1
+else
+    echo "# 0 --> Yes"
+    exit 0
 fi
