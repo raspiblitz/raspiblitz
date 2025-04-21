b22f572e81b86c084c1ce7ab30c74b6b1cbf12df
rootzoll <christian@geektank.de>
2025-04-21 23:11:27 +0200

    fix syntax

--- home.admin/config.scripts/blitz.data.sh
+++ home.admin/config.scripts/blitz.data.sh
@@ -779,9 +779,9 @@ if [ "$action" = "status" ]; then
     if [ ${#storageDevice} -gt 0 ]; then
         storageCelsius=$(smartctl -A /dev/${storageDevice} | grep -E '^Temperature:|Temperature Sensor' | awk '{print $(NF-1)}' | head -n 1)
     fi
-    elif [ ${#dataDevice} -gt 0 ]; then
+    if [ ${#dataDevice} -gt 0 ]; then
         dataCelsius=$(smartctl -A /dev/${dataDevice} | grep -E '^Temperature:|Temperature Sensor' | awk '{print $(NF-1)}' | head -n 1)
-    if [ $combinedDataStorage -eq 1 ]; then
+    elif [ $combinedDataStorage -eq 1 ]; then
         dataCelsius="${storageCelsius}"
     fi
     if [ ${#systemDevice} -gt 0 ]; then
