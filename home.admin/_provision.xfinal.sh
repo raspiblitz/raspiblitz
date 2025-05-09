b1f2d74b01beb34fde11a95ebce291933bad4df1
rootzoll <christian@geektank.de>
2025-05-09 14:53:18 +0200

    remove debug

--- home.admin/_provision.xfinal.sh
+++ home.admin/_provision.xfinal.sh
@@ -7,29 +7,23 @@ echo "# /home/admin/_provision.xfinal.sh" > /var/cache/raspiblitz/final.log
 
 # signal that setup phase is over
 /home/admin/_cache.sh set setupPhase "done"
-echo "# A" > /var/cache/raspiblitz/final.log
 
 # source info fresh
 source /home/admin/raspiblitz.info
-echo "# B" > /var/cache/raspiblitz/final.log
 echo "# source /home/admin/raspiblitz.info" >> /home/admin/raspiblitz.log
 cat /home/admin/raspiblitz.info >> /home/admin/raspiblitz.log
-echo "# C" > /var/cache/raspiblitz/final.log
 
 # make sure for future starts that blockchain service gets started after bootstrap
 # so deamon reloas needed ... system will go into reboot after last loop
 # needs to be after wait loop because otherwise the "restart" on COPY OVER LAN will not work
-echo "# Updating service bitcoind.service ..."
+echo "# Updating service bitcoind.service ..." >> /home/admin/raspiblitz.log
 sudo sed -i "s/^Wants=.*/Wants=bootstrap.service/g" /etc/systemd/system/bitcoind.service
 sudo sed -i "s/^After=.*/After=bootstrap.service/g" /etc/systemd/system/bitcoind.service
 sudo systemctl daemon-reload 2>/dev/null
 
-echo "# D" > /var/cache/raspiblitz/final.log
-
 # delete setup data from RAM
+echo "# removing raspiblitz.setup" >> /home/admin/raspiblitz.log
 sudo rm /var/cache/raspiblitz/temp/raspiblitz.setup
-
-echo "# E" > /var/cache/raspiblitz/final.log
 
 ########################################
 # AFTER SETUP REBOOT
@@ -37,8 +31,7 @@ echo "# E" > /var/cache/raspiblitz/final.log
 sudo cp /home/admin/raspiblitz.log /home/admin/raspiblitz.setup.log
 sudo chmod 640 /home/admin/raspiblitz.setup.log
 sudo chown root:sudo /home/admin/raspiblitz.setup.log
-#timeout 120 sudo /home/admin/config.scripts/blitz.shutdown.sh reboot finalsetup
+echo "# SHUTTING DOWN ..." >> /home/admin/raspiblitz.log
+timeout 120 sudo /home/admin/config.scripts/blitz.shutdown.sh reboot finalsetup
 # if system has not rebooted yet - force reboot directly
-#sudo shutdown -r now
-
-echo "# F" > /var/cache/raspiblitz/final.log
+sudo shutdown -r now
