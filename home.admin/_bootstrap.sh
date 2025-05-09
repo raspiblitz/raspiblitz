a027222adef08a3806d71007422f7895beacd5be
rootzoll <christian@geektank.de>
2025-05-09 19:46:41 +0200

    better debug

--- home.admin/_bootstrap.sh
+++ home.admin/_bootstrap.sh
@@ -810,7 +810,7 @@ if [ "${scenario}" != "ready" ] ; then
       echo "SYSTEM: ${setupCommand} SYSTEM done" >> ${logFile}
     else
       if [ "${systemMountedPath}" = "/" ]; then
-        echo "INFO: ${setupCommand} SYSTEM skipped - ist active system" >> ${logFile}
+        echo "SYSTEM: ${setupCommand} SYSTEM skipped - ist active system" >> ${logFile}
     fi
 
     # DATA
