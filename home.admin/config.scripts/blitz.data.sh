35874a70be15b5f75355df32706f3993ea753f76
rootzoll <christian@geektank.de>
2025-05-13 17:45:10 +0200

    remove debug

--- home.admin/config.scripts/blitz.data.sh
+++ home.admin/config.scripts/blitz.data.sh
@@ -806,7 +806,7 @@ if [ "$action" = "status" ]; then
         dataMountedPath="${storageMountedPath}"
     fi
     
-    echo "# E"
+    # echo "# Used Space"
 
     # get used space on drives in GB
     storageUsePercent=""
@@ -824,7 +824,7 @@ if [ "$action" = "status" ]; then
         systemUsePercent=$(df "/dev/${systemPartition}" 2>/dev/null | awk 'NR==2 {sub(/%/, "", $5); print $5}')
     fi
 
-    echo "# D"
+    # echo "# Free Space"
 
     # get free space on drives
     if [ ${#storagePartition} -gt 0 ]; then
@@ -839,7 +839,7 @@ if [ "$action" = "status" ]; then
         systemFreeKB=$(df -k | grep "/dev/${systemPartition}" 2>/dev/null | awk '{print $4}' | tail -n 1)
     fi
 
-    echo "# C"
+    # echo "# Temperature"
 
     # get Temperature of drives
     if [ ${#storageDevice} -gt 0 ]; then
@@ -854,7 +854,7 @@ if [ "$action" = "status" ]; then
         systemCelsius=$(smartctl -A /dev/${systemDevice} 2>/dev/null | grep -E '^Temperature:|Temperature Sensor' | awk '{print $(NF-1)}' | head -n 1)
     fi
 
-    echo "# B"
+    # echo "# Unused Space"
 
     # get unused space on drives
     storageUnusedPercent=0
@@ -870,7 +870,7 @@ if [ "$action" = "status" ]; then
         systemUnusedPercent=$(parted /dev/${systemDevice} unit % print free 2>/dev/null | awk '/Free Space/ {v=$(NF-2); gsub(/[^0-9.]/, "", v)} END{print int(v)}')
     fi
 
-    echo "# A"
+    # echo "# RESULTS"
 
     # output the result
     echo "scenario='${scenario}'"
