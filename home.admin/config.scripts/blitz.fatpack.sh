93b31d9d76f195819806d851afea8dd43be9e276
rootzoll <christian@geektank.de>
2025-08-26 16:58:51 +0200

    rm sudo

--- home.admin/config.scripts/blitz.fatpack.sh
+++ home.admin/config.scripts/blitz.fatpack.sh
@@ -136,7 +136,7 @@ sudo rm -r /home/blitzapi/blitz_web/build/*
 
 echo "* Adding Code&Compile for WEBUI-APP: ALBYHUB"
 /home/admin/config.scripts/bonus.albyhub.sh install || exit 1
-source <(sudo /home/admin/config.scripts/bonus.albyhub.sh status)
+source <(/home/admin/config.scripts/bonus.albyhub.sh status)
 if [ "${fatpack}" != "1" ]; then
   echo "FATPACK FAIL: albyhub"
   exit 1
@@ -144,7 +144,7 @@ fi
 
 echo "* Adding Code&Compile for WEBUI-APP: LNBITS"
 /home/admin/config.scripts/bonus.lnbits.sh install || exit 1
-source <(sudo /home/admin/config.scripts/bonus.lnbits.sh status)
+source <(/home/admin/config.scripts/bonus.lnbits.sh status)
 if [ "${fatpack}" != "1" ]; then
   echo "FATPACK FAIL: lnbits"
   exit 1
@@ -152,7 +152,7 @@ fi
 
 echo "* Adding Code&Compile for WEBUI-APP: JAM"
 /home/admin/config.scripts/bonus.jam.sh install || exit 1
-source <(sudo /home/admin/config.scripts/bonus.jam.sh status)
+source <(/home/admin/config.scripts/bonus.jam.sh status)
 if [ "${fatpack}" != "1" ]; then
   echo "FATPACK FAIL: jam"
   exit 1
@@ -160,7 +160,7 @@ fi
 
 echo "* Adding Code&Compile for WEBUI-APP: BTCPAYSERVER"
 /home/admin/config.scripts/bonus.btcpayserver.sh install || exit 1
-source <(sudo /home/admin/config.scripts/bonus.btcpayserver.sh status)
+source <(/home/admin/config.scripts/bonus.btcpayserver.sh status)
 if [ "${fatpack}" != "1" ]; then
   echo "FATPACK FAIL: btcpayserver"
   exit 1
@@ -168,7 +168,7 @@ fi
 
 echo "* Adding Code&Compile for WEBUI-APP: RTL"
 /home/admin/config.scripts/bonus.rtl.sh install || exit 1
-source <(sudo /home/admin/config.scripts/bonus.rtl.sh status)
+source <(/home/admin/config.scripts/bonus.rtl.sh status)
 if [ "${fatpack}" != "1" ]; then
   echo "FATPACK FAIL: rtl"
   exit 1
@@ -176,7 +176,7 @@ fi
 
 echo "* Adding Code&Compile for WEBUI-APP: THUNDERHUB"
 /home/admin/config.scripts/bonus.thunderhub.sh install || exit 1
-source <(sudo /home/admin/config.scripts/bonus.thunderhub.sh status)
+source <(/home/admin/config.scripts/bonus.thunderhub.sh status)
 if [ "${fatpack}" != "1" ]; then
   echo "FATPACK FAIL: thunderhub"
   exit 1
@@ -192,7 +192,7 @@ fi
 
 echo "* Adding Code&Compile for WEBUI-APP: MEMPOOL"
 /home/admin/config.scripts/bonus.mempool.sh install || exit 1
-source <(sudo /home/admin/config.scripts/bonus.mempool.sh status)
+source <(/home/admin/config.scripts/bonus.mempool.sh status)
 if [ "${fatpack}" != "1" ]; then
   echo "FATPACK FAIL: mempool"
   exit 1
@@ -200,7 +200,7 @@ fi
 
 echo "* Adding Code&Compile for WEBUI-APP: ELECTRS"
 /home/admin/config.scripts/bonus.electrs.sh install || exit 1
-source <(sudo /home/admin/config.scripts/bonus.electrs.sh status)
+source <(/home/admin/config.scripts/bonus.electrs.sh status)
 if [ "${fatpack}" != "1" ]; then
   echo "FATPACK FAIL: electrs"
   exit 1
