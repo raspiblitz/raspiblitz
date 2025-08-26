a0f382e5df04d0a78a09defc5935a3824e6429d5
rootzoll <christian@geektank.de>
2025-08-26 18:16:14 +0200

    rm debug logs

--- home.admin/config.scripts/bonus.electrs.sh
+++ home.admin/config.scripts/bonus.electrs.sh
@@ -18,7 +18,7 @@ PGPsigner="romanz"
 PGPpubkeyLink="https://github.com/${PGPsigner}.gpg"
 PGPpubkeyFingerprint="87CAE5FA46917CBB"
 
-source /mnt/hdd/app-data/raspiblitz.conf
+source /mnt/hdd/app-data/raspiblitz.conf 2>/dev/null
 
 # give status (dont call regularly - just on occasions)
 if [ "$1" = "status" ]; then
