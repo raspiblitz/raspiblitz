cf4f5f5ba6d7a20c15bed9cf066a5f6ee3167873
rootzoll <christian@geektank.de>
2025-08-27 13:37:29 +0200

    root drive bytes

--- home.admin/config.scripts/blitz.bootdrive.sh
+++ home.admin/config.scripts/blitz.bootdrive.sh
@@ -33,6 +33,7 @@ rootPartitionLine=$(sudo mount | grep " / " | cut -d " " -f 1)
 rootPartition=$(basename ${rootPartitionLine})
 rootDrive=$(basename "$(readlink -f "/sys/class/block/$rootPartition/..")")
 rootPartitionBytes=$(lsblk -b -o NAME,SIZE | grep "${rootPartition}" | awk '{print $2}')
+rootDriveBytes=$(lsblk -b -o NAME,SIZE | grep "${rootDrive}" | awk '{print $2}')
 
 # make conclusions
 needsExpansion=0
@@ -49,6 +50,7 @@ if [ "${action}" == "status" ]; then
     echo "rootPartition='${rootPartition}'"
     echo "rootPartitionBytes=${rootPartitionBytes}"
     echo "rootDrive='${rootDrive}'"
+    echo "rootDriveBytes=${rootDriveBytes}"
     echo "needsExpansion=${needsExpansion}"
     echo "fsexpanded=${fsexpanded}" # from raspiblitz.info
     echo "tooSmall=${tooSmall}"
