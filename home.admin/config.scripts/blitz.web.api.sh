6c41a815efc9c4a8de21c86e84410a3d77519086
rootzoll <christian@geektank.de>
2025-11-11 10:11:26 +0100

    update pip

--- home.admin/config.scripts/blitz.web.api.sh
+++ home.admin/config.scripts/blitz.web.api.sh
@@ -328,6 +328,7 @@ if [ "$1" = "1" ] || [ "$1" = "on" ]; then
     echo "error='pip install upgrade Cython'"
   fi
   echo "# Installing dependencies from requirements.txt ..."
+  sudo -u blitzapi ./venv/bin/python -m pip install --upgrade pip
   sudo -u blitzapi env PIP_CONFIG_FILE=/dev/null PIP_INDEX_URL=https://pypi.org/simple PIP_EXTRA_INDEX_URL= ./venv/bin/pip install --no-cache-dir rich-toolkit==0.14.6
   if ! sudo -u blitzapi ./venv/bin/pip install --no-cache-dir -r requirements.txt --no-deps --index-url https://pypi.org/simple --no-extra-index-url; then
     echo "error='pip install failed'"
