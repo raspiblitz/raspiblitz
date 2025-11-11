2a64a45b46d2bdd18b549e308c7e16cd4c344640
rootzoll <christian@geektank.de>
2025-11-11 15:15:36 +0100

    v2 relative file names

--- home.admin/config.scripts/blitz.migration.sh
+++ home.admin/config.scripts/blitz.migration.sh
@@ -96,7 +96,8 @@ if [ "$1" = "export" ]; then
   dataPath=$(readlink -f /mnt/hdd/app-data)
   echo "# Building the Export File - this can take some time (${dataPath}) .."
   sudo mkdir -p ${defaultDownloadPath}
-  sudo tar -zcvf ${defaultDownloadPath}/raspiblitz-export-temp.tar.gz -X ~/.exclude.temp "${dataPath}" 1>~/.include.temp 2>/dev/null
+  # sudo tar -zcvf ${defaultDownloadPath}/raspiblitz-export-temp.tar.gz "${dataPath}" 1>~/.include.temp 2>/dev/null
+  sudo tar -zcvf "${defaultDownloadPath}/raspiblitz-export-temp.tar.gz" -C "$(dirname "$dataPath")" "$(basename "$dataPath")" 1>~/.include.temp 2>/dev/null
 
   # delete info files from app-data after export
   sudo rm /mnt/hdd/app-data/v2.migration.info 2>/dev/null
