3862f9c8387258b694f0f6af30138db90f344d40
rootzoll <christian@geektank.de>
2025-12-21 14:17:54 +0100

    #5145 set hddBlocksBitcoin cache value

--- home.admin/_bootstrap.sh
+++ home.admin/_bootstrap.sh
@@ -633,8 +633,8 @@ if [ "${scenario}" != "ready" ] ; then
 
   # TODO: REPLACE THIS OLD VALUES IN SSH & WEBUI
   /home/admin/_cache.sh set hddCandidate "${hddCandidate}"
-  /home/admin/_cache.sh set hddGigaBytes "${hddGigaBytes}"
-  /home/admin/_cache.sh set hddBlocksBitcoin "${hddBlocksBitcoin}"
+  /home/admin/_cache.sh set hddGigaBytes "${storageSizeGB}"
+  /home/admin/_cache.sh set hddBlocksBitcoin "${storageBlockchainGB}"
   /home/admin/_cache.sh set hddGotMigrationData "${hddGotMigrationData}"
   /home/admin/_cache.sh set hddVersionLND "${hddVersionLND}"
 
